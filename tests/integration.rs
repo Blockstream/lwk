eaf11f978d797534aca369f0a8930b4f9e514588
Leonardo Comandini <leo@leocomandini.org>
2021-05-25 18:17:08 +0200

    tests: liquidex: swap lbtc <-> asset

--- tests/integration.rs
+++ tests/integration.rs
@@ -125,5 +125,23 @@ fn dex() {
     assert_eq!(wallet2.balance(&asset2), 0);
     assert_eq!(wallet2.balance(&policy_asset), 5_000);
 
+    // L-BTC 5_000 <-> asset2 5_000
+    let sats_w1_policy_before = wallet1.balance(&policy_asset);
+    let utxo = wallet2.asset_utxos(&policy_asset)[0].txo.outpoint;
+    let proposal = wallet2.liquidex_make(&utxo, &asset2, 1.0);
+
+    log::warn!("proposal: {:?}", proposal);
+    let txid = wallet1.liquidex_take(&proposal);
+
+    wallet1.wait_for_tx(&txid);
+    wallet2.wait_for_tx(&txid);
+
+    let fee = wallet1.get_fee(&txid);
+    let sats_w1_policy_after = wallet1.balance(&policy_asset);
+    assert_eq!(sats_w1_policy_after - sats_w1_policy_before + fee, 5_000);
+    assert_eq!(wallet1.balance(&asset2), 5_000);
+    assert_eq!(wallet2.balance(&asset2), 5_000);
+    assert_eq!(wallet2.balance(&policy_asset), 0);
+
     server.stop();
 }

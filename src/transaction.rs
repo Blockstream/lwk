bfa489e7ca18d034a46a0b0bebf8f0c5d0c47b54
Leonardo Comandini <leo@leocomandini.org>
2021-08-26 22:31:23 +0200

    Fix change size for fee estimation
    Also rename some variables.

--- src/transaction.rs
+++ src/transaction.rs
@@ -13,9 +13,10 @@ use std::collections::{HashMap, HashSet};
 
 pub const DUST_VALUE: u64 = 546;
 
-// Sizes used for fee estimation
-pub const SURJECTIONPROOF_SIZE: u64 = 135;
-pub const RANGEPROOF_SIZE: u64 = 4174;
+// 3-input ASP
+pub const DEFAULT_SURJECTIONPROOF_SIZE: u64 = 135;
+// 52-bit rangeproof
+pub const DEFAULT_RANGEPROOF_SIZE: u64 = 4174;
 
 pub fn strip_witness(tx: &mut elements::Transaction) {
     for input in tx.input.iter_mut() {
@@ -120,15 +121,14 @@ pub fn estimated_fee(tx: &elements::Transaction, fee_rate: f64, more_changes: u8
             asset: confidential::Asset::Confidential(mock_generator()),
             value: confidential::Value::Confidential(mock_pedersen_commitment()),
             nonce: confidential::Nonce::Confidential(mock_pubkey()),
+            script_pubkey: vec![0u8; 23].into(),
             ..Default::default()
         };
         tx.output.push(new_out);
     }
 
-    let proofs_size = (SURJECTIONPROOF_SIZE + RANGEPROOF_SIZE) as usize * tx.output.len();
-    for output in tx.output.iter_mut() {
-        output.script_pubkey = vec![0u8; 21].into();
-    }
+    let proofs_size =
+        (DEFAULT_SURJECTIONPROOF_SIZE + DEFAULT_RANGEPROOF_SIZE) as usize * tx.output.len();
 
     tx.output.push(elements::TxOut::default()); // mockup for the explicit fee output
                                                 // proofs belongs to the witness, their size is discounted and thus is not scaled

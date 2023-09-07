3e3443a0fb5ed2e658d0acb1ddf676532c507848
Leonardo Comandini <leo@leocomandini.org>
2023-09-07 10:23:50 +0200

    interface->wallet

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 mod error;
-mod interface;
 mod model;
 mod network;
 mod scripts;
 mod store;
 mod sync;
+mod wallet;
 
 pub use crate::error::Error;
-pub use crate::interface::ElectrumWallet;
 pub use crate::model::{GetTransactionsOpt, TransactionDetails, UnblindedTXO};
 pub use crate::network::ElementsNetwork;
+pub use crate::wallet::ElectrumWallet;

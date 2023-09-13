677d4996bc42bf77b4ebb670410e603bab4df806
Leonardo Comandini <leo@leocomandini.org>
2023-09-13 15:20:27 +0200

    tests: liquid: use signer

--- wollet/tests/integration.rs
+++ wollet/tests/integration.rs
@@ -1,14 +1,15 @@
 mod test_session;
 
+use software_signer::*;
 use test_session::*;
 
 #[test]
 fn liquid() {
     let mut server = setup();
     let mnemonic = "abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about";
-    let xpub = "tpubD6NzVbkrYhZ4XYa9MoLt4BiMZ4gkt2faZ4BcmKu2a9te4LDpQmvEz2L2yDERivHxFPnxXXhqDRkUNnQCpZggCyEZLBktV7VaSmwayqMJy1s"; // tprv8ZgxMBicQKsPe5YMU9gHen4Ez3ApihUfykaqUorj9t6FDqy3nP6eoXiAo2ssvpAjoLroQxHqr3R5nE3a5dU3DHTjTgJDd7zrbniJr6nrCzd
-    let master_blinding_key = "9c8e4f05c7711a98c838be228bcb84924d4570ca53f35fa1c793e58841d47023";
-    let desc_str = format!("ct(slip77({}),elwpkh({}/*))", master_blinding_key, xpub);
+    let signer = Signer::new(mnemonic, &wollet::EC).unwrap();
+    let slip77_key = "9c8e4f05c7711a98c838be228bcb84924d4570ca53f35fa1c793e58841d47023";
+    let desc_str = format!("ct(slip77({}),elwpkh({}/*))", slip77_key, signer.xpub());
     let mut wallet = TestElectrumWallet::new(&server.electrs.electrum_url, &desc_str);
 
     wallet.fund_btc(&mut server);

f8275870ff01b487ab9bb84e7f74e0a0f727ae9b
Riccardo Casatta <riccardo@casatta.it>
2023-09-22 09:55:34 +0200

    remove some instances of insert_bip32_derivation

--- wollet/src/wallet.rs
+++ wollet/src/wallet.rs
@@ -709,10 +709,6 @@ impl ElectrumWallet {
         let fee_output = Output::new_explicit(Script::default(), fee, self.policy_asset(), None);
         pset.add_output(fee_output);
 
-        for output in pset.outputs_mut() {
-            self.insert_bip32_derivation(&output.script_pubkey, &mut output.bip32_derivation);
-        }
-
         // Blind the transaction
         let mut rng = thread_rng();
         pset.blind_last(&mut rng, &EC, &inp_txout_sec)?;
@@ -790,10 +786,6 @@ impl ElectrumWallet {
         }
         let fee_output = Output::new_explicit(Script::default(), fee, self.policy_asset(), None);
         pset.add_output(fee_output);
-
-        for output in pset.outputs_mut() {
-            self.insert_bip32_derivation(&output.script_pubkey, &mut output.bip32_derivation);
-        }
 
         // Blind the transaction
         let mut rng = thread_rng();

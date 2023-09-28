aebeb2537820e940601c8a496c09d8f2b1cd0672
Riccardo Casatta <riccardo@casatta.it>
2023-09-28 08:39:30 +0200

    pin_server: test handshake verify against custom pub key

--- jade/test/pin_server.rs
+++ jade/test/pin_server.rs
@@ -1,6 +1,11 @@
 use std::{collections::HashMap, io::Write};
 
-use bitcoin::{secp256k1::Secp256k1, PrivateKey, PublicKey};
+use bitcoin::{
+    hashes::{hex::FromHex, sha256, Hash},
+    secp256k1::{ecdsa::Signature, Message, Secp256k1},
+    PrivateKey, PublicKey,
+};
+use jade::protocol::HandshakeParams;
 use rand::{thread_rng, RngCore};
 use tempfile::{tempdir, TempDir};
 use testcontainers::{clients, core::WaitFor, Image, ImageArgs};
@@ -92,10 +97,32 @@ impl Image for PinServerEmulator {
 #[test]
 fn pin_server() {
     let docker = clients::Cli::default();
-    let container = docker.run(PinServerEmulator::default());
+    let pin_server = PinServerEmulator::default();
+    let pin_server_pub_key = *pin_server.pub_key();
+    let container = docker.run(pin_server);
+
     let port = container.get_host_port_ipv4(PORT);
-    let result = ureq::get(&format!("http://127.0.0.1:{port}"))
-        .call()
-        .unwrap();
+    let pin_server_url = format!("http://127.0.0.1:{port}");
+    let result = ureq::get(&pin_server_url).call().unwrap();
     assert_eq!(result.status(), 200);
+
+    let start_handshake_url = format!("{pin_server_url}/start_handshake");
+    let resp = ureq::post(&start_handshake_url).call().unwrap();
+    let params: HandshakeParams = resp.into_json().unwrap();
+    verify(&params, &pin_server_pub_key);
+}
+
+pub fn verify(params: &HandshakeParams, pin_server_pub_key: &PublicKey) {
+    let ske_bytes = Vec::<u8>::from_hex(&params.ske).unwrap();
+    let ske_hash = sha256::Hash::hash(&ske_bytes);
+
+    let signature_bytes = Vec::<u8>::from_hex(&params.sig).unwrap();
+    let signature = Signature::from_compact(&signature_bytes).unwrap();
+
+    let message = Message::from_slice(&ske_hash[..]).unwrap();
+
+    let verify = Secp256k1::verification_only()
+        .verify_ecdsa(&message, &signature, &pin_server_pub_key.inner)
+        .is_ok();
+    assert!(verify);
 }

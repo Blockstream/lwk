f0388b3af9aadff961e5eea3aa51068e01306251
Riccardo Casatta <riccardo@casatta.it>
2023-10-04 15:39:25 +0200

    jade: remove stale comment

--- jade/test/emulator.rs
+++ jade/test/emulator.rs
@@ -52,8 +52,6 @@ fn debug_set_mnemonic() {
 
     let result = initialized_jade.jade.version_info().unwrap();
     insta::assert_yaml_snapshot!(result);
-    // TODO make Jade initialization of other testss via debug_set_mnemonic instead of setting the
-    // pin server every time
 }
 
 #[test]

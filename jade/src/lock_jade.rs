21bbe421c20771a07a8dd2e8b202fecd0123b96c
Riccardo Casatta <riccardo@casatta.it>
2023-10-06 13:39:29 +0200

    jade: LockJade add new and get_xpub

--- jade/src/lock_jade.rs
+++ jade/src/lock_jade.rs
@@ -2,12 +2,25 @@ use std::sync::Mutex;
 
 use elements::pset::PartiallySignedTransaction;
 
-use crate::{sign_pset::Error, Jade};
+use crate::{protocol::StringResult, sign_pset, Jade};
 
 pub struct LockJade(Mutex<Jade>);
 
 impl LockJade {
-    pub fn sign_pset(&self, pset: &mut PartiallySignedTransaction) -> Result<u32, Error> {
+    pub fn new(jade: Jade) -> Self {
+        Self(Mutex::new(jade))
+    }
+    pub fn sign_pset(
+        &self,
+        pset: &mut PartiallySignedTransaction,
+    ) -> Result<u32, sign_pset::Error> {
         self.0.lock().unwrap().sign_pset(pset)
+    }
+
+    pub fn get_xpub(
+        &self,
+        params: crate::protocol::GetXpubParams,
+    ) -> Result<StringResult, crate::error::Error> {
+        self.0.lock().unwrap().get_xpub(params)
     }
 }

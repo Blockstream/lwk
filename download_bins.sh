64f16105fa2872eb31df2b29e62b82b9ad26e4a4
Riccardo Casatta <riccardo@casatta.it>
2023-10-13 11:05:02 +0200

    fix download_bins.sh

--- download_bins.sh
+++ download_bins.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -e
 
 mkdir -p bin
 cd bin || exit 1
@@ -9,6 +10,5 @@ gunzip electrs_linux_esplora_a33e97e1a1fc63fa9c20a116bb92579bbf43b254_liquid.gz
 chmod +x electrs_linux_esplora_a33e97e1a1fc63fa9c20a116bb92579bbf43b254_liquid
 
 # elementsd
-elements-22.1.1
 wget https://github.com/ElementsProject/elements/releases/download/elements-22.1.1/elements-22.1.1-x86_64-linux-gnu.tar.gz
 tar -xzf elements-22.1.1-x86_64-linux-gnu.tar.gz

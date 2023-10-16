006dfac671e6b235e0be6bc1fabba89087dc68f2
Riccardo Casatta <riccardo@casatta.it>
2023-10-16 14:25:27 +0200

    wollet: use jade_unlock method

--- wollet/tests/serial.rs
+++ wollet/tests/serial.rs
@@ -1,9 +1,6 @@
 use std::time::Duration;
 
-use jade::{
-    protocol::{HandshakeCompleteParams, HandshakeParams},
-    serialport, Jade,
-};
+use jade::{serialport, Jade};
 
 #[test]
 #[ignore = "requires hardware jade connected via usb/serial "]
@@ -18,29 +15,8 @@ fn jade_send_lbtc() {
 
     let mut jade_api = Jade::new(port.into(), jade::Network::LocaltestLiquid);
 
-    let result = jade_api.auth_user().unwrap();
-    dbg!(&result);
+    let result = jade_api.unlock_jade().unwrap();
 
-    let url = result.urls()[0].as_str();
-    dbg!(&url);
-    let resp = minreq::post(url).send().unwrap();
-    assert_eq!(resp.status_code, 200);
-    let params: HandshakeParams = serde_json::from_slice(resp.as_bytes()).unwrap();
-    dbg!(&params);
-
-    let result = jade_api.handshake_init(params).unwrap();
-    dbg!(&result);
-
-    let handshake_data = result.data();
-    let data = serde_json::to_vec(&handshake_data).unwrap();
-    let next_url = &result.urls()[0];
-    let resp = minreq::post(next_url).with_body(data).send().unwrap();
-    assert_eq!(resp.status_code, 200);
-    let params: HandshakeCompleteParams = serde_json::from_slice(resp.as_bytes()).unwrap();
-
-    dbg!(&params);
-
-    let result = jade_api.handshake_complete(params).unwrap();
     assert!(result);
 
     //TODO start bitcoind/electrs, receive lbtc, send lbtc

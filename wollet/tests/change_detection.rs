04c3fc52cdbc0cdf597d5dd4cbfbbc356df7b85b
Riccardo Casatta <riccardo@casatta.it>
2023-10-19 14:19:48 +0200

    wollet: tests: setup test detect change for multisig

--- wollet/tests/change_detection.rs
+++ wollet/tests/change_detection.rs
@@ -1,12 +1,12 @@
 use bs_containers::testcontainers::clients::Cli;
 use elements::bitcoin::bip32::DerivationPath;
-use jade::get_receive_address::Variant;
+use jade::{get_receive_address::Variant, mutex_jade::MutexJade};
 use signer::Signer;
 use std::str::FromStr;
 
 use crate::{
     jade_emulator::inner_jade_debug_initialization,
-    test_session::{generate_slip77, setup, TestWollet},
+    test_session::{generate_signer, generate_slip77, setup, TestWollet},
     TEST_MNEMONIC,
 };
 
@@ -87,4 +87,38 @@ fn send_lbtc_detect_change(signers: &[&Signer], variant: Variant) {
 
     let node_address = server.node_getnewaddress();
     wallet.send_btc(signers, None, Some((node_address, 10_000)));
+}
+
+#[test]
+fn emul_send_lbtc_detect_change_multisig() {
+    let docker = Cli::default();
+    let jade_init = inner_jade_debug_initialization(&docker, TEST_MNEMONIC.to_string());
+    send_lbtc_detect_change_multisig(jade_init.jade)
+}
+
+fn send_lbtc_detect_change_multisig(mut s1: MutexJade) {
+    let s1_xpub = s1.get_mut().unwrap().get_master_xpub().unwrap();
+    let s1_fingerprint = s1_xpub.fingerprint();
+
+    let s2 = generate_signer();
+    let s2_xpub = s2.xpub();
+    let s2_fingerprint = s2_xpub.fingerprint();
+
+    let slip77 = generate_slip77();
+
+    let desc_str = format!(
+        "ct(slip77({slip77}),elwsh(multi(2,[{s1_fingerprint}]{s1_xpub}/*,[{s2_fingerprint}]{s2_xpub}/*)))"
+    );
+
+    dbg!(&desc_str);
+
+    let server = setup();
+
+    let mut wallet = TestWollet::new(&server.electrs.electrum_url, &desc_str);
+
+    wallet.fund_btc(&server);
+
+    let signers = [&Signer::Jade(&s1), &Signer::Software(s2)];
+    let node_address = server.node_getnewaddress();
+    wallet.send_btc(&signers, None, Some((node_address, 10_000)));
 }

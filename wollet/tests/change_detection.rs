fc1f0e5b96d57efa22bb4ca80fd78fc574bd5e2e
Riccardo Casatta <riccardo@casatta.it>
2023-10-19 11:17:55 +0200

    wollet: tests: refactor out jade from change detect

--- wollet/tests/change_detection.rs
+++ wollet/tests/change_detection.rs
@@ -1,20 +1,16 @@
 use std::{str::FromStr, time::Duration};
 
 use elements::bitcoin::bip32::DerivationPath;
-use jade::{
-    derivation_path_to_vec,
-    mutex_jade::MutexJade,
-    protocol::{GetXpubParams, JadeState},
-    serialport, Jade,
-};
+use jade::{mutex_jade::MutexJade, protocol::JadeState, serialport, Jade};
 use signer::Signer;
 
-use crate::test_session::{setup, TestWollet};
+use crate::test_session::{generate_slip77, setup, TestWollet};
 
 #[test]
 #[ignore = "requires hardware jade: initialized with localtest network, connected via usb/serial"]
-fn jade_send_lbtc() {
+fn jade_send_lbtc_detect_change() {
     let network = jade::Network::LocaltestLiquid;
+
     let ports = serialport::available_ports().unwrap();
     assert!(!ports.is_empty());
     let path = &ports[0].port_name;
@@ -34,31 +30,33 @@ fn jade_send_lbtc() {
         jade_state = jade.get_mut().unwrap().version_info().unwrap().jade_state;
     }
     assert_eq!(jade_state, JadeState::Ready);
+    let signers = [&Signer::Jade(&jade)];
 
-    let server = setup();
+    send_lbtc_detect_change(&signers);
+
+    // refuse the tx on the jade to keep the session logged
+    jade.get_mut().unwrap().logout().unwrap();
+}
+
+fn send_lbtc_detect_change(signers: &[&Signer]) {
     let path = "84h/1h/0h";
-    let master_node = jade.get_mut().unwrap().get_master_xpub().unwrap();
+    let master_node = signers[0].xpub().unwrap();
     let fingerprint = master_node.fingerprint();
-    let xpub = jade
-        .get_xpub(GetXpubParams {
-            network,
-            path: derivation_path_to_vec(&DerivationPath::from_str(&format!("m/{path}")).unwrap()),
-        })
+    let xpub = signers[0]
+        .derive_xpub(&DerivationPath::from_str(&format!("m/{path}")).unwrap())
         .unwrap();
 
-    let slip77_key = "9c8e4f05c7711a98c838be228bcb84924d4570ca53f35fa1c793e58841d47023";
+    let slip77_key = generate_slip77();
 
     // m / purpose' / coin_type' / account' / change / address_index
     let desc_str = format!("ct(slip77({slip77_key}),elwpkh([{fingerprint}/{path}]{xpub}/1/*))");
+
+    let server = setup();
+
     let mut wallet = TestWollet::new(&server.electrs.electrum_url, &desc_str);
 
     wallet.fund_btc(&server);
 
-    let signers = [&Signer::Jade(&jade)];
-
     let node_address = server.node_getnewaddress();
     wallet.send_btc(&signers, None, Some((node_address, 10_000)));
-
-    // refuse the tx on the jade to keep the session logged
-    jade.get_mut().unwrap().logout().unwrap();
 }

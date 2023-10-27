979c6edda2e3bc37d3aa0910b30b3f7df69b2324
Leonardo Comandini <leo@leocomandini.org>
2023-10-27 11:40:49 +0200

    wollet: tests: jade: add roundtrip test
    Candidate to replace some of exisiting tests.
    Use the same server, so we don't have to spin up multiple ones.
    Use the same jade signer, so we don't have to repeat common
    operations.
    Will allow us to do reissuance tests, and to pass the contract/
    asset info to calls following "issue asset".
    
    During development, it's possible to either comment out steps from
    roundtrip tests, or to use different tests or test files.

--- wollet/tests/test_jade/mod.rs
+++ wollet/tests/test_jade/mod.rs
@@ -2,3 +2,75 @@ pub mod burn;
 pub mod change_detection;
 pub mod init;
 pub mod issue;
+
+use bs_containers::testcontainers::clients::Cli;
+use jade::get_receive_address::Variant;
+use signer::Signer;
+
+use crate::{
+    test_jade::init::inner_jade_debug_initialization,
+    test_session::{
+        generate_signer, multisig_desc, setup, singlesig_desc, TestElectrumServer, TestWollet,
+    },
+    TEST_MNEMONIC,
+};
+
+fn roundtrip(
+    server: &TestElectrumServer,
+    signers: &[&Signer],
+    variant: Option<Variant>,
+    threshold: Option<usize>,
+) {
+    let desc_str = match signers.len() {
+        1 => singlesig_desc(signers[0], variant.unwrap()),
+        _ => multisig_desc(signers, threshold.unwrap()),
+    };
+    let mut wallet = TestWollet::new(&server.electrs.electrum_url, &desc_str);
+
+    wallet.fund_btc(server);
+
+    let node_address = server.node_getnewaddress();
+    wallet.send_btc(signers, None, Some((node_address, 10_000)));
+
+    let contract = "{\"entity\":{\"domain\":\"test.com\"},\"issuer_pubkey\":\"0337cceec0beea0232ebe14cba0197a9fbd45fcf2ec946749de920e71434c2b904\",\"name\":\"Test\",\"precision\":2,\"ticker\":\"TEST\",\"version\":0}";
+    let (asset, _token) = wallet.issueasset(signers, 1_000, 1, contract, None);
+    wallet.burnasset(signers, 10, &asset, None);
+}
+
+#[test]
+fn emul_roundtrip() {
+    let server = setup();
+    let docker = Cli::default();
+    let jade_init = inner_jade_debug_initialization(&docker, TEST_MNEMONIC.to_string());
+    let signers = &[&Signer::Jade(&jade_init.jade)];
+
+    // singlesig
+    roundtrip(&server, signers, Some(Variant::Wpkh), None);
+    roundtrip(&server, signers, Some(Variant::ShWpkh), None);
+    // multisig
+    let sw_signer = generate_signer();
+    let signers = &[&Signer::Jade(&jade_init.jade), &Signer::Software(sw_signer)];
+    roundtrip(&server, signers, None, Some(2));
+}
+
+#[cfg(feature = "serial")]
+mod serial {
+    use super::*;
+    use crate::test_jade::init::serial;
+
+    #[test]
+    #[ignore = "requires hardware jade: initialized with localtest network, connected via usb/serial"]
+    fn jade_roundtrip() {
+        let server = setup();
+        let jade = serial::unlock();
+        let signers = &[&Signer::Jade(&jade)];
+
+        roundtrip(&server, signers, Some(Variant::Wpkh), None);
+        roundtrip(&server, signers, Some(Variant::ShWpkh), None);
+        // multisig
+        // let sw_signer = generate_signer();
+        // let signers = &[&Signer::Jade(&jade), &Signer::Software(sw_signer)];
+        // FIXME: register multisig
+        // roundtrip(&server, signers, None, Some(2));
+    }
+}

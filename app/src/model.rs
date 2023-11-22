6b7133cac688867fce08bff6517a679a6c5aa011
Leonardo Comandini <leo@leocomandini.org>
2023-11-22 18:09:52 +0100

    app: model: SignerResponse make some fields Option

--- app/src/model.rs
+++ app/src/model.rs
@@ -74,9 +74,11 @@ pub struct UnloadSignerResponse {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct SignerResponse {
     pub name: String,
-    pub id: XpubIdentifier,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub id: Option<XpubIdentifier>,
     pub fingerprint: String,
-    pub xpub: ExtendedPubKey,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub xpub: Option<ExtendedPubKey>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -225,8 +227,8 @@ impl TryFrom<(String, &AnySigner)> for SignerResponse {
     fn try_from(name_and_signer: (String, &AnySigner)) -> Result<Self, Self::Error> {
         let (name, signer) = name_and_signer;
         let fingerprint = signer.fingerprint()?.to_string();
-        let xpub = signer.xpub()?;
-        let id = signer.identifier()?;
+        let xpub = Some(signer.xpub()?);
+        let id = Some(signer.identifier()?);
 
         Ok(Self {
             name,

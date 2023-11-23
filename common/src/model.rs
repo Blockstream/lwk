6272249eeddf707d498d61cb7605286c0b353e79
Leonardo Comandini <leo@leocomandini.org>
2023-11-23 18:02:05 +0100

    common: PsetDetails::fingerprint_has/missing

--- common/src/model.rs
+++ common/src/model.rs
@@ -1,6 +1,9 @@
-use elements_miniscript::elements::bitcoin::{bip32::KeySource, key::PublicKey};
+use elements_miniscript::elements::bitcoin::{
+    bip32::{Fingerprint, KeySource},
+    key::PublicKey,
+};
 use elements_miniscript::elements::AssetId;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 #[derive(Debug)]
 pub struct PsetBalance {
@@ -20,4 +23,28 @@ pub struct PsetDetails {
 
     /// For each input, existing or missing signatures
     pub sig_details: Vec<PsetSignatures>,
+}
+
+impl PsetDetails {
+    /// Set of fingerprints for which the PSET has a signature
+    pub fn fingerprints_has(&self) -> HashSet<Fingerprint> {
+        let mut r = HashSet::new();
+        for sigs in &self.sig_details {
+            for (_, (fingerprint, _)) in &sigs.has_signature {
+                r.insert(*fingerprint);
+            }
+        }
+        r
+    }
+
+    /// Set of fingerprints for which the PSET is missing a signature
+    pub fn fingerprints_missing(&self) -> HashSet<Fingerprint> {
+        let mut r = HashSet::new();
+        for sigs in &self.sig_details {
+            for (_, (fingerprint, _)) in &sigs.missing_signature {
+                r.insert(*fingerprint);
+            }
+        }
+        r
+    }
 }

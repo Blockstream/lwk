535fd2baacf25faa0af4baec7ca632391cfc82aa
Leonardo Comandini <leo@leocomandini.org>
2024-01-30 12:56:20 +0100

    jade: test: emulator: format

--- lwk_jade/tests/emulator.rs
+++ lwk_jade/tests/emulator.rs
@@ -1,30 +1,28 @@
 use base64::Engine;
 use elements::{
-    bitcoin::{self, bip32::Fingerprint},
+    bitcoin::{self, bip32::Fingerprint, bip32::Xpub, sign_message::signed_msg_hash},
+    hashes::Hash,
     pset::PartiallySignedTransaction,
     secp256k1_zkp::{ecdsa::Signature, Message, Secp256k1},
     Address, AddressParams,
 };
-use elements::{
-    bitcoin::{bip32::Xpub, sign_message::signed_msg_hash},
-    hashes::Hash,
-};
 use elements_miniscript::{
     confidential::Key, ConfidentialDescriptor, DefiniteDescriptorKey, DescriptorPublicKey,
 };
-use lwk_containers::testcontainers::clients::{self};
-use lwk_containers::{PinServer, PIN_SERVER_PORT};
-use lwk_jade::protocol::{
-    GetSignatureParams, GetXpubParams, SignMessageParams, UpdatePinserverParams,
+use lwk_containers::{
+    testcontainers::clients::{self},
+    PinServer, PIN_SERVER_PORT,
 };
 use lwk_jade::{
     get_receive_address::{GetReceiveAddressParams, SingleOrMulti, Variant},
-    protocol::{GetMasterBlindingKeyParams, JadeState, VersionInfoResult},
+    protocol::{
+        GetMasterBlindingKeyParams, GetSignatureParams, GetXpubParams, JadeState,
+        SignMessageParams, UpdatePinserverParams, VersionInfoResult,
+    },
     register_multisig::{JadeDescriptor, MultisigSigner, RegisterMultisigParams},
 };
 use lwk_test_util::jade::TestJadeEmulator;
 use std::{str::FromStr, time::UNIX_EPOCH, vec};
-use tempfile::tempdir;
 
 #[test]
 fn entropy() {
@@ -38,11 +36,10 @@ fn entropy() {
 #[test]
 fn debug_set_mnemonic() {
     let docker = clients::Cli::default();
+    let mut jade = TestJadeEmulator::new(&docker);
+    jade.set_debug_mnemonic(None);
 
-    let mut initialized_jade = TestJadeEmulator::new(&docker);
-    initialized_jade.set_debug_mnemonic(None);
-
-    let result = initialized_jade.jade.version_info().unwrap();
+    let result = jade.jade.version_info().unwrap();
     assert_eq!(result, mock_version_info());
 }
 
@@ -84,7 +81,7 @@ fn update_pinserver() {
     let docker = clients::Cli::default();
     let mut jade = TestJadeEmulator::new(&docker);
 
-    let tempdir = tempdir().unwrap();
+    let tempdir = tempfile::tempdir().unwrap();
     let pin_server = PinServer::new(&tempdir).unwrap();
     let pub_key: Vec<u8> = pin_server.pub_key().to_bytes();
     let container = docker.run(pin_server);
@@ -106,9 +103,9 @@ fn update_pinserver() {
 #[test]
 fn jade_initialization_with_pin_server() {
     let docker = clients::Cli::default();
+    let mut jade = TestJadeEmulator::new_with_pin(&docker);
 
-    let mut initialized_jade = TestJadeEmulator::new_with_pin(&docker);
-    let result = initialized_jade.jade.version_info().unwrap();
+    let result = jade.jade.version_info().unwrap();
     let mut expected = mock_version_info();
     expected.jade_has_pin = true;
     assert_eq!(result, expected);
@@ -117,21 +114,19 @@ fn jade_initialization_with_pin_server() {
 #[test]
 fn jade_init_logout_unlock() {
     let docker = clients::Cli::default();
+    let mut jade = TestJadeEmulator::new_with_pin(&docker);
 
-    let mut initialized_jade = TestJadeEmulator::new_with_pin(&docker);
-    let jade = &mut initialized_jade.jade;
-    assert!(jade.logout().unwrap());
-    jade.unlock().unwrap();
+    assert!(jade.jade.logout().unwrap());
+    jade.jade.unlock().unwrap();
 }
 
 #[test]
 fn jade_xpub() {
     let docker = clients::Cli::default();
+    let mut jade = TestJadeEmulator::new(&docker);
+    jade.set_debug_mnemonic(None);
 
-    let mut initialized_jade = TestJadeEmulator::new(&docker);
-    initialized_jade.set_debug_mnemonic(None);
-    let jade = &mut initialized_jade.jade;
-    let xpub_master = jade.get_master_xpub().unwrap();
+    let xpub_master = jade.jade.get_master_xpub().unwrap();
     assert_eq!(xpub_master.depth, 0);
     assert_eq!(xpub_master.network, bitcoin::Network::Testnet);
 
@@ -139,18 +134,21 @@ fn jade_xpub() {
         network: lwk_jade::Network::LocaltestLiquid,
         path: vec![0],
     };
-    let xpub = jade.get_xpub(params).unwrap();
+    let xpub = jade.jade.get_xpub(params).unwrap();
     assert_ne!(xpub_master, xpub);
     assert_eq!(xpub.depth, 1);
-    assert_eq!(jade.fingerprint().unwrap().as_bytes(), &[115, 197, 218, 10]);
+    assert_eq!(
+        jade.jade.fingerprint().unwrap().as_bytes(),
+        &[115, 197, 218, 10]
+    );
 }
 
 #[test]
 fn jade_receive_address() {
     let docker = clients::Cli::default();
+    let mut jade = TestJadeEmulator::new(&docker);
+    jade.set_debug_mnemonic(None);
 
-    let mut initialized_jade = TestJadeEmulator::new(&docker);
-    initialized_jade.set_debug_mnemonic(None);
     let params = GetReceiveAddressParams {
         network: lwk_jade::Network::LocaltestLiquid,
         address: SingleOrMulti::Single {
@@ -158,7 +156,7 @@ fn jade_receive_address() {
             path: vec![2147483697, 2147483648, 2147483648, 0, 143],
         },
     };
-    let result = initialized_jade.jade.get_receive_address(params).unwrap();
+    let result = jade.jade.get_receive_address(params).unwrap();
     let address = elements::Address::from_str(&result).unwrap();
     assert!(address.blinding_pubkey.is_some());
     assert_eq!(address.params, &AddressParams::ELEMENTS);
@@ -167,18 +165,16 @@ fn jade_receive_address() {
 #[test]
 fn jade_register_multisig() {
     let docker = clients::Cli::default();
+    let mut jade = TestJadeEmulator::new(&docker);
+    jade.set_debug_mnemonic(None);
 
-    let mut initialized_jade = TestJadeEmulator::new(&docker);
-    initialized_jade.set_debug_mnemonic(None);
-    let jade = &mut initialized_jade.jade;
-
-    let jade_master_xpub = jade.get_master_xpub().unwrap();
+    let jade_master_xpub = jade.jade.get_master_xpub().unwrap();
 
     let params = GetXpubParams {
         network: lwk_jade::Network::LocaltestLiquid,
         path: vec![0, 1],
     };
-    let jade_xpub = jade.get_xpub(params).unwrap();
+    let jade_xpub = jade.jade.get_xpub(params).unwrap();
 
     let signers = vec![
         MultisigSigner {
@@ -209,21 +205,18 @@ fn jade_register_multisig() {
             signers,
         },
     };
-    let result = jade.register_multisig(params).unwrap();
+    let result = jade.jade.register_multisig(params).unwrap();
     assert!(result)
 }
 
 #[test]
 fn jade_register_multisig_check_address() {
     let docker = clients::Cli::default();
-
-    let network = lwk_jade::Network::LocaltestLiquid;
-    let mut initialized_jade = TestJadeEmulator::new(&docker);
-    initialized_jade.set_debug_mnemonic(None);
-    let jade = &mut initialized_jade.jade;
+    let mut jade = TestJadeEmulator::new(&docker);
+    jade.set_debug_mnemonic(None);
 
     let multisig_name = "you_and_me".to_string();
-    let jade_master_xpub = jade.get_master_xpub().unwrap();
+    let jade_master_xpub = jade.jade.get_master_xpub().unwrap();
     let other_signer: Xpub= "tpubDDCNstnPhbdd4vwbw5UWK3vRQSF1WXQkvBHpNXpKJAkwFYjwu735EH3GVf53qwbWimzewDUv68MUmRDgYtQ1AU8FRCPkazfuaBp7LaEaohG".parse().unwrap();
     let slip77_key = "9c8e4f05c7711a98c838be228bcb84924d4570ca53f35fa1c793e58841d47023";
 
@@ -232,18 +225,21 @@ fn jade_register_multisig_check_address() {
 
     let desc: ConfidentialDescriptor<DescriptorPublicKey> = desc.parse().unwrap();
     let jade_desc: JadeDescriptor = (&desc).try_into().unwrap();
-    jade.register_multisig(RegisterMultisigParams {
-        network,
-        multisig_name: multisig_name.clone(),
-        descriptor: jade_desc,
-    })
-    .unwrap();
+    let network = lwk_jade::Network::LocaltestLiquid;
+    jade.jade
+        .register_multisig(RegisterMultisigParams {
+            network,
+            multisig_name: multisig_name.clone(),
+            descriptor: jade_desc,
+        })
+        .unwrap();
 
-    let result = jade.get_registered_multisigs().unwrap();
+    let result = jade.jade.get_registered_multisigs().unwrap();
     assert_eq!(result.len(), 1);
     result.get(&multisig_name).unwrap();
 
     let result = jade
+        .jade
         .get_receive_address(GetReceiveAddressParams {
             network,
             address: SingleOrMulti::Multi {
@@ -272,6 +268,10 @@ fn jade_register_multisig_check_address() {
 
 #[test]
 fn jade_sign_message() {
+    let docker = clients::Cli::default();
+    let mut jade = TestJadeEmulator::new(&docker);
+    jade.set_debug_mnemonic(None);
+
     // TODO create anti exfil commitments
     // The following are taken from jade tests, even though they may be random if we are not verifying.
     // To create the commitment jade use wally_ae_host_commit_from_bytes, rust-secp at the moment
@@ -280,19 +280,16 @@ fn jade_sign_message() {
         hex::decode("7b61fad27ce2d95abca09f76bd7226e50212a8542f3ca274ee546cec4bc5c3bb").unwrap();
     let ae_host_entropy =
         hex::decode("3f5540b9336af9bdd50a5b7f69fc2045a12e3b3e0740f7461902d882bf8a8820").unwrap();
-    let docker = clients::Cli::default();
     let message = "Hello world!";
-    let mut initialized_jade = TestJadeEmulator::new(&docker);
-    initialized_jade.set_debug_mnemonic(None);
     let params = SignMessageParams {
         message: message.to_string(),
         path: vec![0],
         ae_host_commitment,
     };
-    let _signer_commitment: Vec<u8> = initialized_jade.jade.sign_message(params).unwrap().to_vec();
+    let _signer_commitment: Vec<u8> = jade.jade.sign_message(params).unwrap().to_vec();
 
     let params = GetSignatureParams { ae_host_entropy };
-    let signature = initialized_jade.jade.get_signature_for_msg(params).unwrap();
+    let signature = jade.jade.get_signature_for_msg(params).unwrap();
     let signature_bytes = base64::engine::general_purpose::STANDARD
         .decode(signature)
         .unwrap();
@@ -301,7 +298,7 @@ fn jade_sign_message() {
         network: lwk_jade::Network::LocaltestLiquid,
         path: vec![0],
     };
-    let xpub = initialized_jade.jade.get_xpub(params).unwrap();
+    let xpub = jade.jade.get_xpub(params).unwrap();
     let msg_hash = signed_msg_hash(message);
     let message = Message::from_digest_slice(msg_hash.as_byte_array()).unwrap();
     let signature = Signature::from_compact(&signature_bytes).unwrap();
@@ -316,27 +313,26 @@ fn jade_sign_message() {
 #[test]
 fn jade_sign_liquid_tx() {
     let docker = clients::Cli::default();
-    let mut initialized_jade = TestJadeEmulator::new(&docker);
-    initialized_jade.set_debug_mnemonic(None);
+    let mut jade = TestJadeEmulator::new(&docker);
+    jade.set_debug_mnemonic(None);
+
     let pset_base64 = include_str!("../test_data/pset_to_be_signed.base64");
     let mut pset: PartiallySignedTransaction = pset_base64.parse().unwrap();
     assert_eq!(pset.outputs().len(), 3);
 
-    initialized_jade.jade.sign(&mut pset).unwrap();
+    jade.jade.sign(&mut pset).unwrap();
 }
 
 #[test]
 fn jade_get_master_blinding_key() {
     let docker = clients::Cli::default();
-    let mut initialized_jade = TestJadeEmulator::new(&docker);
-    initialized_jade.set_debug_mnemonic(None);
+    let mut jade = TestJadeEmulator::new(&docker);
+    jade.set_debug_mnemonic(None);
+
     let params = GetMasterBlindingKeyParams {
         only_if_silent: false,
     };
-    let result = initialized_jade
-        .jade
-        .get_master_blinding_key(params)
-        .unwrap();
+    let result = jade.jade.get_master_blinding_key(params).unwrap();
     assert_eq!(hex::encode(result), lwk_test_util::TEST_MNEMONIC_SLIP77);
 }
 

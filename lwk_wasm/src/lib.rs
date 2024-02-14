ae144e6c25f6016b800a122ebf16bd24dc26dbd1
Riccardo Casatta <riccardo@casatta.it>
2024-02-14 17:21:54 +0100

    wasm: balance return result, remove unwraps

--- lwk_wasm/src/lib.rs
+++ lwk_wasm/src/lib.rs
@@ -1,23 +1,29 @@
 use lwk_wollet::{ElementsNetwork, EsploraWasmClient, NoPersist, Wollet, WolletDescriptor};
-use std::{str::FromStr, sync::Arc};
+use std::{fmt::Debug, str::FromStr, sync::Arc};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub async fn balance() -> JsValue {
-    let descriptor = WolletDescriptor::from_str("ct(slip77(ab5824f4477b4ebb00a132adfd8eb0b7935cf24f6ac151add5d1913db374ce92),elwpkh([759db348/84'/1'/0']tpubDCRMaF33e44pcJj534LXVhFbHibPbJ5vuLhSSPFAw57kYURv4tzXFL6LSnd78bkjqdmE3USedkbpXJUPA1tdzKfuYSL7PianceqAhwL2UkA/<0;1>/*))#cch6wrnp").unwrap();
+pub async fn balance() -> Result<JsValue, String> {
+    let descriptor = WolletDescriptor::from_str("ct(slip77(ab5824f4477b4ebb00a132adfd8eb0b7935cf24f6ac151add5d1913db374ce92),elwpkh([759db348/84'/1'/0']tpubDCRMaF33e44pcJj534LXVhFbHibPbJ5vuLhSSPFAw57kYURv4tzXFL6LSnd78bkjqdmE3USedkbpXJUPA1tdzKfuYSL7PianceqAhwL2UkA/<0;1>/*))#cch6wrnp").map_err(to_debug)?;
     wasm_bindgen_test::console_log!("going to sync {}", descriptor);
 
     let network = ElementsNetwork::LiquidTestnet;
-    let mut wollet = Wollet::new(network, Arc::new(NoPersist {}), descriptor).unwrap();
+    let mut wollet = Wollet::new(network, Arc::new(NoPersist {}), descriptor).map_err(to_debug)?;
 
     let mut client = EsploraWasmClient::new("https://blockstream.info/liquidtestnet/api");
 
-    let update = client.full_scan(&wollet).await.unwrap().unwrap();
-    wollet.apply_update(update).unwrap();
-    let balance = wollet.balance().unwrap();
+    let update = client.full_scan(&wollet).await.map_err(to_debug)?;
+    if let Some(update) = update {
+        wollet.apply_update(update).map_err(to_debug)?;
+    }
+    let balance = wollet.balance().map_err(to_debug)?;
     wasm_bindgen_test::console_log!("balance {:?}", balance);
 
-    serde_wasm_bindgen::to_value(&balance).unwrap()
+    Ok(serde_wasm_bindgen::to_value(&balance).map_err(to_debug)?)
+}
+
+fn to_debug<D: Debug>(d: D) -> String {
+    format!("{d:?}")
 }
 
 mod tests {
@@ -30,7 +36,7 @@ mod tests {
 
     #[wasm_bindgen_test]
     async fn balance_test() {
-        let balance = crate::balance().await;
+        let balance = crate::balance().await.unwrap();
         let balance: HashMap<AssetId, u64> = serde_wasm_bindgen::from_value(balance).unwrap();
         let mut expected = HashMap::new();
         expected.insert(

47597520dede207a92a6c5fb29e683307dabc107
Riccardo Casatta <riccardo@casatta.it>
2024-02-27 11:32:27 +0100

    wasm: remove ad-hoc method usage from demo page

--- lwk_wasm/www/index.js
+++ lwk_wasm/www/index.js
@@ -8,12 +8,20 @@ scanButton.disabled = false;  // The button start disabled and it's enabled here
 scanButton.addEventListener("click", scanButtonPressed);
 
 async function scanButtonPressed(e) {
-    balance.innerText = "Loading... Open the browser dev tools to see network calls..."
-    scanButton.disabled = true;
-
-    let desc = descriptor.value
     try {
-        let val = await lwk.balance(desc);
+        balance.innerText = "Loading... Open the browser dev tools to see network calls..."
+        scanButton.disabled = true;
+        let desc = descriptor.value
+
+        // This is hacky...
+        let network = desc.includes("xpub") ? lwk.Network.mainnet() : lwk.Network.testnet()
+
+        let client = network.defaultEsploraClient()
+        let wolletDescriptor = new lwk.WolletDescriptor(desc)
+        let wollet = new lwk.Wollet(network, wolletDescriptor)
+        let update = await client.fullScan(wollet)
+        wollet.applyUpdate(update)
+        let val = wollet.balance();
         let balanceString = JSON.stringify(Object.fromEntries(val), null, 1)
             .replace("6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d", "L-BTC")
             .replace("144c654344aa716d6f3abcc1ca90e5641e4e2a7f633bc09fe3baf64585819a49", "tL-BTC")

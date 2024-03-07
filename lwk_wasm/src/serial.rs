f66e70758325eba7a58ba68191546884bb422957
Riccardo Casatta <riccardo@casatta.it>
2024-03-07 14:10:17 +0100

    wasm: add get_jade_serial fn

--- lwk_wasm/src/serial.rs
+++ lwk_wasm/src/serial.rs
@@ -1,6 +1,36 @@
 use lwk_jade::asyncr::Stream;
-use wasm_bindgen::JsValue;
+use wasm_bindgen::{JsCast, JsValue};
 use web_sys::js_sys::Uint8Array;
+
+use crate::Error;
+
+async fn get_jade_serial(_filter: bool) -> Result<web_sys::SerialPort, Error> {
+    let window =
+        web_sys::window().ok_or_else(|| Error::Generic("cannot get window".to_string()))?;
+    let navigator = window.navigator();
+    let serial = navigator.serial(); // TODO verify it exists, on firefox it doesn't
+
+    // TODO optionally filter "official jade" with SerialPortRequestOptions
+    let promise = serial.request_port();
+    let result = wasm_bindgen_futures::JsFuture::from(promise)
+        .await
+        .map_err(generic_err)?;
+
+    let serial: web_sys::SerialPort = result.dyn_into().map_err(generic_err)?;
+
+    let serial_options = web_sys::SerialOptions::new(115_200);
+
+    let promise = serial.open(&serial_options);
+    wasm_bindgen_futures::JsFuture::from(promise)
+        .await
+        .map_err(generic_err)?;
+
+    Ok(serial)
+}
+
+fn generic_err(val: JsValue) -> Error {
+    Error::Generic(format!("{:?}", val))
+}
 
 struct WebSerial {
     reader: web_sys::ReadableStreamDefaultReader,
@@ -12,8 +42,9 @@ impl Stream for WebSerial {
         let promise = self.reader.read();
         let result = wasm_bindgen_futures::JsFuture::from(promise)
             .await
-            .map_err(generic)?;
-        let value = web_sys::js_sys::Reflect::get(&result, &"value".into()).map_err(generic)?;
+            .map_err(generic_jade_err)?;
+        let value =
+            web_sys::js_sys::Reflect::get(&result, &"value".into()).map_err(generic_jade_err)?;
         let data = web_sys::js_sys::Uint8Array::new(&value).to_vec();
         buf.copy_from_slice(&data);
         Ok(data.len())
@@ -25,11 +56,11 @@ impl Stream for WebSerial {
         let promise = self.writer.write_with_chunk(&arr);
         wasm_bindgen_futures::JsFuture::from(promise)
             .await
-            .map_err(generic)?;
+            .map_err(generic_jade_err)?;
         Ok(())
     }
 }
 
-fn generic(val: JsValue) -> lwk_jade::Error {
+fn generic_jade_err(val: JsValue) -> lwk_jade::Error {
     lwk_jade::Error::Generic(format!("{:?}", val))
 }

03ed9e62eb69cfc37903d08e5998fa3ebffb2d2a
Riccardo Casatta <riccardo@casatta.it>
2024-03-18 12:08:29 +0100

    wasm: add other methods to TxBuilder

--- lwk_wasm/src/tx_builder.rs
+++ lwk_wasm/src/tx_builder.rs
@@ -1,13 +1,26 @@
 use std::fmt::Display;
 
+use lwk_wollet::UnvalidatedRecipient;
 use wasm_bindgen::prelude::*;
 
-use crate::Network;
+use crate::{Address, AssetId, Error, Network, Pset, Wollet};
 
 #[wasm_bindgen]
 #[derive(Debug)]
 pub struct TxBuilder {
     inner: lwk_wollet::TxBuilder,
+}
+
+impl From<lwk_wollet::TxBuilder> for TxBuilder {
+    fn from(value: lwk_wollet::TxBuilder) -> Self {
+        Self { inner: value }
+    }
+}
+
+impl From<TxBuilder> for lwk_wollet::TxBuilder {
+    fn from(value: TxBuilder) -> Self {
+        value.inner
+    }
 }
 
 #[wasm_bindgen]
@@ -20,10 +33,49 @@ impl TxBuilder {
         }
     }
 
+    /// Build the transaction
+    pub fn finish(self, wollet: &Wollet) -> Result<Pset, Error> {
+        Ok(self.inner.finish(wollet.as_ref())?.into())
+    }
+
+    /// Set the fee rate
     pub fn fee_rate(self, fee_rate: Option<f32>) -> TxBuilder {
-        TxBuilder {
-            inner: self.inner.fee_rate(fee_rate),
-        }
+        self.inner.fee_rate(fee_rate).into()
+    }
+
+    /// Add a recipient receiving L-BTC
+    ///
+    /// Errors if address's network is incompatible
+    pub fn add_lbtc_recipient(self, address: &Address, satoshi: u64) -> Result<TxBuilder, Error> {
+        let unvalidated_recipient = UnvalidatedRecipient::lbtc(address.to_string(), satoshi);
+        // TODO error variant should contain the TxBuilder so that caller can recover it
+        Ok(self.inner.add_recipient(&unvalidated_recipient)?.into())
+    }
+
+    /// Add a recipient receiving the given asset
+    ///
+    /// Errors if address's network is incompatible
+    pub fn add_recipient(
+        self,
+        address: &Address,
+        satoshi: u64,
+        asset: &AssetId,
+    ) -> Result<TxBuilder, Error> {
+        let unvalidated_recipient = UnvalidatedRecipient {
+            satoshi,
+            address: address.to_string(),
+            asset: asset.to_string(),
+        };
+        Ok(self.inner.add_recipient(&unvalidated_recipient)?.into())
+    }
+
+    /// Burn satoshi units of the given asset
+    pub fn add_burn(self, satoshi: u64, asset: &AssetId) -> TxBuilder {
+        let unvalidated_recipient = UnvalidatedRecipient::burn(asset.to_string(), satoshi);
+        self.inner
+            .add_recipient(&unvalidated_recipient)
+            .expect("recipient can't be invalid")
+            .into()
     }
 
     #[wasm_bindgen(js_name = toString)]
@@ -51,9 +103,15 @@ mod tests {
     #[wasm_bindgen_test]
     fn test_builder() {
         let network = Network::mainnet();
+        let policy = network.policy_asset();
+
         let mut builder = TxBuilder::new(network);
         assert_eq!(builder.to_string(), "TxBuilder { network: Liquid, addressees: [], fee_rate: 100.0, issuance_request: None }");
+
         builder = builder.fee_rate(Some(200.0));
         assert_eq!(builder.to_string(), "TxBuilder { network: Liquid, addressees: [], fee_rate: 200.0, issuance_request: None }");
+
+        builder = builder.add_burn(1000, &policy);
+        assert_eq!(builder.to_string(), "TxBuilder { network: Liquid, addressees: [Recipient { satoshi: 1000, script_pubkey: Script(OP_RETURN OP_0), blinding_pubkey: None, asset: 6f0279e9ed041c3d710a9f57d0c02928416460c4b722ae3457a11eec381c526d }], fee_rate: 200.0, issuance_request: None }");
     }
 }

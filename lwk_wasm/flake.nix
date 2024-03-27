2ed559a4fce146b50b723432fbfaa9b76584d225
Riccardo Casatta <riccardo@casatta.it>
2024-03-27 18:42:04 +0100

    nix: add flake to build wasm

---
+++ lwk_wasm/flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "wasm-pack setup";
+
+  inputs = {
+    nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
+    rust-overlay = { url = "github:oxalica/rust-overlay"; };
+  };
+
+  outputs = { nixpkgs, rust-overlay, ... }:
+    let system = "x86_64-linux";
+    in {
+      devShell.${system} = let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlay ];
+        };
+      in (({ pkgs, ... }:
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            cargo
+            cargo-watch
+            nodejs
+            wasm-pack
+            wasm-bindgen-cli
+            clang
+            (rust-bin.stable.latest.default.override {
+              extensions = [ "rust-src" ];
+              targets = [ "wasm32-unknown-unknown" ];
+            })
+          ];
+
+          shellHook = "";
+          CC_wasm32_unknown_unknown = "clang-17";
+          CFLAGS_wasm32_unknown_unknown="-I${pkgs.clang_17}/resource-root/include";
+
+        }) { pkgs = pkgs; });
+    };
+}

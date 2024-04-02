7cca8005905b79fc2a83bf5fc766ba55113c4315
Riccardo Casatta <riccardo@casatta.it>
2024-04-02 16:18:23 +0200

    bindings: name python wheel just lwk

---
+++ lwk_bindings/lwk/__init__.py
@@ -0,0 +1,5 @@
+from .lwk import *  # NOQA
+
+__doc__ = lwk.__doc__
+if hasattr(lwk, "__all__"):
+    __all__ = lwk.__all__
--- lwk_bindings/lwk_bindings/__init__.py
+++
@@ -1,5 +0,0 @@
-from .lwk_bindings import *  # NOQA
-
-__doc__ = lwk_bindings.__doc__
-if hasattr(lwk_bindings, "__all__"):
-    __all__ = lwk_bindings.__all__

2e074b99c865dffd2557590bb5c8fe627fe7b667
Byron Hambly <bhambly@blockstream.com>
2024-04-03 10:50:45 +0200

    ci: add windows runner

--- lwk_rpc_model/src/request.rs
+++ lwk_rpc_model/src/request.rs
@@ -122,7 +122,7 @@ pub struct Send {
     /// Recipient addressees
     pub addressees: Vec<UnvalidatedAddressee>,
 
-    /// Optiona fee rate in sat/vb
+    /// Optional fee rate in sat/vb
     pub fee_rate: Option<f32>,
 }
 

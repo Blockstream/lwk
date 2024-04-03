2870f3af6b2931d83d51cc61ce7b8d9439c27fdf
Riccardo Casatta <riccardo@casatta.it>
2024-04-03 08:53:14 +0200

    bindings: add todo for nested package in help

--- lwk_bindings/lwk/__init__.py
+++ lwk_bindings/lwk/__init__.py
@@ -3,3 +3,7 @@ from .lwk import *  # NOQA
 __doc__ = lwk.__doc__
 if hasattr(lwk, "__all__"):
     __all__ = lwk.__all__
+
+# TODO: `help(lwk)` shows nested packages like `lwk.lwk.lwk.Address` even though
+# they are available with just `lwk.Address`
+

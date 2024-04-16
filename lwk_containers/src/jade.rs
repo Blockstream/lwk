48798b7ce034ee675ffacacde83782645d38ac82
Riccardo Casatta <riccardo@casatta.it>
2024-04-16 10:15:46 +0200

    containers: remove unused Args from jade

--- lwk_containers/src/jade.rs
+++ lwk_containers/src/jade.rs
@@ -7,16 +7,6 @@ pub const EMULATOR_PORT: u16 = 30_121;
 #[derive(Debug, Default)]
 pub struct JadeEmulator;
 
-#[derive(Clone, Debug, Default)]
-struct Args;
-
-impl ImageArgs for Args {
-    fn into_iterator(self) -> Box<dyn Iterator<Item = String>> {
-        let args = ["bash".to_string()];
-        Box::new(args.into_iter())
-    }
-}
-
 impl Image for JadeEmulator {
     type Args = ();
 

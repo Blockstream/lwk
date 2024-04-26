51f30d0556b68d6747ab6d450bdb87bab794fe45
Riccardo Casatta <riccardo@casatta.it>
2024-04-26 13:19:01 +0200

    wasm: add method to ask addresses to jade

--- lwk_wasm/src/jade.rs
+++ lwk_wasm/src/jade.rs
@@ -3,6 +3,7 @@ use crate::{
     Error, Network,
 };
 use lwk_jade::asyncr;
+use lwk_jade::get_receive_address::{GetReceiveAddressParams, SingleOrMulti, Variant};
 use wasm_bindgen::prelude::*;
 
 /// Wrapper of [`asyncr::Jade`]
@@ -38,5 +39,86 @@ impl Jade {
         self.inner.unlock().await?;
         let xpub = self.inner.get_master_xpub().await?;
         Ok(xpub.to_string())
+    }
+
+    /// Return a single sig address with the given `variant` and `path` derivation
+    #[wasm_bindgen(js_name = getReceiveAddressSingle)]
+    pub async fn get_receive_address_single(
+        &self,
+        variant: SingleVariant,
+        path: Vec<u32>,
+    ) -> Result<String, Error> {
+        let network = self.inner.network();
+        self.inner.unlock().await?;
+        let xpub = self
+            .inner
+            .get_receive_address(GetReceiveAddressParams {
+                network,
+                address: SingleOrMulti::Single {
+                    variant: variant.into(),
+                    path,
+                },
+            })
+            .await?;
+        Ok(xpub.to_string())
+    }
+
+    /// Return a multisig address of a registered `multisig_name` wallet
+    ///
+    /// This method accept `path` and `path_n` in place of a single `Vec<Vec<u32>>` because the
+    /// latter is not supported by wasm_bindgen (and neither `(u32, Vec<u32>)`). `path` and `path_n`
+    /// are converted internally to a `Vec<Vec<u32>>` with the caveat all the paths are the same,
+    /// which is almost always the case.
+    #[wasm_bindgen(js_name = getReceiveAddressMulgti)]
+    pub async fn get_receive_address_multi(
+        &self,
+        multisig_name: String,
+        path: Vec<u32>,
+        path_n: u32,
+    ) -> Result<String, Error> {
+        let network = self.inner.network();
+        self.inner.unlock().await?;
+        let mut paths = vec![];
+        for _ in 0..path_n {
+            paths.push(path.clone());
+        }
+        let xpub = self
+            .inner
+            .get_receive_address(GetReceiveAddressParams {
+                network,
+                address: SingleOrMulti::Multi {
+                    multisig_name,
+                    paths,
+                },
+            })
+            .await?;
+        Ok(xpub.to_string())
+    }
+}
+
+#[wasm_bindgen]
+pub enum SingleVariant {
+    /// Witness Public Key Hash or native segwit
+    Wpkh,
+
+    /// Script Hash Witness Public Key Hash or wrapped segwit
+    ShWpkh,
+}
+
+impl From<SingleVariant> for Variant {
+    fn from(v: SingleVariant) -> Self {
+        match v {
+            SingleVariant::Wpkh => Variant::Wpkh,
+            SingleVariant::ShWpkh => Variant::ShWpkh,
+        }
+    }
+}
+
+impl From<Variant> for SingleVariant {
+    fn from(v: Variant) -> Self {
+        match v {
+            Variant::Wpkh => SingleVariant::Wpkh,
+            Variant::ShWpkh => SingleVariant::ShWpkh,
+        }
     }
 }

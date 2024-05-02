8049e1803984212a8740fe2a04028bdee0673406
Leonardo Comandini <leo@leocomandini.org>
2024-05-02 18:54:13 +0200

    ledger: wallet

--- lwk_ledger/src/lib.rs
+++ lwk_ledger/src/lib.rs
@@ -5,10 +5,14 @@ mod error;
 mod interpreter;
 mod merkle;
 mod transport;
+mod wallet;
+
 // Adapted from
 // https://github.com/LedgerHQ/app-bitcoin-new/tree/master/bitcoin_client_rs
 use crate::client::LiquidClient;
 use transport::TransportTcp;
+
+pub use wallet::{Version, WalletPolicy, WalletPubKey};
 
 pub fn new(port: u16) -> LiquidClient<TransportTcp> {
     LiquidClient::new(TransportTcp::new(port).expect("TODO"))

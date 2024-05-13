40a7b9c5c0e55e746674b7e68455e4ad1590963d
Leonardo Comandini <leo@leocomandini.org>
2024-05-13 18:16:29 +0200

    ledger: error: derive thiserror

--- lwk_ledger/src/error.rs
+++ lwk_ledger/src/error.rs
@@ -2,15 +2,23 @@ use core::fmt::Debug;
 
 use crate::{apdu::StatusWord, interpreter::InterpreterError};
 
-#[derive(Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum LiquidClientError<T: Debug> {
+    #[error("Client Error: {0}")]
     ClientError(String),
+    #[error("Invalid PSET")]
     InvalidPsbt,
+    #[error("Transport Error {0:?}")]
     Transport(T),
+    #[error("Interpreter Error {0:?}")]
     Interpreter(InterpreterError),
+    #[error("Device error, command {command}")]
     Device { command: u8, status: StatusWord },
+    #[error("Unexpected Result, command {command}")]
     UnexpectedResult { command: u8, data: Vec<u8> },
+    #[error("Invalid Response {0}")]
     InvalidResponse(String),
+    #[error("Unsupported App Version")]
     UnsupportedAppVersion,
 }
 

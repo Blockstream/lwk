39fba4d592fc1580ede9e493b1b717deae48c5a5
Leonardo Comandini <leo@leocomandini.org>
2024-06-05 11:40:55 +0200

    wollet: docs: example: sync wallet

--- lwk_wollet/src/lib.rs
+++ lwk_wollet/src/lib.rs
@@ -31,6 +31,30 @@
 //! # Ok(())
 //! # }
 //! ```
+//!
+//! ### Sync wallet
+//! ```rust,no_run
+//! # use lwk_wollet::{WolletDescriptor, Wollet, ElementsNetwork, ElectrumClient, ElectrumUrl,
+//! full_scan_with_electrum_client};
+//! # fn main() -> Result<(), lwk_wollet::Error> {
+//! # let desc = "ct(slip77(ab5824f4477b4ebb00a132adfd8eb0b7935cf24f6ac151add5d1913db374ce92),elwpkh([759db348/84'/1'/0']tpubDCRMaF33e44pcJj534LXVhFbHibPbJ5vuLhSSPFAw57kYURv4tzXFL6LSnd78bkjqdmE3USedkbpXJUPA1tdzKfuYSL7PianceqAhwL2UkA/<0;1>/*))#cch6wrnp";
+//! # let descriptor: WolletDescriptor = desc.parse()?;
+//! # let mut wollet = Wollet::without_persist(
+//! #    ElementsNetwork::LiquidTestnet,
+//! #    descriptor,
+//! # )?;
+//! // Use an Electrum server
+//! let electrum_url = ElectrumUrl::new("blockstream.info:465", true, true);
+//! let mut electrum_client = ElectrumClient::new(&electrum_url)?;
+//! full_scan_with_electrum_client(&mut wollet, &mut electrum_client)?;
+//!
+//! // Print a summary of the wallet transactions
+//! for tx in wollet.transactions()?.into_iter().rev() {
+//!     println!("TXID: {}, balance {:?}", tx.txid, tx.balance);
+//! }
+//! # Ok(())
+//! # }
+//! ```
 
 mod clients;
 mod config;

7310a008b59111ebd9e4174e9cfc68f50a0898b8
Riccardo Casatta <riccardo@casatta.it>
2024-07-05 11:14:08 +0200

    wollet: use block comment

--- lwk_wollet/src/clients/mod.rs
+++ lwk_wollet/src/clients/mod.rs
@@ -301,74 +301,76 @@ pub fn try_unblind(output: TxOut, descriptor: &WolletDescriptor) -> Result<TxOut
 
 #[cfg(test)]
 mod tests {
-    // use std::time::Instant;
+    /*
+    use std::time::Instant;
 
-    // use crate::{
-    //     clients::esplora_client::EsploraClient, BlockchainBackend, ElectrumClient, ElectrumUrl,
-    //     ElementsNetwork,
-    // };
+    use crate::{
+        clients::esplora_client::EsploraClient, BlockchainBackend, ElectrumClient, ElectrumUrl,
+        ElementsNetwork,
+    };
 
-    // #[test]
-    // #[ignore = "test with prod servers"]
-    // fn esplora_electrum_compare() {
-    //     lwk_test_util::init_logging();
+    #[test]
+    #[ignore = "test with prod servers"]
+    fn esplora_electrum_compare() {
+        lwk_test_util::init_logging();
 
-    //     let desc_str = lwk_test_util::TEST_DESCRIPTOR;
+        let desc_str = lwk_test_util::TEST_DESCRIPTOR;
 
-    //     let urls = [
-    //         "blockstream.info:465",
-    //         "https://blockstream.info/liquidtestnet/api",
-    //         "https://liquid.network/liquidtestnet/api",
-    //     ];
+        let urls = [
+            "blockstream.info:465",
+            "https://blockstream.info/liquidtestnet/api",
+            "https://liquid.network/liquidtestnet/api",
+        ];
 
-    //     let vec: Vec<Box<dyn BlockchainBackend>> = vec![
-    //         Box::new(ElectrumClient::new(&ElectrumUrl::new(urls[0], true, true)).unwrap()),
-    //         Box::new(EsploraClient::new(urls[1])),
-    //         Box::new(EsploraClient::new(urls[2])),
-    //     ];
+        let vec: Vec<Box<dyn BlockchainBackend>> = vec![
+            Box::new(ElectrumClient::new(&ElectrumUrl::new(urls[0], true, true)).unwrap()),
+            Box::new(EsploraClient::new(urls[1])),
+            Box::new(EsploraClient::new(urls[2])),
+        ];
 
-    //     let mut prec = None;
+        let mut prec = None;
 
-    //     for (i, mut bb) in vec.into_iter().enumerate() {
-    //         let tempdir = tempfile::tempdir().unwrap();
-    //         let desc = desc_str.parse().unwrap();
-    //         let mut wollet =
-    //             crate::Wollet::with_fs_persist(ElementsNetwork::LiquidTestnet, desc, &tempdir)
-    //                 .unwrap();
+        for (i, mut bb) in vec.into_iter().enumerate() {
+            let tempdir = tempfile::tempdir().unwrap();
+            let desc = desc_str.parse().unwrap();
+            let mut wollet =
+                crate::Wollet::with_fs_persist(ElementsNetwork::LiquidTestnet, desc, &tempdir)
+                    .unwrap();
 
-    //         let start = Instant::now();
-    //         let first_update = bb.full_scan(&wollet).unwrap().unwrap();
-    //         wollet.apply_update(first_update.clone()).unwrap();
+            let start = Instant::now();
+            let first_update = bb.full_scan(&wollet).unwrap().unwrap();
+            wollet.apply_update(first_update.clone()).unwrap();
 
-    //         let balance = wollet.balance().unwrap();
+            let balance = wollet.balance().unwrap();
 
-    //         if let Some(prec) = prec.as_ref() {
-    //             assert_eq!(&balance, prec);
-    //         }
-    //         prec = Some(balance);
+            if let Some(prec) = prec.as_ref() {
+                assert_eq!(&balance, prec);
+            }
+            prec = Some(balance);
 
-    //         tracing::info!(
-    //             "first run: {}: {:.2}s",
-    //             urls[i],
-    //             start.elapsed().as_secs_f64()
-    //         );
+            tracing::info!(
+                "first run: {}: {:.2}s",
+                urls[i],
+                start.elapsed().as_secs_f64()
+            );
 
-    //         let start = Instant::now();
-    //         let second_update = bb.full_scan(&wollet.state()).unwrap();
-    //         if let Some(update) = second_update {
-    //             // the tip could have been updated, checking no new tx have been found
-    //             assert!(update.new_txs.unblinds.is_empty());
-    //             assert!(update.scripts.is_empty());
-    //             assert!(update.timestamps.is_empty());
-    //             assert!(update.txid_height_new.is_empty());
-    //             assert!(update.txid_height_delete.is_empty());
-    //             assert_ne!(update.tip, first_update.tip);
-    //         }
-    //         tracing::info!(
-    //             "second run: {}: {:.2}s",
-    //             urls[i],
-    //             start.elapsed().as_secs_f64()
-    //         );
-    //     }
-    // }
+            let start = Instant::now();
+            let second_update = bb.full_scan(&wollet.state()).unwrap();
+            if let Some(update) = second_update {
+                // the tip could have been updated, checking no new tx have been found
+                assert!(update.new_txs.unblinds.is_empty());
+                assert!(update.scripts.is_empty());
+                assert!(update.timestamps.is_empty());
+                assert!(update.txid_height_new.is_empty());
+                assert!(update.txid_height_delete.is_empty());
+                assert_ne!(update.tip, first_update.tip);
+            }
+            tracing::info!(
+                "second run: {}: {:.2}s",
+                urls[i],
+                start.elapsed().as_secs_f64()
+            );
+        }
+    }
+    * */
 }

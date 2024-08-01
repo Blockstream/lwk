8b12cea7fbb16371257d38f78fdf5aec0d06759e
Leonardo Comandini <leo@leocomandini.org>
2024-08-01 14:45:20 +0200

    wollet: tests: ledger: multisig

--- lwk_wollet/tests/test_ledger/mod.rs
+++ lwk_wollet/tests/test_ledger/mod.rs
@@ -2,29 +2,32 @@ use lwk_common::{singlesig_desc, Singlesig};
 use lwk_containers::testcontainers::clients::Cli;
 use lwk_ledger::TestLedgerEmulator;
 use lwk_signer::AnySigner;
+use lwk_test_util::TestElectrumServer;
 
-use crate::test_wollet::{test_client_electrum, TestWollet};
+use crate::test_wollet::{generate_signer, multisig_desc, test_client_electrum, TestWollet};
 
-fn emul_roundtrip(singlesig_type: Singlesig) {
-    let server = lwk_test_util::setup();
-    let docker = Cli::default();
-    let ledger = TestLedgerEmulator::new(&docker);
-    // TODO
-    use elements_miniscript::bitcoin::hashes::Hash;
-    let xpub_identifier = elements_miniscript::bitcoin::XKeyIdentifier::all_zeros();
-    let signers = &[&AnySigner::Ledger(ledger.ledger, xpub_identifier)];
+use elements_miniscript::bitcoin::hashes::Hash;
 
-    let desc_str = singlesig_desc(
-        signers[0],
-        singlesig_type,
-        lwk_common::DescriptorBlindingKey::Slip77,
-        false,
-    )
-    .unwrap();
+fn roundtrip(
+    server: &TestElectrumServer,
+    signers: &[&AnySigner],
+    variant: Option<lwk_common::Singlesig>,
+    threshold: Option<usize>,
+) {
+    let desc_str = match signers.len() {
+        1 => singlesig_desc(
+            signers[0],
+            variant.unwrap(),
+            lwk_common::DescriptorBlindingKey::Slip77,
+            false,
+        )
+        .unwrap(),
+        _ => multisig_desc(signers, threshold.unwrap()),
+    };
     let client = test_client_electrum(&server.electrs.electrum_url);
     let mut wallet = TestWollet::new(client, &desc_str);
 
-    wallet.fund_btc(&server);
+    wallet.fund_btc(server);
 
     let node_address = server.elementsd_getnewaddress();
     wallet.send_btc(signers, None, Some((node_address, 10_000)));
@@ -34,12 +37,40 @@ fn emul_roundtrip(singlesig_type: Singlesig) {
     wallet.burnasset(signers, 5, &asset, None);
 }
 
+fn emul_roundtrip_singlesig(variant: Singlesig) {
+    let server = lwk_test_util::setup();
+    let docker = Cli::default();
+    let ledger = TestLedgerEmulator::new(&docker);
+    // TODO
+    let xpub_identifier = elements_miniscript::bitcoin::XKeyIdentifier::all_zeros();
+    let signers = &[&AnySigner::Ledger(ledger.ledger, xpub_identifier)];
+    roundtrip(&server, signers, Some(variant), None);
+}
+
+fn emul_roundtrip_multisig(threshold: usize) {
+    let server = lwk_test_util::setup();
+    let docker = Cli::default();
+    let ledger = TestLedgerEmulator::new(&docker);
+    let xpub_identifier = elements_miniscript::bitcoin::XKeyIdentifier::all_zeros();
+    let sw_signer = generate_signer();
+    let signers = &[
+        &AnySigner::Ledger(ledger.ledger, xpub_identifier),
+        &AnySigner::Software(sw_signer),
+    ];
+    roundtrip(&server, signers, None, Some(threshold));
+}
+
 #[test]
 fn emul_roundtrip_wpkh() {
-    emul_roundtrip(Singlesig::Wpkh);
+    emul_roundtrip_singlesig(Singlesig::Wpkh);
 }
 
 #[test]
 fn emul_roundtrip_shwpkh() {
-    emul_roundtrip(Singlesig::ShWpkh);
+    emul_roundtrip_singlesig(Singlesig::ShWpkh);
+}
+
+#[test]
+fn emul_roundtrip_2of2() {
+    emul_roundtrip_multisig(2);
 }

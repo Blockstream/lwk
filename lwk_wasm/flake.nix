637eca352cf3015f4f054abdfdca284b49471dbc
Riccardo Casatta <riccardo@casatta.it>
2024-10-14 14:36:49 +0200

    nix: remove deprecation

--- lwk_wasm/flake.nix
+++ lwk_wasm/flake.nix
@@ -18,7 +18,7 @@
         let
           pkgs = import nixpkgs {
             inherit system;
-            overlays = [ rust-overlay.overlay ];
+            overlays = [ rust-overlay.overlays.default ];
           };
         in
         (({ pkgs, ... }:

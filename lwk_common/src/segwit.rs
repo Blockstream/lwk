c25447d8dd3dba05fd13fb3bcd3684bfc60307d5
Leonardo Comandini <leo@leocomandini.org>
2024-10-16 09:10:59 +0200

    common: tests: not provably segwit

--- lwk_common/src/segwit.rs
+++ lwk_common/src/segwit.rs
@@ -18,3 +18,32 @@ pub fn is_provably_segwit(prevout_scriptpubkey: &Script, redeem_script: &Option<
     }
     false
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use elements_miniscript::elements::bitcoin::PublicKey;
+    use elements_miniscript::elements::PubkeyHash;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_provably_segwit() {
+        let op_return = Script::new_op_return(&[]);
+        assert!(!is_provably_segwit(&op_return, &None));
+
+        let s = "020202020202020202020202020202020202020202020202020202020202020202";
+        let pk = PublicKey::from_str(s).unwrap();
+        let p2pk = Script::new_p2pk(&pk);
+        assert!(!is_provably_segwit(&p2pk, &None));
+
+        let s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        let pkh = PubkeyHash::from_str(s).unwrap();
+        let p2pkh = Script::new_p2pkh(&pkh);
+        assert!(!is_provably_segwit(&p2pkh, &None));
+
+        let s = "522102ebc62c20f1e09e169a88745f60f6dac878c92db5c7ed78c6703d2d0426a01f942102c2d59d677122bc292048833003fd5cb19d27d32896b1d0feec654c291f7ede9e52ae";
+        let multi = Script::from_str(s).unwrap();
+        let p2sh_multi = multi.to_p2sh();
+        assert!(!is_provably_segwit(&p2sh_multi, &Some(multi)));
+    }
+}

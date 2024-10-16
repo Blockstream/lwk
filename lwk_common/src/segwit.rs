f0d660e84c110d0b4a0b2f93bfa5a115773da11f
Leonardo Comandini <leo@leocomandini.org>
2024-10-16 09:36:16 +0200

    common: tests: provably segwit

--- lwk_common/src/segwit.rs
+++ lwk_common/src/segwit.rs
@@ -23,7 +23,9 @@ pub fn is_provably_segwit(prevout_scriptpubkey: &Script, redeem_script: &Option<
 mod test {
     use super::*;
     use elements_miniscript::elements::bitcoin::PublicKey;
-    use elements_miniscript::elements::PubkeyHash;
+    use elements_miniscript::elements::schnorr::UntweakedPublicKey;
+    use elements_miniscript::elements::secp256k1_zkp::Secp256k1;
+    use elements_miniscript::elements::{PubkeyHash, WPubkeyHash};
     use std::str::FromStr;
 
     #[test]
@@ -43,7 +45,32 @@ mod test {
 
         let s = "522102ebc62c20f1e09e169a88745f60f6dac878c92db5c7ed78c6703d2d0426a01f942102c2d59d677122bc292048833003fd5cb19d27d32896b1d0feec654c291f7ede9e52ae";
         let multi = Script::from_str(s).unwrap();
+        let wsh = multi.wscript_hash();
         let p2sh_multi = multi.to_p2sh();
         assert!(!is_provably_segwit(&p2sh_multi, &Some(multi)));
+
+        let s = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb";
+        let wpkh = WPubkeyHash::from_str(s).unwrap();
+        let p2wpkh = Script::new_v0_wpkh(&wpkh);
+        assert!(is_provably_segwit(&p2wpkh, &None));
+
+        let p2wsh_multi = Script::new_v0_wsh(&wsh);
+        assert!(is_provably_segwit(&p2wsh_multi, &None));
+
+        let s = "0202020202020202020202020202020202020202020202020202020202020202";
+        let upk = UntweakedPublicKey::from_str(s).unwrap();
+        let secp = Secp256k1::new();
+        let p2tr = Script::new_v1_p2tr(&secp, upk, None);
+        assert!(is_provably_segwit(&p2tr, &None));
+
+        let p2sh_p2wpkh = p2wpkh.to_p2sh();
+        assert!(is_provably_segwit(&p2sh_p2wpkh, &Some(p2wpkh)));
+        assert!(!is_provably_segwit(&p2sh_p2wpkh, &None));
+        assert!(!is_provably_segwit(&p2sh_p2wpkh, &Some(Script::default())));
+
+        let p2sh_p2wsh = p2wsh_multi.to_p2sh();
+        assert!(is_provably_segwit(&p2sh_p2wsh, &Some(p2wsh_multi)));
+        assert!(!is_provably_segwit(&p2sh_p2wsh, &None));
+        assert!(!is_provably_segwit(&p2sh_p2wsh, &Some(Script::default())));
     }
 }

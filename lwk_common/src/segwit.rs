40006692011c4eda4bf29d2f576dc196c8a380f0
Leonardo Comandini <leo@leocomandini.org>
2024-10-17 11:35:31 +0200

    common: is_provably_segwit: improve doc

--- lwk_common/src/segwit.rs
+++ lwk_common/src/segwit.rs
@@ -1,16 +1,16 @@
 use elements_miniscript::elements::Script;
 
-/// Determine if a transaction input is provably segwit
-pub fn is_provably_segwit(prevout_scriptpubkey: &Script, redeem_script: &Option<Script>) -> bool {
-    if prevout_scriptpubkey.is_witness_program() {
+/// Whether a script pubkey is provably segwit
+///
+/// The redeem script is necessary for P2SH wrapped scripts.
+pub fn is_provably_segwit(scriptpubkey: &Script, redeem_script: &Option<Script>) -> bool {
+    if scriptpubkey.is_witness_program() {
         // Native Segwit
         return true;
     }
-    if prevout_scriptpubkey.is_p2sh() {
+    if scriptpubkey.is_p2sh() {
         if let Some(redeem_script) = redeem_script {
-            if redeem_script.is_witness_program()
-                && &redeem_script.to_p2sh() == prevout_scriptpubkey
-            {
+            if redeem_script.is_witness_program() && &redeem_script.to_p2sh() == scriptpubkey {
                 // Segwit Wrapped
                 return true;
             }

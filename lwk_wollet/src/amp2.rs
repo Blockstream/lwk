47431d59a23813cf9f826b5ef668c72d56565011
Leonardo Comandini <leo@leocomandini.org>
2024-12-20 12:10:39 +0100

    wollet: amp2: refactor interface

--- lwk_wollet/src/amp2.rs
+++ lwk_wollet/src/amp2.rs
@@ -13,6 +13,8 @@
 use crate::WolletDescriptor;
 use elements::bitcoin::bip32::{KeySource, Xpub};
 use elements::pset::PartiallySignedTransaction;
+use lwk_common::keyorigin_xpub_from_str;
+use serde::{Deserialize, Serialize};
 
 pub const FINGERPRINT_TESTNET: &str = "3d970d04";
 pub const XPUB_TESTNET: &str = "tpubDC347GyKEGtyd4swZDaEmBTcNuqseyX7E3Yw58FoeV1njuBcUmBMr5vBeBh6eRsxKYHeCAEkKj8J2p2dBQQJwB8n33uyAPrdgwFxLFTCXRd";
@@ -20,55 +22,95 @@ pub const DERIVATION_PATH_TESTNET: &str = "m/87h/1h/0h";
 pub const KEYORIGIN_XPUB_TESTNET: &str = "[3d970d04/87h/1h/0h]tpubDC347GyKEGtyd4swZDaEmBTcNuqseyX7E3Yw58FoeV1njuBcUmBMr5vBeBh6eRsxKYHeCAEkKj8J2p2dBQQJwB8n33uyAPrdgwFxLFTCXRd";
 pub const URL_TESTNET: &str = "https://amp2.testnet.blockstream.com/";
 
-/// AMP2 wallet
-pub struct Amp2Wallet {
-    descriptor: WolletDescriptor,
-    #[allow(unused)]
+/// Context for actions interacting with AMP2
+pub struct Amp2 {
+    server_key: String,
     url: String,
 }
 
-#[derive(serde::Serialize)]
+/// An AMP2 descriptor
+pub struct Amp2Descriptor {
+    inner: WolletDescriptor,
+}
+
+impl Amp2Descriptor {
+    fn new(inner: WolletDescriptor) -> Self {
+        Self { inner }
+    }
+
+    pub fn descriptor(&self) -> WolletDescriptor {
+        self.inner.clone()
+    }
+}
+
+#[derive(Serialize)]
 struct RegisterRequest {
     descriptor: String,
 }
 
-#[derive(serde::Deserialize)]
-struct RegisterResponse {
-    wid: String,
+#[derive(Serialize, Deserialize)]
+pub struct RegisterResponse {
+    pub wid: String,
 }
 
-#[derive(serde::Serialize)]
-struct SignRequest {
-    pset: PartiallySignedTransaction,
+#[derive(Serialize)]
+struct CosignRequest {
+    pset: String,
 }
 
-#[derive(serde::Deserialize)]
-struct SignResponse {
-    pset: PartiallySignedTransaction,
+#[derive(Deserialize)]
+struct CosignResponseInner {
+    pset: String,
 }
 
-impl Amp2Wallet {
-    /// AMP2 wallet for Liquid Testnet
-    pub fn new_testnet(user_keysource: KeySource, user_xpub: Xpub) -> Self {
-        // TODO: allow to set custom blinding key
-        let k = "slip77(0684e43749a3a3eb0362dcef8c66994bd51d33f8ce6b055126a800a626fc0d67)";
-        let amp2_xpub = KEYORIGIN_XPUB_TESTNET;
-        let user_xpub = format!("[{}/{}]{}", user_keysource.0, user_keysource.1, user_xpub);
-        let s = format!("ct({k},elwsh(multi(2,{amp2_xpub}/<0;1>/*,{user_xpub}/<0;1>/*)))");
-        let descriptor: WolletDescriptor = s.parse().expect("fixed descriptor structure");
+#[derive(Serialize, Deserialize)]
+pub struct CosignResponse {
+    pub pset: PartiallySignedTransaction,
+}
+
+impl TryFrom<CosignResponseInner> for CosignResponse {
+    type Error = crate::Error;
+
+    fn try_from(r: CosignResponseInner) -> Result<CosignResponse, Self::Error> {
+        let pset = r.pset.parse()?;
+        Ok(CosignResponse { pset })
+    }
+}
+
+impl Amp2 {
+    pub fn new_testnet() -> Self {
         Self {
-            descriptor,
+            server_key: KEYORIGIN_XPUB_TESTNET.into(),
             url: URL_TESTNET.into(),
         }
     }
 
-    pub fn descriptor(&self) -> WolletDescriptor {
-        self.descriptor.clone()
+    /// Get an AMP2 wallet descriptor from the keyorigin xpub string obtained from a signer
+    pub fn descriptor_from_str(
+        &self,
+        keyorigin_xpub: &str,
+    ) -> Result<Amp2Descriptor, crate::Error> {
+        let (keysource, xpub) = keyorigin_xpub_from_str(keyorigin_xpub)?;
+        let keysource = keysource.ok_or_else(|| crate::Error::MissingKeyorigin)?;
+        Ok(self.descriptor(keysource, xpub))
     }
 
-    pub async fn register(&self) -> Result<String, crate::Error> {
+    /// Get an AMP2 wallet descriptor
+    pub fn descriptor(&self, user_keysource: KeySource, user_xpub: Xpub) -> Amp2Descriptor {
+        // TODO; check Xpub network is consistent
+        // TODO: allow to set custom blinding key
+        let k = "slip77(0684e43749a3a3eb0362dcef8c66994bd51d33f8ce6b055126a800a626fc0d67)";
+        let amp2_xpub = &self.server_key;
+        let user_xpub = format!("[{}/{}]{}", user_keysource.0, user_keysource.1, user_xpub);
+        let s = format!("ct({k},elwsh(multi(2,{amp2_xpub}/<0;1>/*,{user_xpub}/<0;1>/*)))");
+        let descriptor: WolletDescriptor = s.parse().expect("fixed descriptor structure");
+        Amp2Descriptor::new(descriptor)
+    }
+
+    /// Register an AMP2 wallet with the AMP2 server
+    pub async fn register(&self, desc: Amp2Descriptor) -> Result<RegisterResponse, crate::Error> {
         let body = RegisterRequest {
-            descriptor: self.descriptor().to_string(),
+            descriptor: desc.descriptor().to_string(),
         };
         let j: RegisterResponse = reqwest::Client::new()
             .post(format!("{}/wallets/register", self.url))
@@ -77,22 +119,25 @@ impl Amp2Wallet {
             .await?
             .json()
             .await?;
-        Ok(j.wid)
+        Ok(j)
     }
 
-    pub async fn sign(
+    /// Ask the AMP2 server to cosign a PSET
+    pub async fn cosign(
         &self,
         pset: &PartiallySignedTransaction,
-    ) -> Result<PartiallySignedTransaction, crate::Error> {
-        let body = SignRequest { pset: pset.clone() };
-        let j: SignResponse = reqwest::Client::new()
+    ) -> Result<CosignResponse, crate::Error> {
+        let body = CosignRequest {
+            pset: pset.to_string(),
+        };
+        let j: CosignResponseInner = reqwest::Client::new()
             .post(format!("{}/wallets/sign", self.url))
             .json(&body)
             .send()
             .await?
             .json()
             .await?;
-        Ok(j.pset)
+        j.try_into()
     }
 }
 
@@ -123,19 +168,24 @@ mod test {
     #[test]
     fn amp2_desc() {
         let (keysource, xpub) = user_key();
+        let keyorigin_xpub = "[c67f5991/87'/1'/0']tpubDC4SUtWGWcMQPtwjgQQ4DYnFmAYhiKxw3f3KKCvMGT9sojZNvHsQ4rVW6nQeCPtk4rLAxGKeuAzMmBmH92X3HDgLho3nRWpvuJrpCmYgeQj";
         let expected = "ct(slip77(0684e43749a3a3eb0362dcef8c66994bd51d33f8ce6b055126a800a626fc0d67),elwsh(multi(2,[3d970d04/87'/1'/0']tpubDC347GyKEGtyd4swZDaEmBTcNuqseyX7E3Yw58FoeV1njuBcUmBMr5vBeBh6eRsxKYHeCAEkKj8J2p2dBQQJwB8n33uyAPrdgwFxLFTCXRd/<0;1>/*,[c67f5991/87'/1'/0']tpubDC4SUtWGWcMQPtwjgQQ4DYnFmAYhiKxw3f3KKCvMGT9sojZNvHsQ4rVW6nQeCPtk4rLAxGKeuAzMmBmH92X3HDgLho3nRWpvuJrpCmYgeQj/<0;1>/*)))#6j2fne4s";
-        let amp2 = Amp2Wallet::new_testnet(keysource, xpub);
-        assert_eq!(amp2.descriptor().to_string(), expected);
+
+        let amp2 = Amp2::new_testnet();
+        let desc = amp2.descriptor(keysource, xpub);
+        let desc1 = amp2.descriptor_from_str(keyorigin_xpub).unwrap();
+        assert_eq!(desc.descriptor().to_string(), expected);
+        assert_eq!(desc1.descriptor().to_string(), expected);
     }
 
     #[ignore]
     #[tokio::test]
     async fn amp2_network_calls() {
         let (keysource, xpub) = user_key();
-        let amp2 = Amp2Wallet::new_testnet(keysource, xpub);
-
-        let wid = amp2.register().await.unwrap();
-        assert!(!wid.is_empty());
+        let amp2 = Amp2::new_testnet();
+        let d = amp2.descriptor(keysource, xpub);
+        let r = amp2.register(d).await.unwrap();
+        assert!(!r.wid.is_empty());
 
         // TODO: test sign
     }

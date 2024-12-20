2781f5a45d04fca96e275be5b8a798112063be65
Leonardo Comandini <leo@leocomandini.org>
2024-12-20 08:11:14 +0100

    bindings: (new) clippy fixes

--- lwk_bindings/src/types/mod.rs
+++ lwk_bindings/src/types/mod.rs
@@ -1,8 +1,8 @@
 //! In this module there are wrapper of existing foreign simple types that:
 //! * can be exposed via the bindings
 //! * restrict the possible values of the builtin type, for example [`hex::Hex`] restrict the
-//! possible values of the builtin String type to only numbers and letter from 'a' to 'e'. Note the
-//! restriction is done at usage time, not at instantiation time.
+//!   possible values of the builtin String type to only numbers and letter from 'a' to 'e'. Note the
+//!   restriction is done at usage time, not at instantiation time.
 
 mod asset_id;
 mod hex;

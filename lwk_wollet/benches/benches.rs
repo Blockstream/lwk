2026c9ef18e01c7574c1056b8bae4aa8ea83c26d
Riccardo Casatta <riccardo@casatta.it>
2025-02-18 17:03:40 +0100

    wollet: bench Wollet::transactions() method

--- lwk_wollet/benches/benches.rs
+++ lwk_wollet/benches/benches.rs
@@ -1,17 +1,40 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use lwk_wollet::WolletDescriptor;
+use lwk_wollet::{ElementsNetwork, NoPersist, Update, Wollet, WolletDescriptor};
 
-criterion_group!(benches, descriptor);
+criterion_group!(benches, wollet);
 criterion_main!(benches);
 
-pub fn descriptor(c: &mut Criterion) {
-    c.benchmark_group("descriptor")
-        .bench_function("parse", |b: &mut criterion::Bencher<'_>| {
+pub fn wollet(c: &mut Criterion) {
+    c.benchmark_group("wollet")
+        .bench_function("descriptor parse", |b: &mut criterion::Bencher<'_>| {
             let desc_str = include_str!("../../lwk_common/test_data/pset_details/descriptor");
 
             b.iter(|| {
                 let d: WolletDescriptor = desc_str.parse().unwrap();
                 black_box(d);
             });
+        })
+        .bench_function("wallet transactions", |b: &mut criterion::Bencher<'_>| {
+            let wollet = test_wollet_with_many_transactions();
+            b.iter(|| {
+                let txs = wollet.transactions().unwrap();
+                black_box(txs);
+            });
         });
+}
+
+// duplicated from tests/test_wollet.rs
+pub fn test_wollet_with_many_transactions() -> Wollet {
+    let update = lwk_test_util::update_test_vector_many_transactions();
+    let descriptor = lwk_test_util::wollet_descriptor_many_transactions();
+    let descriptor: WolletDescriptor = descriptor.parse().unwrap();
+    let update = Update::deserialize(&update).unwrap();
+    let mut wollet = Wollet::new(
+        ElementsNetwork::LiquidTestnet,
+        std::sync::Arc::new(NoPersist {}),
+        descriptor,
+    )
+    .unwrap();
+    wollet.apply_update(update).unwrap();
+    wollet
 }

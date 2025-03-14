8fa3d627005a6905f141840d1baa0b745a197b9f
Riccardo Casatta <riccardo@casatta.it>
2025-03-14 09:24:30 +0100

    ledger: remove unused functions

--- lwk_ledger/src/asyncr/mod.rs
+++ lwk_ledger/src/asyncr/mod.rs
@@ -1,12 +1,6 @@
 pub use crate::psbt::PartialSignature;
 pub use client::{LiquidClient, Transport};
-use elements_miniscript::{
-    bitcoin::bip32::{DerivationPath, Fingerprint, Xpub},
-    slip77,
-};
 pub use transport_tcp::TransportTcp;
-
-use crate::Error;
 
 mod client;
 mod transport_tcp;
@@ -27,29 +21,5 @@ impl<T: Transport> Ledger<T> {
     pub fn from_transport(transport: T) -> Self {
         let client = LiquidClient::new(transport);
         Self { client }
-    }
-}
-
-/// TODO Should implement Signer, but here we are async. Make async signer and impl here and for jade
-impl Ledger<TransportTcp> {
-    pub async fn derive_xpub(&self, path: &DerivationPath) -> std::result::Result<Xpub, Error> {
-        let r = self
-            .client
-            .get_extended_pubkey(path, false)
-            .await
-            .expect("FIXME");
-        Ok(r)
-    }
-
-    pub async fn slip77_master_blinding_key(
-        &self,
-    ) -> std::result::Result<slip77::MasterBlindingKey, Error> {
-        let r = self.client.get_master_blinding_key().await.expect("FIXME");
-        Ok(r)
-    }
-
-    pub async fn fingerprint(&self) -> std::result::Result<Fingerprint, Error> {
-        let r = self.client.get_master_fingerprint().await.expect("FIXME");
-        Ok(r)
     }
 }

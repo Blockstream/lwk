dbbb5b665f8163b773f2e51bea4f169d87f2428b
tosynthegeek <oluwatosinojedapo@gmail.com>
2025-06-21 17:54:53 +0100

    wollet: updated custom error for transactions that requires too many inputs

--- lwk_wollet/src/error.rs
+++ lwk_wollet/src/error.rs
@@ -228,7 +228,7 @@ pub enum Error {
     #[error("Issuance amount greater than 21M*10^8 are not allowed")]
     IssuanceAmountGreaterThanBtcMax,
 
-    #[error("Transaction input {0} exceeds maximum allowed input count of 256")]
+    #[error("Number of transaction inputs ({0}) exceeds maximum allowed input count of 256")]
     TooManyInputs(usize),
 }
 

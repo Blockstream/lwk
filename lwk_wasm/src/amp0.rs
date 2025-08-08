48387eeb1704437047c2c6d647a5f7416ba45e7e
Riccardo Casatta <riccardo@casatta.it>
2025-08-08 18:08:25 +0200

    wasm: test amp0 methods

--- lwk_wasm/src/amp0.rs
+++ lwk_wasm/src/amp0.rs
@@ -22,6 +22,12 @@ impl Amp0 {
     }
 }
 
+#[wasm_bindgen]
+pub fn encrypt_credentials(username: &str, password: &str) -> String {
+    let (u, p) = lwk_wollet::amp0::encrypt_credentials(username, password);
+    format!("{}:{}", u, p)
+}
+
 #[cfg(all(test, target_arch = "wasm32"))]
 mod tests {
     use super::*;
@@ -31,11 +37,24 @@ mod tests {
     wasm_bindgen_test_configure!(run_in_browser);
 
     #[wasm_bindgen_test]
+    async fn test_amp0_just_connect() {
+        let _amp0 = Amp0::new_testnet().await.unwrap();
+    }
+
+    #[wasm_bindgen_test]
     async fn test_amp0_login() {
         let amp0 = Amp0::new_testnet().await.unwrap();
         let login_response = amp0.login("userleo456", "userleo456").await.unwrap();
 
         // TODO: this fails at the moment
         // assert!(login_response.contains("GA2zxWdhAYtREeYCVFTGRhHQmYMPAP"));
+
+        assert_eq!(login_response, ""); // just to see return value
+    }
+
+    #[wasm_bindgen_test]
+    async fn test_encrypt_credentials() {
+        let encrypted = super::encrypt_credentials("userleo456", "userleo456");
+        assert_eq!(encrypted, "a3c7f7de9a34bcab4554f7cedf6046e041eeb3a9211466d92ecaa9763ac3557b:f3ac0f33fe97412a39ebb5d11d111961a754ecbbbdf12c71342adb7022ae3a2d");
     }
 }

3f58e5f33d38d97bc72d2f3ecde135d6342deb52
Leonardo Comandini <leo@leocomandini.org>
2025-08-25 15:40:19 +0200

    wasm: expose amp0

--- lwk_wasm/src/lib.rs
+++ lwk_wasm/src/lib.rs
@@ -37,6 +37,8 @@ mod websocket;
 mod wollet;
 mod xpub;
 
+#[cfg(all(feature = "serial", target_arch = "wasm32"))]
+pub use amp0::Amp0Ext;
 pub use amp2::{Amp2, Amp2Descriptor};
 pub use bip::Bip;
 pub use blockdata::address::{Address, AddressResult};

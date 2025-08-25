5b6f825ad556fa75f9d15491adfbb47f9a68f232
Leonardo Comandini <leo@leocomandini.org>
2025-08-25 10:27:06 +0200

    wasm: tests: amp0: ext

--- lwk_wasm/src/amp0.rs
+++ lwk_wasm/src/amp0.rs
@@ -97,4 +97,13 @@ mod tests {
         let encrypted = super::encrypt_credentials("userleo456", "userleo456");
         assert_eq!(encrypted, "a3c7f7de9a34bcab4554f7cedf6046e041eeb3a9211466d92ecaa9763ac3557b:f3ac0f33fe97412a39ebb5d11d111961a754ecbbbdf12c71342adb7022ae3a2d");
     }
+
+    #[wasm_bindgen_test]
+    async fn test_amp0ext() {
+        let amp0 = Amp0Ext::new_mainnet("userleo456", "userleo456", "")
+            .await
+            .unwrap();
+        let last_index = amp0.last_index();
+        assert!(last_index > 20);
+    }
 }

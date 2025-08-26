407d28229caf6ef9ee55e9cf82588baa58fbb6b1
Leonardo Comandini <leo@leocomandini.org>
2025-08-26 09:27:31 +0200

    wasm: amp0: remove encrypt_credentials

--- lwk_wasm/src/amp0.rs
+++ lwk_wasm/src/amp0.rs
@@ -2,12 +2,6 @@ use crate::{AddressResult, Error, Network, WebSocketSerial};
 
 use wasm_bindgen::prelude::*;
 
-#[wasm_bindgen]
-pub fn encrypt_credentials(username: &str, password: &str) -> String {
-    let (u, p) = lwk_wollet::amp0::encrypt_credentials(username, password);
-    format!("{}:{}", u, p)
-}
-
 /// Wrapper of [`lwk_wollet::amp0::Amp0`]
 #[wasm_bindgen]
 pub struct Amp0 {
@@ -77,12 +71,6 @@ mod tests {
     use wasm_bindgen_test::*;
 
     wasm_bindgen_test_configure!(run_in_browser);
-
-    #[wasm_bindgen_test]
-    async fn test_encrypt_credentials() {
-        let encrypted = super::encrypt_credentials("userleo456", "userleo456");
-        assert_eq!(encrypted, "a3c7f7de9a34bcab4554f7cedf6046e041eeb3a9211466d92ecaa9763ac3557b:f3ac0f33fe97412a39ebb5d11d111961a754ecbbbdf12c71342adb7022ae3a2d");
-    }
 
     #[wasm_bindgen_test]
     async fn test_amp0ext() {

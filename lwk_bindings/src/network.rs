d6881ad31366e6f71f7914521eb60ee502923fa4
Leonardo Comandini <leo@leocomandini.org>
2025-08-27 10:08:02 +0200

    bindings: network: more conversion

--- lwk_bindings/src/network.rs
+++ lwk_bindings/src/network.rs
@@ -34,6 +34,18 @@ impl From<&Network> for lwk_wollet::ElementsNetwork {
     }
 }
 
+impl From<&Network> for lwk_common::Network {
+    fn from(value: &Network) -> Self {
+        match value.inner {
+            lwk_wollet::ElementsNetwork::Liquid => lwk_common::Network::Liquid,
+            lwk_wollet::ElementsNetwork::LiquidTestnet => lwk_common::Network::TestnetLiquid,
+            lwk_wollet::ElementsNetwork::ElementsRegtest { .. } => {
+                lwk_common::Network::LocaltestLiquid
+            }
+        }
+    }
+}
+
 #[uniffi::export]
 impl Network {
     #[uniffi::constructor]

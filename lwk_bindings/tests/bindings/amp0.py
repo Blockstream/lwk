2012ebeb8dfda67dfccb4f6d33850e9c9ace5431
Leonardo Comandini <leo@leocomandini.org>
2025-09-02 10:20:14 +0200

    bindings: tests: amp0: get address 1

--- lwk_bindings/tests/bindings/amp0.py
+++ lwk_bindings/tests/bindings/amp0.py
@@ -2,5 +2,5 @@ from lwk import *
 
 amp0 = Amp0(Network.mainnet(), "userleo456", "userleo456", "")
 assert amp0.last_index() > 20
-addr0 = str(amp0.address(0).address())
-assert addr0 == 'VJLHASNov3RqftSBp6QwmfsS5XEfNKnpfjWqYpz8im7rf4TGL1nXUGXLjVjhEX1fz4BXVV4Zyx79BAi6'
+addr0 = str(amp0.address(1).address())
+assert addr0 == 'VJL5Z9HekG84akxnGnvrtqB3t2Vg9mTP9k5k6FRNHuJhvzayGugagPZBt4omtWhg1VoeqwH2TDSsaW4w'

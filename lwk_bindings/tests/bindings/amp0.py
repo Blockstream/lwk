c7d1fb1f8ff2fd60c6ebe3e89a64f6188dfacefe
Leonardo Comandini <leo@leocomandini.org>
2025-09-04 09:15:38 +0200

    bindings: tests: amp0: clarify example

--- lwk_bindings/tests/bindings/amp0.py
+++ lwk_bindings/tests/bindings/amp0.py
@@ -1,6 +1,20 @@
 from lwk import *
 
-amp0 = Amp0(Network.testnet(), "userleo34567", "userleo34567", "")
-assert amp0.last_index() > 20
-addr0 = str(amp0.address(1).address())
-assert addr0 == 'vjTvpDMQx3EQ2bS3pmmy7RivU3QTjGyyJFJy1Y5basdKmwpW3R4YRdsxFNT7B3bPNmJkgKCRCS63AtjR'
+# AMP0 credentials
+username = "userleo34567"
+password = "userleo34567"
+# AMP ID
+amp_id = ""
+
+# BIP39 mnemonic corresponding to the AMP0 account
+mnemonic = Mnemonic("idea bind tissue wood february mention unable collect expand stuff snap stock")
+
+network = Network.testnet()
+url = "https://waterfalls.liquidwebwallet.org/liquidtestnet/api"
+
+# Create AMP0 object
+amp0 = Amp0(network, username, password, amp_id)
+
+# Get an address
+addr1 = str(amp0.address(1).address())
+assert addr1 == 'vjTvpDMQx3EQ2bS3pmmy7RivU3QTjGyyJFJy1Y5basdKmwpW3R4YRdsxFNT7B3bPNmJkgKCRCS63AtjR'

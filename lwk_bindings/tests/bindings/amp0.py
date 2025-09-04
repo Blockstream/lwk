9cf8ee2def3226fb3cebd15b18e8825045d68371
Leonardo Comandini <leo@leocomandini.org>
2025-09-04 08:29:55 +0200

    bindings: tests: amp0: use testnet

--- lwk_bindings/tests/bindings/amp0.py
+++ lwk_bindings/tests/bindings/amp0.py
@@ -1,6 +1,6 @@
 from lwk import *
 
-amp0 = Amp0(Network.mainnet(), "userleo456", "userleo456", "")
+amp0 = Amp0(Network.testnet(), "userleo34567", "userleo34567", "")
 assert amp0.last_index() > 20
 addr0 = str(amp0.address(1).address())
-assert addr0 == 'VJL5Z9HekG84akxnGnvrtqB3t2Vg9mTP9k5k6FRNHuJhvzayGugagPZBt4omtWhg1VoeqwH2TDSsaW4w'
+assert addr0 == 'vjTvpDMQx3EQ2bS3pmmy7RivU3QTjGyyJFJy1Y5basdKmwpW3R4YRdsxFNT7B3bPNmJkgKCRCS63AtjR'

51fd725df5b28db5f06508790c7b4c34dd2150fc
Riccardo Casatta <riccardo@casatta.it>
2025-09-18 11:35:59 +0200

    common: document Issuance

--- lwk_common/src/model.rs
+++ lwk_common/src/model.rs
@@ -43,6 +43,7 @@ pub struct PsetSignatures {
     pub missing_signature: Vec<(PublicKey, KeySource)>,
 }
 
+/// The details of an issuance or reissuance
 #[derive(Debug, Clone)]
 pub struct Issuance {
     asset: AssetId,
@@ -52,8 +53,11 @@ pub struct Issuance {
 }
 
 impl Issuance {
+    /// Create a new issuance or reissuance information from a input of a PSET
     pub fn new(input: &Input) -> Self {
-        // There are meaningless if inner is null
+        // TODO: return Result<Self, Error> and error if input.asset_issuance() is null
+
+        // These are meaningless if inner is null
         let (asset, token) = input.issuance_ids();
         let prev_output = OutPoint::new(input.previous_txid, input.previous_output_index);
         Self {
@@ -64,42 +68,52 @@ impl Issuance {
         }
     }
 
+    /// Return true if the issuance or reissuance is null
     pub fn is_null(&self) -> bool {
         self.inner.is_null()
     }
 
+    /// Return true if this is effectively an issuance
     pub fn is_issuance(&self) -> bool {
         !self.is_null() && self.inner.asset_blinding_nonce == ZERO_TWEAK
     }
 
+    /// Return true if this is effectively a reissuance
     pub fn is_reissuance(&self) -> bool {
         !self.is_null() && self.inner.asset_blinding_nonce != ZERO_TWEAK
     }
 
+    /// Return true if the issuance or reissuance is confidential
     pub fn is_confidential(&self) -> bool {
         self.inner.amount.is_confidential() || self.inner.inflation_keys.is_confidential()
     }
 
+    /// Return the amount of the asset in satoshis
     pub fn asset_satoshi(&self) -> Option<u64> {
         self.inner.amount.explicit()
     }
 
+    /// Return the amount of the reissuance token in satoshis
     pub fn token_satoshi(&self) -> Option<u64> {
         self.inner.inflation_keys.explicit()
     }
 
+    /// Return the asset id or None if it's a null issuance
     pub fn asset(&self) -> Option<AssetId> {
         (!self.is_null()).then_some(self.asset)
     }
 
+    /// Return the token id or None if it's a null issuance
     pub fn token(&self) -> Option<AssetId> {
         (!self.is_null()).then_some(self.token)
     }
 
+    /// Return the previous transaction id or None if it's a null issuance
     pub fn prev_txid(&self) -> Option<Txid> {
         (!self.is_null()).then_some(self.prev_output.txid)
     }
 
+    /// Return the previous output index or None if it's a null issuance
     pub fn prev_vout(&self) -> Option<u32> {
         (!self.is_null()).then_some(self.prev_output.vout)
     }

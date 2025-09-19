03799cb4a37295c687fd13a05bc3a19d80f13a94
Riccardo Casatta <riccardo@casatta.it>
2025-09-19 14:01:16 +0200

    wasm: document Precision

--- lwk_wasm/src/precision.rs
+++ lwk_wasm/src/precision.rs
@@ -13,7 +13,8 @@ pub struct Precision {
 
 #[wasm_bindgen]
 impl Precision {
-    /// Creates a Precision
+    /// Create a new Precision, useful to encode e decode values for assets with precision.
+    /// erroring if the given precision is greater than the allowed maximum (8)
     #[wasm_bindgen(constructor)]
     pub fn new(precision: u8) -> Result<Precision, Error> {
         Ok(Precision {
@@ -21,11 +22,17 @@ impl Precision {
         })
     }
 
+    /// Convert the given satoshi value to the formatted value according to our precision
+    ///
+    /// For example 100 satoshi with precision 2 is "1.00"
     #[wasm_bindgen(js_name = satsToString)]
     pub fn sats_to_string(&self, sats: i64) -> String {
         self.inner.sats_to_string(sats)
     }
 
+    /// Convert the given string with precision to satoshi units.
+    ///
+    /// For example the string "1.00" of an asset with precision 2 is 100 satoshi.
     #[wasm_bindgen(js_name = stringToSats)]
     pub fn string_to_sats(&self, sats: &str) -> Result<i64, Error> {
         Ok(self.inner.string_to_sats(sats)?)

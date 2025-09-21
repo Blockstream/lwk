54e351e0e649764f73846bcfee577d99fdd4fc93
Riccardo Casatta <riccardo@casatta.it>
2025-09-21 09:27:17 +0200

    bindings: document LwkTestEnv

--- lwk_bindings/src/test_env.rs
+++ lwk_bindings/src/test_env.rs
@@ -11,6 +11,7 @@ pub struct LwkTestEnv {
 
 #[uniffi::export]
 impl LwkTestEnv {
+    /// Creates a new test environment
     #[allow(clippy::new_without_default)]
     #[uniffi::constructor]
     pub fn new() -> LwkTestEnv {
@@ -19,28 +20,34 @@ impl LwkTestEnv {
         }
     }
 
+    /// Generate `blocks` blocks from the node
     pub fn generate(&self, blocks: u32) {
         self.inner.elementsd_generate(blocks);
     }
 
+    /// Get the height of the node
     pub fn height(&self) -> u64 {
         self.inner.elementsd_height()
     }
 
+    /// Send `satoshi` to `address` from the node
     pub fn send_to_address(&self, address: &Address, satoshi: u64, asset: Option<AssetId>) -> Txid {
         self.inner
             .elementsd_sendtoaddress(address.as_ref(), satoshi, asset.map(Into::into))
             .into()
     }
 
+    /// Issue `satoshi` of an asset from the node
     pub fn issue_asset(&self, satoshi: u64) -> AssetId {
         self.inner.elementsd_issueasset(satoshi).into()
     }
 
+    /// Get a new address from the node
     pub fn get_new_address(&self) -> Arc<Address> {
         Arc::new(self.inner.elementsd_getnewaddress().into())
     }
 
+    /// Get the Electrum URL of the test environment
     pub fn electrum_url(&self) -> String {
         self.inner.electrs.electrum_url.clone()
     }

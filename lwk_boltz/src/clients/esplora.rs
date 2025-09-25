11cb9c939f0ac5c5d7085afa19bcd5056794c932
Riccardo Casatta <riccardo@casatta.it>
2025-09-25 11:44:40 +0200

    boltz: mockup LiquidClient for esplora client

---
+++ lwk_boltz/src/clients/esplora.rs
@@ -0,0 +1,28 @@
+use async_trait::async_trait;
+use boltz_client::elements;
+use boltz_client::error::Error;
+use boltz_client::network::LiquidChain;
+
+struct EsploraClient(lwk_wollet::asyncr::EsploraClient);
+
+#[async_trait]
+impl boltz_client::network::LiquidClient for EsploraClient {
+    async fn get_address_utxo(
+        &self,
+        _address: &elements::Address,
+    ) -> Result<Option<(elements::OutPoint, elements::TxOut)>, Error> {
+        todo!()
+    }
+
+    async fn get_genesis_hash(&self) -> Result<elements::BlockHash, Error> {
+        todo!()
+    }
+
+    async fn broadcast_tx(&self, _signed_tx: &elements::Transaction) -> Result<String, Error> {
+        todo!()
+    }
+
+    fn network(&self) -> LiquidChain {
+        todo!()
+    }
+}
--- lwk_boltz/src/clients/mod.rs
+++ lwk_boltz/src/clients/mod.rs
@@ -1,1 +1,2 @@
 mod electrum;
+mod esplora;

e6a6cb7d58a6b9acf135d351f9f9cce83f632b1a
Riccardo Casatta <riccardo@casatta.it>
2025-09-25 12:23:53 +0200

    boltz: network conversion methods

--- lwk_boltz/src/lib.rs
+++ lwk_boltz/src/lib.rs
@@ -1,1 +1,22 @@
 pub mod clients;
+
+use boltz_client::network::LiquidChain;
+use lwk_wollet::ElementsNetwork;
+
+/// Convert an ElementsNetwork to a LiquidChain
+pub fn elements_network_to_liquid_chain(network: ElementsNetwork) -> LiquidChain {
+    match network {
+        ElementsNetwork::Liquid => LiquidChain::Liquid,
+        ElementsNetwork::LiquidTestnet => LiquidChain::LiquidTestnet,
+        ElementsNetwork::ElementsRegtest { .. } => LiquidChain::LiquidRegtest,
+    }
+}
+
+/// Convert a LiquidChain to an ElementsNetwork
+pub fn liquid_chain_to_elements_network(chain: LiquidChain) -> ElementsNetwork {
+    match chain {
+        LiquidChain::Liquid => ElementsNetwork::Liquid,
+        LiquidChain::LiquidTestnet => ElementsNetwork::LiquidTestnet,
+        LiquidChain::LiquidRegtest => ElementsNetwork::default_regtest(),
+    }
+}

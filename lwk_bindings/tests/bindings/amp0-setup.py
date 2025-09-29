3d9eb1223233d1a4507b9b47fecf3f41fb9c7912
Leonardo Comandini <leo@leocomandini.org>
2025-09-29 14:10:33 +0200

    bindings: tests: amp0-setup.py

---
+++ lwk_bindings/tests/bindings/amp0-setup.py
@@ -0,0 +1,39 @@
+from lwk import *
+
+# ANCHOR: amp0-setup
+# Create signer and watch only credentials
+username = "<username>";
+password = "<password>";
+mnemonic = "<mnemonic>";
+
+mnemonic = Mnemonic.from_random(12); # ANCHOR: ignore
+network = Network.testnet()
+signer = Signer(mnemonic, network)
+fingerprint = signer.keyorigin_xpub(Bip.new_bip84())[1:9] # ANCHOR: ignore
+username = f"user{fingerprint}" # ANCHOR: ignore
+password = f"pass{fingerprint}" # ANCHOR: ignore
+
+# Collect signer data
+signer_data = signer.amp0_signer_data();
+
+# Connect to AMP0
+amp0 = Amp0Connected(network, signer_data);
+
+# Obtain and sign the authentication challenge
+challenge = amp0.get_challenge();
+sig = signer.amp0_sign_challenge(challenge);
+
+# Login
+amp0 = amp0.login(sig);
+
+# Create a new AMP0 account
+pointer = amp0.next_account();
+account_xpub = signer.amp0_account_xpub(pointer);
+amp_id = amp0.create_amp0_account(pointer, account_xpub);
+
+# Create watch only entries
+amp0.create_watch_only(username, password);
+
+# Use watch only credentials to interact with AMP0
+amp0 = Amp0(network, username, password, amp_id);
+# ANCHOR_END: amp0-setup

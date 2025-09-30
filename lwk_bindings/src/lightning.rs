10ccdce755bf02b26ce49e01b5fdd62b4e7aa987
Riccardo Casatta <riccardo@casatta.it>
2025-09-30 09:35:35 +0200

    bindings: document LightningSession

--- lwk_bindings/src/lightning.rs
+++ lwk_bindings/src/lightning.rs
@@ -1,10 +1,14 @@
 use crate::{ElectrumClient, LwkError, Network};
 
+/// A session to pay and receive lightning payments.
+///
+/// Lightning payments are done via LBTC swaps using Boltz.
 pub struct LighthingSession {
     inner: lwk_boltz::LighthingSession,
 }
 
 impl LighthingSession {
+    /// Create the lightning session
     pub fn new(network: Network, client: ElectrumClient) -> Result<Self, LwkError> {
         let client = lwk_boltz::clients::ElectrumClient::from_client(
             client.into_inner().expect("todo"),

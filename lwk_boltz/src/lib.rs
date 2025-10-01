2184b25f2595915b3ebf46cfb170eb353615e9f1
Riccardo Casatta <riccardo@casatta.it>
2025-10-01 14:27:08 +0200

    boltz: move submarine to own file

--- lwk_boltz/src/lib.rs
+++ lwk_boltz/src/lib.rs
@@ -1,14 +1,13 @@
 pub mod clients;
 mod error;
+mod submarine;
 
-use std::str::FromStr;
 use std::sync::Arc;
 
 use boltz_client::boltz::BoltzApiClientV2;
 use boltz_client::boltz::BoltzWsApi;
 use boltz_client::boltz::BoltzWsConfig;
 use boltz_client::boltz::CreateReverseRequest;
-use boltz_client::boltz::CreateSubmarineRequest;
 use boltz_client::boltz::BOLTZ_MAINNET_URL_V2;
 use boltz_client::boltz::BOLTZ_REGTEST;
 use boltz_client::boltz::BOLTZ_TESTNET_URL_V2;
@@ -23,7 +22,6 @@ use boltz_client::swaps::SwapTransactionParams;
 use boltz_client::swaps::TransactionOptions;
 use boltz_client::util::secrets::Preimage;
 use boltz_client::util::sleep;
-use boltz_client::Bolt11Invoice;
 use boltz_client::Secp256k1;
 use boltz_client::{Keypair, PublicKey};
 use lwk_wollet::secp256k1::rand::thread_rng;
@@ -64,86 +62,6 @@ impl LightningSession {
 
     fn chain(&self) -> Chain {
         Chain::Liquid(self.liquid_chain)
-    }
-
-    pub async fn prepare_pay(
-        &self,
-        bolt11_invoice: &str,
-        // refund_address: elements::Address,
-    ) -> Result<PreparePayResponse, Error> {
-        let chain = self.chain();
-        let bolt11_parsed = Bolt11Invoice::from_str(bolt11_invoice)?;
-
-        let secp = Secp256k1::new();
-        let our_keys = Keypair::new(&secp, &mut thread_rng());
-        let refund_public_key = PublicKey {
-            inner: our_keys.public_key(),
-            compressed: true,
-        };
-
-        let create_swap_req = CreateSubmarineRequest {
-            from: chain.to_string(),
-            to: "BTC".to_string(),
-            invoice: bolt11_invoice.to_string(),
-            refund_public_key,
-            pair_hash: None,
-            referral_id: None,
-            webhook: None,
-        };
-
-        let create_swap_response = self.api.post_swap_req(&create_swap_req).await?;
-
-        let bolt11_amount = bolt11_parsed
-            .amount_milli_satoshis()
-            .ok_or(Error::InvoiceWithoutAmount(bolt11_invoice.to_string()))?
-            / 1000;
-        let fee = create_swap_response
-            .expected_amount
-            .checked_sub(bolt11_amount)
-            .ok_or(Error::ExpectedAmountLowerThanInvoice(
-                create_swap_response.expected_amount,
-                bolt11_invoice.to_string(),
-            ))?;
-
-        log::info!("Got Swap Response from Boltz server {create_swap_response:?}");
-
-        create_swap_response.validate(&bolt11_invoice, &refund_public_key, chain)?;
-        log::info!("VALIDATED RESPONSE!");
-
-        let swap_script =
-            SwapScript::submarine_from_swap_resp(chain, &create_swap_response, refund_public_key)?;
-        let swap_id = create_swap_response.id.clone();
-        log::info!("Created Swap Script id:{swap_id} swap_script:{swap_script:?}");
-
-        let mut rx = self.ws.updates();
-        self.ws.subscribe_swap(&swap_id).await?;
-        let update = rx.recv().await?;
-        match update.status.as_str() {
-            "invoice.set" => {
-                log::info!(
-                    "Send {} sats to {} address {}",
-                    create_swap_response.expected_amount,
-                    chain,
-                    create_swap_response.address
-                );
-                Ok(PreparePayResponse {
-                    swap_id,
-                    uri: create_swap_response.bip21,
-                    address: create_swap_response.address,
-                    amount: create_swap_response.expected_amount,
-                    fee,
-                    rx,
-                    swap_script,
-                    api: self.api.clone(),
-                    our_keys,
-                    bolt11_invoice: bolt11_invoice.to_string(),
-                })
-            }
-            _ => Err(Error::UnexpectedUpdate {
-                swap_id: update.id,
-                status: update.status,
-            }),
-        }
     }
 
     pub async fn invoice(
@@ -221,32 +139,6 @@ impl LightningSession {
     }
 }
 
-#[derive(Debug)]
-pub struct PreparePayResponse {
-    pub swap_id: String,
-
-    /// A liquidnetwork uri with the address to pay and the amount.
-    /// Note the amount is greater that what is specified in the bolt11 invoice because of fees
-    pub uri: String,
-
-    /// The address to pay to.
-    /// It is the same contained in the uri but provided for convenience.
-    pub address: String,
-
-    /// The amount to pay.
-    /// It is the same contained in the uri but provided for convenience.
-    pub amount: u64,
-
-    /// Fee in satoshi, it's equal to the `amount` less the bolt11 amount
-    pub fee: u64,
-
-    rx: tokio::sync::broadcast::Receiver<boltz_client::boltz::SwapStatus>,
-    bolt11_invoice: String,
-    swap_script: SwapScript,
-    api: Arc<BoltzApiClientV2>,
-    our_keys: Keypair,
-}
-
 pub struct InvoiceResponse {
     pub swap_id: String,
     /// The invoice to show to the payer, the invoice amount will be exactly like the amount parameter,
@@ -266,78 +158,6 @@ pub struct InvoiceResponse {
     preimage: Preimage,
     claim_address: String,
     chain_client: Arc<ChainClient>,
-}
-
-impl PreparePayResponse {
-    pub async fn complete_pay(mut self) -> Result<bool, Error> {
-        loop {
-            let update = self.rx.recv().await.unwrap();
-            log::info!("Got Update from server: {}", update.status);
-            log::debug!("Update: {update:?}");
-            match update.status.as_str() {
-                "transaction.mempool" => {}
-                "transaction.confirmed" => {}
-                "invoice.pending" => {}
-                "invoice.paid" => {}
-                "transaction.claim.pending" => {
-                    let response = self
-                        .swap_script
-                        .submarine_cooperative_claim(
-                            &self.swap_id,
-                            &self.our_keys,
-                            &self.bolt11_invoice,
-                            &self.api,
-                        )
-                        .await
-                        .unwrap();
-                    log::debug!("Received claim tx details : {response:?}");
-                }
-
-                "transaction.claimed" => {
-                    break Ok(true);
-                }
-
-                // This means the funding transaction was rejected by Boltz for whatever reason, and we need to get
-                // the funds back via refund.
-                "transaction.lockupFailed" | "invoice.failedToPay" => {
-                    todo!();
-                    // sleep(WAIT_TIME).await;
-                    // let tx = swap_script
-                    //     .construct_refund(SwapTransactionParams {
-                    //         keys: our_keys,
-                    //         output_address: refund_address,
-                    //         fee: Fee::Absolute(1000),
-                    //         swap_id: swap_id.clone(),
-                    //         chain_client,
-                    //         boltz_client: &boltz_api_v2,
-                    //         options: None,
-                    //     })
-                    //     .await
-                    //     .unwrap();
-
-                    // let txid = chain_client.broadcast_tx(&tx).await.unwrap();
-                    // log::info!("Cooperative Refund Successfully broadcasted: {txid}");
-
-                    // Non cooperative refund requires expired swap
-                    /*log::info!("Cooperative refund failed. {:?}", e);
-                    log::info!("Attempting Non-cooperative refund.");
-
-                    let tx = swap_tx
-                        .sign_refund(&our_keys, Fee::Absolute(1000), None)
-                        .await
-                        .unwrap();
-                    let txid = swap_tx
-                        .broadcast(&tx, bitcoin_client)
-                        .await
-                        .unwrap();
-                    log::info!("Non-cooperative Refund Successfully broadcasted: {}", txid);*/
-                }
-                _ => {
-                    panic!("Unexpected update: {}", update.status);
-                }
-            };
-        }
-    }
 }
 
 impl InvoiceResponse {

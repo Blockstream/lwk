47c464a052f3bb54ae6630d03ca1f2ba6c66d5e7
Riccardo Casatta <riccardo@casatta.it>
2025-10-01 11:11:53 +0200

    bindings: add some comment for the lightning test

--- lwk_bindings/tests/bindings/lightning.py
+++ lwk_bindings/tests/bindings/lightning.py
@@ -14,3 +14,11 @@ invoice_response = lightning_session.invoice(1000, "ciao", str(claim_address))
 bolt11_invoice = invoice_response.bolt11_invoice()
 print(bolt11_invoice)
 assert bolt11_invoice.startswith("lnbc1")
+
+# invoice_response.complete_pay() # pay the invoice externally
+
+
+## Pay invoice
+# prepare_pay_response = lightning_session.prepare_pay(invoice)
+# uri = prepare_pay_response.uri() # pay the liquid uri
+# prepare_pay_response.complete_pay()

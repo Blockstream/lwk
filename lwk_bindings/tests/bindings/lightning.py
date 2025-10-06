0f878088f4c8fbfe58225ab16c40dffb7a8123d9
Riccardo Casatta <riccardo@casatta.it>
2025-10-06 21:54:32 +0200

    bindings: show MagicRoutingHint handling in python test

--- lwk_bindings/tests/bindings/lightning.py
+++ lwk_bindings/tests/bindings/lightning.py
@@ -32,8 +32,19 @@ assert bolt11_invoice.startswith("lnbc1")
 
 # invoice_response.complete_pay() # pay the invoice externally
 
+## Pay invoice with MRH
+## just an example of how to handle the MagicRoutingHint error by reusing our invoice
+## in the real world any invoice generated from a Boltz-enabled wallet will contain a MRH
+try:
+    refund_address = wollet.address(3).address()
+    prepare_pay_response = lightning_session.prepare_pay(bolt11_invoice, refund_address)
+except LwkError.MagicRoutingHint as e:
+    # Handle the specific MagicRoutingHint error
+    print(f"Magic routing hint detected!")
+    print(f"You can pay directly {e.uri}")
+except Exception as e:
+    # Handle any other unexpected errors
+    print(f"Unexpected error: {e}")
 
-## Pay invoice
-# prepare_pay_response = lightning_session.prepare_pay(invoice)
 # uri = prepare_pay_response.uri() # pay the liquid uri
 # prepare_pay_response.complete_pay()

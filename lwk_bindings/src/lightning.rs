1f7bd1c7132fa8caa553fb9179f1f5fe0f49ef2a
Riccardo Casatta <riccardo@casatta.it>
2025-10-09 23:17:02 +0200

    boltz/bindings: expose swap id

--- lwk_bindings/src/lightning.rs
+++ lwk_bindings/src/lightning.rs
@@ -197,6 +197,17 @@ impl PreparePayResponse {
         Ok(response.complete_pay()?)
     }
 
+    pub fn swap_id(&self) -> Result<String, LwkError> {
+        Ok(self
+            .inner
+            .lock()?
+            .as_ref()
+            .ok_or_else(|| LwkError::Generic {
+                msg: "This PreparePayResponse already called complete_pay or errored".to_string(),
+            })?
+            .swap_id())
+    }
+
     /// Serialize the prepare pay response data to a json string
     ///
     /// This can be used to restore the prepare pay response after a crash
@@ -255,6 +266,17 @@ impl InvoiceResponse {
             })?
             .bolt11_invoice();
         Ok(Bolt11Invoice::from(bolt11_invoice))
+    }
+
+    pub fn swap_id(&self) -> Result<String, LwkError> {
+        Ok(self
+            .inner
+            .lock()?
+            .as_ref()
+            .ok_or_else(|| LwkError::Generic {
+                msg: "This InvoiceResponse already called complete_pay or errored".to_string(),
+            })?
+            .swap_id())
     }
 
     /// Serialize the prepare pay response data to a json string

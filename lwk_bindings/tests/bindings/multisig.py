4be3c1d93d182c6b287fca8c9ab40fa6cb86eb46
Leonardo Comandini <leo@leocomandini.org>
2025-10-15 10:30:54 +0200

    bindings: tests: snippets: py: multisig: setup

--- lwk_bindings/tests/bindings/multisig.py
+++ lwk_bindings/tests/bindings/multisig.py
@@ -1,17 +1,34 @@
 from lwk import *
 
+node = LwkTestEnv() # launch electrs and elementsd
 
-network = Network.regtest_default()
+# ANCHOR: multisig-setup
+network = Network.testnet()
+network = Network.regtest_default() # ANCHOR: ignore
+# Derivation for multisig
+bip = Bip.new_bip87()
 
-mnemonic1 = Mnemonic("abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about")
-mnemonic2 = Mnemonic("tissue mix draw siren diesel escape menu misery tube yellow zoo measure")
+# Alice creates their signer and gets the xpub
+signer_a = Signer.random(network)
+xpub_a = signer_a.keyorigin_xpub(bip);
 
-signer1 = Signer(mnemonic1, network)
-signer2 = Signer(mnemonic2, network)
-xpub1 = signer1.keyorigin_xpub(Bip.new_bip87())
-xpub2 = signer2.keyorigin_xpub(Bip.new_bip87())
+# Bob creates their signer and gets the xpub
+signer_b = Signer.random(network)
+xpub_b = signer_b.keyorigin_xpub(bip);
 
-desc = f"ct(elip151,elwsh(multi(2,{xpub1}/<0;1>/*,{xpub2}/<0;1>/*)))"
-desc = WolletDescriptor(desc)
+# Carol, who acts as a coordinator, creates their signer and gets the xpub
+signer_c = Signer.random(network)
+xpub_c = signer_c.keyorigin_xpub(bip);
 
-assert str(desc) == "ct(f68ce538e5774697e9b8e45528597ccb93f40b0a67e6853a954fc2db4afc8e9b,elwsh(multi(2,[73c5da0a/87'/1'/0']tpubDCChhoz5Qdrkn7Z7KXawq6Ad6r3A4MUkCoVTqeWxfTkA6bHNJ3CHUEtALQdkNeixNz4446PcAmw4WKcj3mV2vb29H7sg9EPzbyCU1y2merw/<0;1>/*,[0f04356d/87'/1'/0']tpubDD2NZt5nWoiA5uuWNNWw8eKiexd8EFs8kwChV5DrzkXQ3ZoNu3SZdAmD82z78oYGmt4aihPi5rPfEFNZGs7C9WiAshoD7UEtL5R79Jo76TA/<0;1>/*)))#d8apxcd6"
+# Carol generates a random SLIP77 descriptor blinding key
+import os
+slip77_rand_key = os.urandom(32).hex()
+desc_blinding_key = f"slip77({slip77_rand_key})"
+
+# Carol uses the collected xpubs and the descriptor blinding key to create
+# the 2of3 descriptor
+threshold = 2;
+desc = f"ct({desc_blinding_key},elwsh(multi({threshold},{xpub_a}/<0;1>/*,{xpub_b}/<0;1>/*,{xpub_c}/<0;1>/*)))"
+# Validate the descriptor string
+wd = WolletDescriptor(desc)
+# ANCHOR_END: multisig-setup

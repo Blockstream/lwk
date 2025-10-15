85503248c693e4a70e79c8bea77dbdb212ca430a
Riccardo Casatta <riccardo@casatta.it>
2025-10-15 14:25:28 +0200

    bindings: update python example with webhook

--- lwk_bindings/tests/bindings/lightning.py
+++ lwk_bindings/tests/bindings/lightning.py
@@ -26,7 +26,7 @@ logger = MyLogger()
 mnemonic_lightning = signer.derive_bip85_mnemonic(0, 12) # for security reasons using a different mnemonic for the lightning session
 lightning_session = LightningSession(network, client, 10, logger, mnemonic_lightning)
 
-invoice_response = lightning_session.invoice(1000, "ciao", claim_address)
+invoice_response = lightning_session.invoice(1000, "ciao", claim_address, None) # optionally accept a WebHook("https://example.com/webhook")
 bolt11_invoice_obj = invoice_response.bolt11_invoice()
 bolt11_invoice = str(bolt11_invoice_obj)
 print(bolt11_invoice)
@@ -39,7 +39,7 @@ assert bolt11_invoice.startswith("lnbc1")
 ## in the real world any invoice generated from a Boltz-enabled wallet will contain a MRH
 try:
     refund_address = wollet.address(3).address()
-    prepare_pay_response = lightning_session.prepare_pay(bolt11_invoice_obj, refund_address)
+    prepare_pay_response = lightning_session.prepare_pay(bolt11_invoice_obj, refund_address, None) # optionally accept a WebHook("https://example.com/webhook")
 except LwkError.MagicRoutingHint as e:
     # Handle the specific MagicRoutingHint error
     print(f"Magic routing hint detected!")

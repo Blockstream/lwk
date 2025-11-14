947a0cb906354ccdfad5dda64326bcfd0c27f2ef
Leonardo Comandini <leo@leocomandini.org>
2025-11-14 12:15:14 +0100

    context: env: waterfalls

--- context/env.sh
+++ context/env.sh
@@ -2,6 +2,7 @@
 export ELECTRS_LIQUID_EXEC="$PWD/bin/electrs_linux_esplora_a33e97e1a1fc63fa9c20a116bb92579bbf43b254_liquid"
 export ELEMENTSD_EXEC="$PWD/bin/elements-23.2.4/bin/elementsd"
 export BITCOIND_EXEC="$PWD/bin/bitcoin-26.0/bin/bitcoind"
+export WATERFALLS_EXEC="$PWD/bin/waterfalls_b8818e1"
 export JADE_EMULATOR_IMAGE_NAME=xenoky/local-jade-emulator
 export JADE_EMULATOR_IMAGE_VERSION="1.0.27"
 export PIN_SERVER_IMAGE_NAME=tulipan81/blind_pin_server

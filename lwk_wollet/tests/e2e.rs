1a6357c7ad5067e2d17d2aedc168f9a82a4fc7b3
Leonardo Comandini <leo@leocomandini.org>
2025-11-27 10:42:22 +0100

    wollet: tests: e2e: amp0

--- lwk_wollet/tests/e2e.rs
+++ lwk_wollet/tests/e2e.rs
@@ -1,3 +1,4 @@
+mod amp0;
 mod test_jade;
 mod test_ledger;
 mod test_wollet;
@@ -3591,7 +3592,7 @@ fn test_blinding_nonces() {
 
     // Amp0Pset::new checks that blinding nonces and PSET are consistent
     let fake_blinding_nonces = vec![String::new(); blinding_nonces.len()];
-    let res = crate::amp0::Amp0Pset::new(pset, fake_blinding_nonces);
+    let res = lwk_wollet::amp0::Amp0Pset::new(pset, fake_blinding_nonces);
     assert!(res.is_err());
 }
 
@@ -3769,134 +3770,6 @@ fn basics() -> Result<(), Box<dyn std::error::Error>> {
     // (optional)
     wollet.apply_transaction(tx)?;
     // ANCHOR_END: broadcast
-
-    Ok(())
-}
-
-#[test]
-#[allow(unused)]
-#[cfg(feature = "amp0")]
-#[ignore = "requires network calls"]
-fn test_amp0_setup() -> Result<(), Box<dyn std::error::Error>> {
-    // ANCHOR: amp0-setup
-    use lwk_common::{Amp0Signer, Network};
-    use lwk_signer::SwSigner;
-    use lwk_wollet::amp0::blocking::{Amp0, Amp0Connected};
-
-    // Create signer and watch only credentials
-    let network = Network::TestnetLiquid;
-    let is_mainnet = false;
-    let (signer, mnemonic) = SwSigner::random(is_mainnet)?;
-    let username = "<username>";
-    let password = "<password>";
-    let username = format!("user{}", signer.fingerprint()); // ANCHOR: ignore
-    let password = format!("pass{}", signer.fingerprint()); // ANCHOR: ignore
-
-    // Collect signer data
-    let signer_data = signer.amp0_signer_data()?;
-
-    // Connect to AMP0
-    let amp0 = Amp0Connected::new(network, signer_data)?;
-
-    // Obtain and sign the authentication challenge
-    let challenge = amp0.get_challenge()?;
-    let sig = signer.amp0_sign_challenge(&challenge)?;
-
-    // Login
-    let mut amp0 = amp0.login(&sig)?;
-
-    // Create a new AMP0 account
-    let pointer = amp0.next_account()?;
-    let account_xpub = signer.amp0_account_xpub(pointer)?;
-    let amp_id = amp0.create_amp0_account(pointer, &account_xpub)?;
-
-    // Create watch only entries
-    amp0.create_watch_only(&username, &password)?;
-
-    // Use watch only credentials to interact with AMP0
-    let amp0 = Amp0::new(network, &username, &password, &amp_id)?;
-    // ANCHOR_END: amp0-setup
-
-    Ok(())
-}
-
-#[test]
-#[allow(unused)]
-#[cfg(feature = "amp0")]
-#[ignore = "requires network calls"]
-#[rustfmt::skip] // our priority here is that generated docs renders nicely
-fn test_amp0_daily_ops() -> Result<(), Box<dyn std::error::Error>> {
-    // ANCHOR: amp0-daily-ops
-    use lwk_common::{Network, Signer};
-    use lwk_signer::SwSigner;
-    use lwk_wollet::amp0::{blocking::Amp0, Amp0Pset};
-    use lwk_wollet::{clients::blocking::EsploraClient, ElementsNetwork, Wollet};
-
-    // Signer
-    let mnemonic = "<mnemonic>";
-    // AMP0 Watch-Only credentials
-    let username = "<username>";
-    let password = "<password>";
-    let mnemonic = "thrive metal cactus come oval candy medal bounce captain shock permit joke"; // ANCHOR: ignore
-    let username = "userlwk001"; // ANCHOR: ignore
-    let password = "userlwk001"; // ANCHOR: ignore
-    // AMP ID (optional)
-    let amp_id = "";
-
-    // Create AMP0 context
-    let network = Network::TestnetLiquid;
-
-    let mut amp0 = Amp0::new(network, username, password, amp_id)?;
-
-    // Create AMP0 Wollet
-    let wd = amp0.wollet_descriptor();
-    let mut wollet = Wollet::without_persist(ElementsNetwork::LiquidTestnet, wd)?;
-
-    // Get a new address
-    let addr = amp0.address(None);
-
-    // Update the wallet with (new) blockchain data
-    let url = "https://blockstream.info/liquidtestnet/api";
-    let mut client = EsploraClient::new(url, ElementsNetwork::LiquidTestnet)?;
-    // esplora is too slow // ANCHOR: ignore
-    let url = "https://waterfalls.liquidwebwallet.org/liquidtestnet/api"; // ANCHOR: ignore
-    let mut client = EsploraClient::new_waterfalls(url, ElementsNetwork::LiquidTestnet)?; // ANCHOR: ignore
-    if let Some(update) = client.full_scan_to_index(&wollet, amp0.last_index())? {
-        wollet.apply_update(update)?;
-    }
-
-    // Get balance
-    let balance = wollet.balance()?;
-    let lbtc = wollet.policy_asset(); // ANCHOR: ignore
-    let balance = *balance.get(&lbtc).unwrap_or(&0); // ANCHOR: ignore
-    if balance < 500 { // ANCHOR: ignore
-        let addr = amp0.address(Some(1)).unwrap(); // ANCHOR: ignore
-        panic!("Send some tLBTC to {}", addr.address()); // ANCHOR: ignore
-    } // ANCHOR: ignore
-
-    // Construct a PSET sending LBTC back to the wallet
-    let amp0pset = wollet
-        .tx_builder()
-        .drain_lbtc_wallet()
-        .finish_for_amp0()?;
-    let mut pset = amp0pset.pset().clone();
-    let blinding_nonces = amp0pset.blinding_nonces();
-
-    // User signs the PSET
-    let is_mainnet = false;
-    let signer = SwSigner::new(mnemonic, is_mainnet)?;
-    let sigs = signer.sign(&mut pset)?;
-    assert!(sigs > 0);
-
-    // Reconstruct the Amp0 PSET with the PSET signed by the user
-    let amp0pset = Amp0Pset::new(pset, blinding_nonces.to_vec())?;
-
-    // AMP0 signs
-    let tx = amp0.sign(&amp0pset)?;
-
-    // Broadcast the transaction
-    let txid = client.broadcast(&tx)?;
-    // ANCHOR_END: amp0-daily-ops
 
     Ok(())
 }

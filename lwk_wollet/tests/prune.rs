4d2a26a54ade9f65b94972646d596bdbff29cbca
Leonardo Comandini <leo@leocomandini.org>
2025-12-02 11:14:33 +0100

    wollet: tests: update prune

--- lwk_wollet/tests/prune.rs
+++ lwk_wollet/tests/prune.rs
@@ -4,6 +4,7 @@ use lwk_test_util::*;
 #[test]
 fn test_prune() {
     let env = TestEnvBuilder::from_env().with_electrum().build();
+
     let signer = generate_signer();
     let view_key = generate_view_key();
     let desc = format!("ct({view_key},elwpkh({}/*))", signer.xpub());
@@ -11,16 +12,20 @@ fn test_prune() {
     let client = test_client_electrum(&env.electrum_url());
     let mut wallet = TestWollet::new(client, &desc);
 
+    // Fund the wallet and explicitly apply the update with the transaction.
     let address = wallet.address();
     let _ = env.elementsd_sendtoaddress(&address, 100_000, None);
 
     let mut update = next_tx_update(&mut wallet);
     let size_before = update.serialize().unwrap().len();
+    // Update.prune() does not remove the _wallet_ rangeproofs
     update.prune(&wallet.wollet);
     let size_after = update.serialize().unwrap().len();
     assert!(size_after < size_before);
     wallet.wollet.apply_update(update).unwrap();
 
+    // Building transactions requires the wallet rangeproofs
+    // (since add_input_rangeproofs still defaults to true)
     let mut pset = wallet
         .tx_builder()
         .add_lbtc_recipient(&address, 10_000)
@@ -30,5 +35,12 @@ fn test_prune() {
     let _details = wallet.wollet.get_details(&pset).unwrap();
 
     wallet.sign(&signer, &mut pset);
-    wallet.send(&mut pset);
+    let _txid = wallet.send(&mut pset);
+
+    // Create Wollet pruning witnesses
+    // Get transaction and check there are no witnesses
+    // Reload from persisted and check there are no witnesses
+    // Create transaction
+    // Call reunblind
+    // Call unblind with utxos
 }

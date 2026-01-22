e17aa7a5a116edabce28c611db206ead768799f4
Riccardo Casatta <riccardo@casatta.it>
2026-01-22 16:14:25 +0100

    wasm: fix feature disabled_regtest

--- lwk_wasm/src/boltz.rs
+++ lwk_wasm/src/boltz.rs
@@ -353,7 +353,7 @@ impl LightningPayment {
 
 // Launch boltz regtest and remove "disable_regtest" to use these tests
 // these test relies on .env file to be present at compile time.
-#[cfg(all(test, target_arch = "wasm32", disabled_regtest))]
+#[cfg(all(test, target_arch = "wasm32", feature = "disabled_regtest"))]
 mod tests {
     use wasm_bindgen_test::*;
 

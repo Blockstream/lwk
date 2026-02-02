7a710039547e03802395751f706a226e702b7c5b
Leonardo Comandini <leo@leocomandini.org>
2026-02-02 11:50:42 +0100

    wollet: tests: update

--- lwk_wollet/tests/e2e.rs
+++ lwk_wollet/tests/e2e.rs
@@ -1828,13 +1828,21 @@ fn test_unblinded_utxo() {
     let txid = w.fund_explicit(&env, satoshi, None, None);
 
     assert_eq!(w.balance(&policy_asset), 0);
-    assert_eq!(w.wollet.transactions().unwrap().len(), 0);
+    assert_eq!(w.wollet.transactions().unwrap().len(), 1);
     let tx = w.wollet.transaction(&txid).unwrap().unwrap();
     assert!(tx.inputs.iter().all(|i| i.is_none()));
-    assert!(tx.outputs.iter().all(|o| o.is_none()));
+    let txout = tx
+        .outputs
+        .iter()
+        .find(|o| o.is_some())
+        .unwrap()
+        .as_ref()
+        .unwrap();
+    assert_eq!(txout.unblinded.value, satoshi);
+    assert!(txout.address.blinding_pubkey.is_none());
     assert_eq!(*tx.balance.get(&policy_asset).unwrap_or(&0), 0);
     assert_eq!(w.wollet.utxos().unwrap().len(), 0);
-    assert_eq!(w.wollet.txos().unwrap().len(), 0);
+    assert_eq!(w.wollet.txos().unwrap().len(), 1);
 
     let external_utxo = w.wollet.explicit_utxos().unwrap()[0].clone();
 

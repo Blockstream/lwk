3dc091eb1d23cd7b7ad3eb45464df71f6e4726bf
Leonardo Comandini <leo@leocomandini.org>
2026-02-26 13:08:20 +0100

    wollet: tests: apply_amp0pset

--- lwk_wollet/tests/amp0.rs
+++ lwk_wollet/tests/amp0.rs
@@ -4,6 +4,43 @@ use lwk_test_util::*;
 use lwk_wollet::clients::blocking::BlockchainBackend;
 
 #[test]
+fn test_blinders() {
+    // keep blinders for outgoing outputs
+    // in amp0 temporarily
+    let env = TestEnvBuilder::from_env().with_electrum().build();
+
+    let signer = generate_signer();
+    let view_key = generate_view_key();
+    let desc = format!("ct({view_key},elwpkh({}/*))", signer.xpub());
+    let client = test_client_electrum(&env.electrum_url());
+    // TODO: leo blinders store!
+    let mut w = TestWollet::new(client, &desc);
+
+    let lbtc = w.policy_asset();
+    w.fund_btc(&env);
+
+    let node_addr = env.elementsd_getnewaddress();
+    let amp0pset = w
+        .tx_builder()
+        .add_recipient(&node_addr, 1000, lbtc)
+        .unwrap()
+        .add_recipient(&node_addr, 2000, lbtc)
+        .unwrap()
+        .finish_for_amp0()
+        .unwrap();
+
+    w.wollet.apply_amp0pset(&amp0pset).unwrap();
+    // Check that we have the blinders
+
+    let mut pset = amp0pset.pset().clone();
+
+    let sigs = signer.sign(&mut pset).unwrap();
+    assert!(sigs > 0);
+
+    w.send(&mut pset);
+}
+
+#[test]
 fn test_blinding_nonces() {
     // Construct a transaction and obtain the blinding nonces
     let env = TestEnvBuilder::from_env().with_electrum().build();

35e66cc13fe3f80536b1168b0864dc3a5b62c6c7
Riccardo Casatta <riccardo@casatta.it>
2026-03-04 10:56:29 +0100

    boltz: chainswaps: add lockup_txid

--- lwk_boltz/src/blocking.rs
+++ lwk_boltz/src/blocking.rs
@@ -415,6 +415,11 @@ impl LockupResponse {
         self.inner.claim_txid()
     }
 
+    /// See [`crate::LockupResponse::lockup_txid()`]
+    pub fn lockup_txid(&self) -> Option<&str> {
+        self.inner.lockup_txid()
+    }
+
     pub fn advance(&mut self) -> Result<ControlFlow<bool, SwapStatus>, Error> {
         let inner = self.runtime.block_on(self.inner.advance())?;
         Ok(inner)

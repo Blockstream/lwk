ee6cc54f5d0f43fe6c7c06d0fff35860eb148a7b
Leonardo Comandini <leo@leocomandini.org>
2026-03-31 10:15:49 +0200

    wollet: tests: move utxo only tests

---
+++ lwk_wollet/tests/utxo_only.rs
@@ -0,0 +1,326 @@
+use crate::test_wollet::*;
+use clients::blocking::BlockchainBackend;
+use lwk_common::Signer;
+use lwk_signer::*;
+use lwk_test_util::*;
+use lwk_wollet::*;
+use std::str::FromStr;
+
+#[cfg(feature = "esplora")]
+#[test]
+fn test_esplora_waterfalls_utxo_only() {
+    let env = TestEnvBuilder::from_env().with_waterfalls().build();
+
+    let signer = generate_signer();
+    let view_key = generate_view_key();
+    let desc = format!("ct({},elwpkh({}/*))", view_key, signer.xpub());
+
+    let desc = WolletDescriptor::from_str(&desc).unwrap();
+
+    let network = ElementsNetwork::default_regtest();
+    let mut wollet = WolletBuilder::new(network, desc.clone()).build().unwrap();
+    let mut client = asyncr::EsploraClientBuilder::new(&env.waterfalls_url(), network)
+        .waterfalls(true)
+        .build_blocking()
+        .unwrap();
+
+    let mut wollet_utxo_only = WolletBuilder::new(network, desc.clone()).build().unwrap();
+    let mut client_utxo_only = asyncr::EsploraClientBuilder::new(&env.waterfalls_url(), network)
+        .waterfalls(true)
+        .utxo_only(true)
+        .build_blocking()
+        .unwrap();
+
+    let address = wollet.address(None).unwrap();
+    let _txid = env.elementsd_sendtoaddress(address.address(), 1_000_000, None);
+    std::thread::sleep(std::time::Duration::from_millis(2_000));
+
+    // check both wallets have the same balance
+    let update = client.full_scan(&wollet).unwrap().unwrap();
+    wollet.apply_update(update).unwrap();
+    let update = client_utxo_only
+        .full_scan(&wollet_utxo_only)
+        .unwrap()
+        .unwrap();
+    wollet_utxo_only.apply_update(update).unwrap();
+    assert_eq!(
+        format!("{:?}", *wollet.balance().unwrap()),
+        "{5ac9f65c0efcc4775e0baec4ec03abdde22473cd3cf33c0419ca290e0751b225: 1000000}"
+    );
+    assert_eq!(
+        wollet.balance().unwrap(),
+        wollet_utxo_only.balance().unwrap()
+    );
+    assert_eq!(wollet.utxos().unwrap().len(), 1);
+    assert_eq!(wollet_utxo_only.utxos().unwrap().len(), 1);
+    assert_eq!(
+        wollet.transactions().unwrap(),
+        wollet_utxo_only.transactions().unwrap()
+    );
+
+    // spend from wollet and sync again both wallets
+    let address = env.elementsd_getnewaddress();
+    let mut pset = wollet
+        .tx_builder()
+        .add_lbtc_recipient(&address, 100_000)
+        .unwrap()
+        .finish()
+        .unwrap();
+    signer.sign(&mut pset).unwrap();
+    let pset_details = wollet.get_details(&pset).unwrap();
+
+    let tx = wollet.finalize(&mut pset).unwrap();
+
+    client.broadcast(&tx).unwrap();
+
+    env.elementsd_generate(1); // TODO: remove this
+    std::thread::sleep(std::time::Duration::from_millis(2_000));
+
+    let update = client.full_scan(&wollet).unwrap().unwrap();
+    wollet.apply_update(update).unwrap();
+    let update = client_utxo_only
+        .full_scan(&wollet_utxo_only)
+        .unwrap()
+        .unwrap();
+    wollet_utxo_only.apply_update(update).unwrap();
+
+    assert_eq!(
+        format!("{:?}", *wollet.balance().unwrap()),
+        format!(
+            "{{5ac9f65c0efcc4775e0baec4ec03abdde22473cd3cf33c0419ca290e0751b225: {}}}",
+            1_000_000 - pset_details.balance.fee - 100_000
+        )
+    );
+    assert_eq!(
+        wollet.balance().unwrap(),
+        wollet_utxo_only.balance().unwrap()
+    );
+
+    assert_eq!(wollet.utxos().unwrap().len(), 1);
+    assert_eq!(wollet_utxo_only.utxos().unwrap().len(), 1);
+
+    assert_eq!(wollet_utxo_only.transactions().unwrap().len(), 1);
+    assert_eq!(wollet.transactions().unwrap().len(), 2);
+
+    // ensure the dummy tx is not in the transactions list, the dummy_tx has zero outputs.
+    assert!(wollet_utxo_only
+        .transactions()
+        .unwrap()
+        .iter()
+        .all(|tx| !tx.outputs.is_empty()));
+}
+
+#[test]
+fn test_waterfalls_utxo_only_with_dummy() {
+    let env = TestEnvBuilder::from_env().with_waterfalls().build();
+
+    let signer = generate_signer();
+    let view_key = generate_view_key();
+    let desc = format!("ct({},elwpkh({}/<0;1>/*))", view_key, signer.xpub());
+    let signers = [&AnySigner::Software(signer)];
+    let network = ElementsNetwork::default_regtest();
+    let lbtc = network.policy_asset();
+
+    let client = asyncr::EsploraClientBuilder::new(&env.waterfalls_url(), network)
+        .waterfalls(true)
+        .build_blocking()
+        .unwrap();
+    let mut w = TestWollet::new(client, &desc);
+
+    let wd = WolletDescriptor::from_str(&desc).unwrap();
+    let mut client_utxo_only = asyncr::EsploraClientBuilder::new(&env.waterfalls_url(), network)
+        .waterfalls(true)
+        .utxo_only(true)
+        .build_blocking()
+        .unwrap();
+    let mut wollet_utxo_only = WolletBuilder::new(network, wd).build().unwrap();
+
+    let node_address = env.elementsd_getnewaddress();
+    let txid0 = w.fund_btc(&env);
+    let txid1 = w.send_btc(&signers, None, Some((node_address.clone(), 1)));
+    env.elementsd_generate(1);
+
+    // Utxo only intermediate sync
+    let update = client_utxo_only
+        .full_scan(&wollet_utxo_only)
+        .unwrap()
+        .unwrap();
+    assert!(update
+        .new_txs
+        .txs
+        .iter()
+        .any(|(_, tx)| tx.output.is_empty()));
+    wollet_utxo_only.apply_update(update).unwrap();
+
+    let txid2 = w.send_btc(&signers, None, Some((node_address.clone(), 1)));
+    let txid3 = w.send_btc(&signers, None, Some((node_address.clone(), 1)));
+    env.elementsd_generate(1);
+    wait_for_tx_confirmation(&mut w.wollet, &mut w.client, &txid3);
+
+    let balance = w.wollet.balance().unwrap();
+    assert!(*balance.get(&lbtc).unwrap_or(&0) > 0);
+    let txs = w.wollet.transactions().unwrap();
+    assert_eq!(txs.len(), 4);
+    assert!(txs.iter().any(|tx| tx.txid == txid0));
+    assert!(txs.iter().any(|tx| tx.txid == txid1));
+    assert!(txs.iter().any(|tx| tx.txid == txid2));
+    assert!(txs.iter().any(|tx| tx.txid == txid3));
+
+    // Utxo only final sync
+    let update = client_utxo_only
+        .full_scan(&wollet_utxo_only)
+        .unwrap()
+        .unwrap();
+    assert!(update
+        .new_txs
+        .txs
+        .iter()
+        .any(|(_, tx)| tx.output.is_empty()));
+    wollet_utxo_only.apply_update(update).unwrap();
+
+    assert_eq!(wollet_utxo_only.balance().unwrap(), balance);
+    let txs = wollet_utxo_only.transactions().unwrap();
+    assert_eq!(txs.len(), 1);
+    assert!(txs.iter().any(|tx| tx.txid == txid3));
+}
+
+#[cfg(feature = "esplora")]
+async fn test_esplora_waterfalls_balance_comparison(
+    descriptor: &str,
+    esplora_url: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    use clients::asyncr;
+
+    init_logging();
+
+    let desc = WolletDescriptor::from_str(descriptor)?;
+    let network = ElementsNetwork::LiquidTestnet;
+
+    let mut wollet = WolletBuilder::new(network, desc.clone()).build()?;
+    let mut client = asyncr::EsploraClientBuilder::new(esplora_url, network)
+        .waterfalls(true)
+        .concurrency(4)
+        .build()?;
+
+    let mut wollet_utxo_only = WolletBuilder::new(network, desc.clone()).build()?;
+    let mut client_utxo_only = asyncr::EsploraClientBuilder::new(esplora_url, network)
+        .utxo_only(true)
+        .waterfalls(true)
+        .concurrency(4)
+        .build()?;
+
+    // Perform full scan on both wallets
+    let update = client.full_scan(&wollet).await?.unwrap();
+    wollet.apply_update(update)?;
+
+    let update = client_utxo_only
+        .full_scan(&wollet_utxo_only)
+        .await?
+        .unwrap();
+    wollet_utxo_only.apply_update(update)?;
+
+    let u1 = wollet.utxos()?;
+    let u2 = wollet_utxo_only.utxos()?;
+    assert_eq!(u1.len(), u2.len());
+    assert_eq!(u1, u2);
+
+    // Compare balances
+    let balance = wollet.balance()?;
+    let balance_utxo_only = wollet_utxo_only.balance()?;
+
+    assert_eq!(balance, balance_utxo_only);
+
+    Ok(())
+}
+
+#[cfg(feature = "esplora")]
+#[tokio::test]
+#[ignore]
+async fn test_esplora_waterfalls_testnet_utxo_only_1() {
+    let descriptor = "ct(slip77(4892ff8181d55103c9b0a3a0ec2eb384a7518c51a87d59a9da011ce671d6e657),elwpkh([8fd75c12/84'/1'/0']tpubDDkuNJ5AvNAgekVh7Y4sAkmCzKs7mySbuq1GSnpA3oM7XxkCWVnT7y8ZSbbHFYxQYkdxNdzinLKt6kBKSVYD75UEHduiVjNz24Ew8YgpS5E/<0;1>/*))#qfvkjcee";
+    let esplora_url = "https://waterfalls.liquidwebwallet.org/liquidtestnet/api";
+
+    test_esplora_waterfalls_balance_comparison(descriptor, esplora_url)
+        .await
+        .unwrap();
+}
+
+#[cfg(feature = "esplora")]
+#[tokio::test]
+#[ignore]
+async fn test_esplora_waterfalls_testnet_utxo_only_2() {
+    let descriptor = "ct(slip77(ab5824f4477b4ebb00a132adfd8eb0b7935cf24f6ac151add5d1913db374ce92),elwpkh([759db348/84'/1'/0']tpubDCRMaF33e44pcJj534LXVhFbHibPbJ5vuLhSSPFAw57kYURv4tzXFL6LSnd78bkjqdmE3USedkbpXJUPA1tdzKfuYSL7PianceqAhwL2UkA/<0;1>/*))#cch6wrnp";
+    let esplora_url = "https://waterfalls.liquidwebwallet.org/liquidtestnet/api";
+
+    test_esplora_waterfalls_balance_comparison(descriptor, esplora_url)
+        .await
+        .unwrap();
+}
+
+#[test]
+fn test_faucet() {
+    // Simulate a couple of errors that we see with the testnet faucet
+    let env = TestEnvBuilder::from_env().with_waterfalls().build();
+
+    let signer = generate_signer();
+    let view_key = generate_view_key();
+    let desc = format!("ct({view_key},elwpkh({}/*))", signer.xpub());
+    let client = clients::EsploraClientBuilder::new(&env.waterfalls_url(), ElementsNetwork::Liquid)
+        .utxo_only(true)
+        .waterfalls(true)
+        .build_blocking()
+        .unwrap();
+    let mut w = TestWollet::new(client, &desc);
+
+    let lbtc = w.policy_asset();
+    let txid0 = w.fund_btc(&env);
+
+    let utxos = w.wollet.utxos().unwrap();
+    assert_eq!(utxos.len(), 1);
+    assert_eq!(utxos[0].outpoint.txid, txid0);
+
+    let node_address = env.elementsd_getnewaddress();
+    let mut pset = w
+        .tx_builder()
+        .add_recipient(&node_address, 1000, lbtc)
+        .unwrap()
+        .finish()
+        .unwrap();
+    let sigs = signer.sign(&mut pset).unwrap();
+    assert!(sigs > 0);
+    let tx = w.wollet.finalize(&mut pset).unwrap();
+
+    let txid1 = tx.txid();
+    w.wollet.apply_transaction(tx.clone()).unwrap();
+
+    let utxos = w.wollet.utxos().unwrap();
+    assert_eq!(utxos.len(), 1);
+    assert_eq!(utxos[0].outpoint.txid, txid1);
+
+    // Simulate a situation where the tx is seen by the node *after* the next sync
+    w.sync();
+    w.client.broadcast(&tx).unwrap();
+
+    // We see the old state
+    let utxos = w.wollet.utxos().unwrap();
+    assert_eq!(utxos.len(), 1);
+    assert_eq!(utxos[0].outpoint.txid, txid0);
+
+    // Sending a new tx will trigger an error
+    let mut pset = w
+        .tx_builder()
+        .add_recipient(&node_address, 1000, lbtc)
+        .unwrap()
+        .finish()
+        .unwrap();
+    let sigs = signer.sign(&mut pset).unwrap();
+    assert!(sigs > 0);
+    let tx = w.wollet.finalize(&mut pset).unwrap();
+    let err = w.client.broadcast(&tx).unwrap_err();
+    assert!(err.to_string().contains("txn-mempool-conflict"));
+
+    // If the tx is included in a block, the error is different
+    env.elementsd_generate(1);
+    let err = w.client.broadcast(&tx).unwrap_err();
+    assert!(err.to_string().contains("bad-txns-inputs-missingorspent"));
+}

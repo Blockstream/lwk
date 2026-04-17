22d17d04ee296cdb0644a330683e2a53e31790dd
Riccardo Casatta <riccardo@casatta.it>
2026-04-17 14:06:56 +0200

    wasm: test txs store

--- lwk_wasm/tests/node/store.js
+++ lwk_wasm/tests/node/store.js
@@ -19,6 +19,17 @@ function createStorage() {
     };
 }
 
+function storageSize(storage) {
+    let size = 0;
+    for (const [key, value] of storage._data.entries()) {
+        size += Buffer.byteLength(key, 'utf8');
+        if (value) {
+            size += value.byteLength;
+        }
+    }
+    return size;
+}
+
 function runWolletBuilderStoreTest() {
     const descriptorString = fs
         .readFileSync(`${__dirname}/../../test_data/update_with_mnemonic/descriptor.txt`, 'utf8')
@@ -41,19 +52,35 @@ function runWolletBuilderStoreTest() {
     wollet.applyUpdate(update);
 
     assert(jsStorage._data.has('000000000000'));
+    const jsStorageWithoutTxsStoreSize = storageSize(jsStorage);
 
     const expectedAddress = wollet.address(0).address().toString();
     const expectedTransactions = wollet.transactions().length;
     const expectedBalance = JSON.stringify(wollet.balance().toJSON());
 
     const restored = new lwk.WolletBuilder(network, descriptor)
-        .withStore(jsStorage)
+        .withExperimentalStore(jsStorage)
         .build();
 
     assert.strictEqual(restored.address(0).address().toString(), expectedAddress);
     assert.strictEqual(1, expectedTransactions);
     assert.strictEqual(restored.transactions().length, expectedTransactions);
     assert.strictEqual(JSON.stringify(restored.balance().toJSON()), expectedBalance);
+
+    const jsStorageWithTxsStore = createStorage();
+    const txsStorage = createStorage();
+    const wolletWithTxsStore = new lwk.WolletBuilder(network, descriptor)
+        .withExperimentalStore(jsStorageWithTxsStore)
+        .withTxsStore(txsStorage, false)
+        .build();
+    wolletWithTxsStore.applyUpdate(update);
+
+    assert(jsStorageWithTxsStore._data.has('000000000000'));
+    assert(txsStorage._data.has('wollet:txids'));
+    assert(
+        storageSize(jsStorageWithTxsStore) < jsStorageWithoutTxsStoreSize,
+        'jsStorage should be smaller when transactions are stored separately'
+    );
 }
 
 if (require.main === module) {

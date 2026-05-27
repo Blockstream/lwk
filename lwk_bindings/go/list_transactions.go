5808df7745ff358b9df221da5e449b0a66c9ee37
Leonardo Comandini <leo@leocomandini.org>
2026-05-27 15:37:45 +0200

    bindings: tests: go: regtest

--- lwk_bindings/go/list_transactions.go
+++ lwk_bindings/go/list_transactions.go
@@ -6,21 +6,19 @@ import (
 )
 
 func main() {
-	network := lwk.NetworkTestnet()
+	node := lwk.NewLwkTestEnv()
+	fmt.Printf("Regtest height: %d\n", node.Height())
+
+	network := lwk.NetworkRegtestDefault()
 	fmt.Println("Network:", network)
 
 	mnemonic, err := lwk.NewMnemonic("abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Mnemonic:", mnemonic)
 
-	client, err := network.DefaultElectrumClient()
+	client, err := lwk.ElectrumClientFromUrl(node.ElectrumUrl())
 	if err != nil {
-		panic(err)
-	}
-
-	if err := client.Ping(); err != nil {
 		panic(err)
 	}
 
@@ -40,20 +38,31 @@ func main() {
 		panic(err)
 	}
 
-	update, err := client.FullScan(wollet)
+	idx := uint32(0)
+	addrResult, err := wollet.Address(&idx)
 	if err != nil {
 		panic(err)
 	}
-	if update != nil {
-		if err := wollet.ApplyUpdate(*update); err != nil {
-			panic(err)
-		}
+	fmt.Println("Wollet address:", addrResult.Address())
+
+	fundedSatoshi := uint64(100000)
+	txid := node.SendToAddress(addrResult.Address(), fundedSatoshi, nil)
+	fmt.Println("Funded txid:", txid)
+
+	if _, err := wollet.WaitForTx(txid, client); err != nil {
+		panic(err)
 	}
+
+	balance, err := wollet.Balance()
+	if err != nil {
+		panic(err)
+	}
+	policyAsset := network.PolicyAsset()
+	fmt.Printf("Balance (policy asset): %d sats\n", balance[policyAsset])
 
 	txs, err := wollet.Transactions()
 	if err != nil {
 		panic(err)
 	}
-
 	fmt.Printf("Number of transactions: %d\n", len(txs))
 }

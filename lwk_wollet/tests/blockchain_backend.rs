4f75437bfb027c77c896fea5f363d73ca5c0ca0d
Volodymyr Herashchenko <herashchenko_vs@proton.me>
2026-06-03 18:30:31 +0300

    wollet: tests: client: has_txs

---
+++ lwk_wollet/tests/blockchain_backend.rs
@@ -0,0 +1,54 @@
+use crate::test_wollet::*;
+use lwk_test_util::*;
+use lwk_wollet::clients::blocking::BlockchainBackend;
+use lwk_wollet::*;
+
+fn test_has_txs_before_and_after_funding<C: BlockchainBackend>(env: TestEnv, client: C) {
+    let signer = generate_signer();
+    let view_key = generate_view_key();
+    let desc_str = format!("ct({view_key},elwpkh({}/<0;1>/*))", signer.xpub());
+    let wd: WolletDescriptor = desc_str.parse().unwrap();
+
+    let mut wallet = TestWollet::new(client, &desc_str);
+
+    assert!(!wallet.client.has_txs(&wd, None).unwrap());
+    assert!(!wallet.client.has_txs(&wd, Some(100)).unwrap());
+
+    let addr = wallet.wollet.address(Some(15)).unwrap().address().clone();
+    wallet.fund(&env, 10000, Some(addr), None);
+
+    assert!(wallet.client.has_txs(&wd, None).unwrap());
+    assert!(wallet.client.has_txs(&wd, Some(16)).unwrap());
+    assert!(!wallet.client.has_txs(&wd, Some(15)).unwrap());
+}
+
+#[test]
+fn test_has_txs_before_and_after_funding_electrum() {
+    let env = TestEnvBuilder::from_env().with_electrum().build();
+    let client = test_client_electrum(&env.electrum_url());
+
+    test_has_txs_before_and_after_funding(env, client);
+}
+
+#[test]
+fn test_has_txs_before_and_after_funding_esplora() {
+    let env = TestEnvBuilder::from_env().with_esplora().build();
+    let client =
+        clients::blocking::EsploraClient::new(&env.esplora_url(), Network::default_regtest())
+            .unwrap();
+
+    test_has_txs_before_and_after_funding(env, client);
+}
+
+#[test]
+fn test_has_txs_before_and_after_funding_waterfalls() {
+    let env = TestEnvBuilder::from_env().with_waterfalls().build();
+
+    let client = clients::blocking::EsploraClient::new_waterfalls(
+        &env.waterfalls_url(),
+        Network::default_regtest(),
+    )
+    .unwrap();
+
+    test_has_txs_before_and_after_funding(env, client);
+}

84aad5ce3e083cba549d235c402cf573c9672df0
Riccardo Casatta <riccardo@casatta.it>
2026-06-16 15:26:48 +0200

    nix: use subscribe waterfalls

--- flake.nix
+++ flake.nix
@@ -27,7 +27,7 @@
       };
     };
     waterfalls-flake = {
-      url = "github:RCasatta/waterfalls";
+      url = "github:RCasatta/waterfalls/subscribe";
       inputs.nixpkgs.follows = "waterfalls-nixpkgs";
       inputs.crane.follows = "waterfalls-crane";
       inputs.rust-overlay.follows = "waterfalls-rust-overlay";

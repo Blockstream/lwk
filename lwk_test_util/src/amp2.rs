2136f9ff4324a248e47ee79bf2dfef406cadf413
Leonardo Comandini <leo@leocomandini.org>
2026-06-18 09:34:42 +0200

    test util: amp2: wait

--- lwk_test_util/src/amp2.rs
+++ lwk_test_util/src/amp2.rs
@@ -1,6 +1,7 @@
 use std::ffi::OsStr;
 use std::net::TcpListener;
 use std::process::{Child, Command};
+use std::time::Duration;
 
 pub struct Amp2D {
     process: Child,
@@ -19,14 +20,35 @@ impl Amp2D {
             .unwrap()
             .to_string();
 
-        let process = Command::new(&exe)
+        let mut process = Command::new(&exe)
             .args([&addr])
             .stdout(std::process::Stdio::null())
             .stderr(std::process::Stdio::null())
             .spawn()
             .unwrap();
 
+        match process.try_wait() {
+            Ok(Some(_)) | Err(_) => {
+                let _ = process.kill();
+                panic!("failed to start amp2_mock");
+            }
+            Ok(None) => {}
+        }
+
         let url = format!("http://{addr}");
+
+        let mut started = false;
+        let check_url = format!("{url}/info/xpub");
+        for _ in 0..50 {
+            std::thread::sleep(Duration::from_millis(100));
+            if let Ok(r) = reqwest::blocking::get(&check_url) {
+                if r.status().as_u16() == 200 {
+                    started = true;
+                    break;
+                }
+            }
+        }
+        assert!(started, "amp2_mock hasn't started after 5s");
 
         Amp2D { process, url }
     }

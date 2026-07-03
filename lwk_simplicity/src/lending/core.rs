578994ed2e344e92fc2cd6b99837f9b9cd13622d
Volodymyr Herashchenko <herashchenko_vs@proton.me>
2026-07-03 15:58:48 +0300

    simplicity: lending: implemenet borrower_create_offer

--- lwk_simplicity/src/lending/core.rs
+++ lwk_simplicity/src/lending/core.rs
@@ -1,7 +1,13 @@
+use std::{collections::HashMap, str::FromStr};
+
 use lwk_common::Network;
 use lwk_wollet::{
     blocking::{BlockchainBackend, EsploraClient},
-    elements::{pset::PartiallySignedTransaction, Address, AssetId, Script, Transaction, Txid},
+    elements::{
+        confidential::{AssetBlindingFactor, ValueBlindingFactor},
+        pset::PartiallySignedTransaction,
+        Address, AssetId, OutPoint, Script, Transaction, TxOutSecrets, Txid,
+    },
     ElectrumClient, Wollet, WolletBuilder, WolletDescriptor, EC,
 };
 
@@ -12,12 +18,18 @@ use simplex::transaction::{
     UTXO,
 };
 
-use lending_contracts::programs::issuance_factory::{IssuanceFactory, IssuanceFactoryParameters};
-use lending_contracts::programs::program::SimplexProgram;
+use lending_contracts::programs::{
+    issuance_factory::{IssuanceFactory, IssuanceFactoryParameters},
+    lending::{LendingOfferParameters, OfferParameters},
+};
+use lending_contracts::programs::{lending::LendingOffer, program::SimplexProgram};
 use lending_contracts::utils::get_random_seed;
+use simplicityhl::WitnessValues;
 
 use crate::lending::error::LendingError;
 use crate::lending::network::to_simplicity_network;
+
+use super::indexer::client::FactoryDetailsResponse;
 
 enum AnyClient {
     Electrum(Box<ElectrumClient>),
@@ -40,6 +52,13 @@ impl AnyClient {
         match self {
             AnyClient::Electrum(c) => c.full_scan(wollet),
             AnyClient::Esplora(c) => c.full_scan(wollet),
+        }
+    }
+
+    fn get_transaction(&self, txid: Txid) -> Result<Transaction, lwk_wollet::Error> {
+        match self {
+            AnyClient::Electrum(c) => c.get_transaction(txid),
+            AnyClient::Esplora(c) => c.get_transaction(txid),
         }
     }
 }
@@ -174,15 +193,266 @@ impl LendingSession {
         })
     }
 
-    /// Create borrow offer
+    /// Create a borrow offer
     ///
     /// # Errors
-    /// Borrower account was not previously created
+    /// Returns an error if the wallet has no suitable UTXOs or the lending transaction construction
+    /// fails.
     pub fn borrower_create_offer(
-        &self,
-        _details: OfferDetails,
+        &mut self,
+        details: OfferDetails,
+        factory: FactoryDetailsResponse,
     ) -> Result<CreateBorrowTransaction, LendingError> {
-        todo!()
+        // TODO: we should estimate fee dynamically
+        const FEE: u64 = 250;
+        const NFT_AMOUNT: u64 = 1;
+
+        let policy_asset = *self.network.policy_asset();
+
+        let auth_utxo_data = factory.auth_utxo.as_ref().ok_or_else(|| {
+            LendingError::Config("factory has no auth UTXO on the indexer".into())
+        })?;
+        let program_utxo_data = factory.program_utxo.as_ref().ok_or_else(|| {
+            LendingError::Config("factory has no program UTXO on the indexer".into())
+        })?;
+
+        let auth_outpoint = OutPoint {
+            txid: Txid::from_str(&auth_utxo_data.txid)
+                .map_err(|e| LendingError::Config(format!("invalid auth txid: {e}")))?,
+            vout: auth_utxo_data.vout,
+        };
+        let program_outpoint = OutPoint {
+            txid: Txid::from_str(&program_utxo_data.txid)
+                .map_err(|e| LendingError::Config(format!("invalid program txid: {e}")))?,
+            vout: program_utxo_data.vout,
+        };
+
+        let factory_asset_id = AssetId::from_str(&factory.factory_asset_id)
+            .map_err(|e| LendingError::Config(format!("invalid factory asset id: {e}")))?;
+
+        let issuance_factory_params = IssuanceFactoryParameters {
+            issuing_utxos_count: 2,
+            reissuance_flags: 0,
+            network: to_simplicity_network(self.network),
+        };
+        let issuance_factory = IssuanceFactory::new(issuance_factory_params);
+
+        // Fetch the prepare transaction to obtain the raw txouts for the factory utxos.
+        let prepare_tx = self
+            .client
+            .get_transaction(
+                Txid::from_str(&auth_utxo_data.txid)
+                    .map_err(|e| LendingError::Config(format!("invalid created_at_txid: {e}")))?,
+            )
+            .map_err(|e| LendingError::Config(format!("failed to fetch prepare tx: {e}")))?;
+
+        let program_tx = self
+            .client
+            .get_transaction(
+                Txid::from_str(&program_utxo_data.txid)
+                    .map_err(|e| LendingError::Config(format!("invalid created_at_txid: {e}")))?,
+            )
+            .map_err(|e| LendingError::Config(format!("failed to fetch prepare tx: {e}")))?;
+
+        let auth_txout = prepare_tx
+            .output
+            .get(auth_outpoint.vout as usize)
+            .ok_or_else(|| LendingError::Config("auth vout out of bounds".into()))?
+            .clone();
+
+        let auth_script = auth_txout.script_pubkey.clone();
+        let program_txout = program_tx
+            .output
+            .get(program_outpoint.vout as usize)
+            .ok_or_else(|| LendingError::Config("program vout out of bounds".into()))?
+            .clone();
+
+        // TODO: should we select this UTXOs outside of the session?
+        // Find collateral UTXO via wollet
+        let utxos = self.wollet.utxos().map_err(LendingError::Wallet)?;
+        let collateral_utxo = utxos
+            .iter()
+            .filter(|u| {
+                u.unblinded.asset == details.collateral_asset_id
+                    && u.unblinded.value >= details.collateral_amount
+            })
+            .min_by_key(|u| u.unblinded.value)
+            .ok_or_else(|| LendingError::Config("no suitable collateral UTXO in wallet".into()))?;
+        let collateral_value = collateral_utxo.unblinded.value;
+
+        let collateral_tx = self
+            .client
+            .get_transaction(collateral_utxo.outpoint.txid)
+            .map_err(|e| LendingError::Config(format!("failed to fetch collateral tx: {e}")))?;
+        let collateral_txout = collateral_tx.output[collateral_utxo.outpoint.vout as usize].clone();
+
+        // Select a UTXO for a fee
+        let fee_funding_utxo = utxos
+            .iter()
+            .filter(|u| {
+                u.unblinded.asset == policy_asset
+                    && u.unblinded.value > FEE
+                    && u.outpoint != collateral_utxo.outpoint
+            })
+            .min_by_key(|u| u.unblinded.value)
+            .ok_or_else(|| LendingError::Config("no suitable fee funding UTXO in wallet".into()))?;
+        let fee_funding_value = fee_funding_utxo.unblinded.value;
+
+        let fee_funding_tx = self
+            .client
+            .get_transaction(fee_funding_utxo.outpoint.txid)
+            .map_err(|e| LendingError::Config(format!("failed to fetch fee funding tx: {e}")))?;
+        let fee_funding_txout =
+            fee_funding_tx.output[fee_funding_utxo.outpoint.vout as usize].clone();
+
+        // Derive the user's next receive address for auth return and change
+        let address_result = self.wollet.address(None).map_err(LendingError::Wallet)?;
+        let user_script = address_result.address().script_pubkey();
+        let user_blinding_pk = address_result
+            .address()
+            .blinding_pubkey
+            .ok_or_else(|| LendingError::Config("address has no blinding key".into()))?;
+
+        // Use shared entropy for both NFTs
+        let nfts_entropy = get_random_seed();
+
+        // Build the transaction
+        let mut ft = FinalTransaction::new();
+
+        // Input 0: auth UTXO
+        ft.add_input(
+            PartialInput::new(UTXO {
+                outpoint: auth_outpoint,
+                txout: auth_txout,
+                secrets: Some(TxOutSecrets {
+                    asset: factory_asset_id,
+                    asset_bf: AssetBlindingFactor::zero(),
+                    value: 1,
+                    value_bf: ValueBlindingFactor::zero(),
+                }),
+            }),
+            RequiredSignature::NativeEcdsa,
+        );
+
+        // Output 0: auth UTXO
+        ft.add_output(PartialOutput::new(
+            auth_script.clone(),
+            NFT_AMOUNT,
+            factory_asset_id,
+        ));
+
+        // - Input 1: program UTXO with borrower NFT issuance
+        // - Output 1: program UTXO
+        let program_issuance = IssuanceInput::new_issuance(NFT_AMOUNT, 0, nfts_entropy);
+        let borrower_nft_details = issuance_factory.attach_assets_issuance(
+            &mut ft,
+            UTXO {
+                outpoint: program_outpoint,
+                txout: program_txout,
+                secrets: Some(TxOutSecrets {
+                    asset: factory_asset_id,
+                    asset_bf: AssetBlindingFactor::zero(),
+                    value: 1,
+                    value_bf: ValueBlindingFactor::zero(),
+                }),
+            },
+            program_issuance,
+        );
+
+        // Output 2: borrower NFT to user (from the factory issuance)
+        ft.add_output(PartialOutput::new(
+            user_script.clone(),
+            NFT_AMOUNT,
+            borrower_nft_details.asset_id,
+        ));
+
+        // Input 2: collateral UTXO with lender NFT issuance
+        let lender_nft_issuance = IssuanceInput::new_issuance(NFT_AMOUNT, 0, nfts_entropy);
+        let lender_nft_details = ft.add_issuance_input(
+            PartialInput::new(UTXO {
+                outpoint: collateral_utxo.outpoint,
+                txout: collateral_txout,
+                secrets: Some(collateral_utxo.unblinded),
+            }),
+            lender_nft_issuance,
+            RequiredSignature::NativeEcdsa,
+        );
+
+        // Input 3: fee funding UTXO
+        ft.add_input(
+            PartialInput::new(UTXO {
+                outpoint: fee_funding_utxo.outpoint,
+                txout: fee_funding_txout,
+                secrets: Some(fee_funding_utxo.unblinded),
+            }),
+            RequiredSignature::NativeEcdsa,
+        );
+
+        // Build the LendingOffer
+        let lending_offer_params = LendingOfferParameters {
+            collateral_asset_id: details.collateral_asset_id,
+            principal_asset_id: details.principal_asset_id,
+            borrower_nft_asset_id: borrower_nft_details.asset_id,
+            lender_nft_asset_id: lender_nft_details.asset_id,
+            protocol_fee_keeper_asset_id: details.protocol_fee_keeper_asset_id,
+            offer_parameters: OfferParameters {
+                collateral_amount: details.collateral_amount,
+                principal_amount: details.principal_amount,
+                loan_expiration_time: details.loan_expiration_time,
+                principal_interest_rate: details.principal_interest_rate,
+            },
+            network: to_simplicity_network(self.network),
+        };
+        let lending_offer = LendingOffer::new_pending(lending_offer_params);
+
+        // - Output 3: lender NFT with ScriptAuth
+        // - Output 4: OP_RETURN metadata
+        // - Output 5: lending covenant collateral
+        lending_offer.attach_creation(&mut ft);
+
+        let user_blinding_pk_btc =
+            lwk_wollet::elements::bitcoin::PublicKey::from_slice(&user_blinding_pk.serialize())
+                .map_err(|e| LendingError::Config(format!("invalid blinding key: {e}")))?;
+
+        // Add fee output
+        ft.add_output(PartialOutput::new(Script::default(), FEE, policy_asset));
+
+        // Add fee change output
+        let fee_change_value = fee_funding_value - FEE;
+        ft.add_output(
+            PartialOutput::new(user_script.clone(), fee_change_value, policy_asset)
+                .with_blinding_key(user_blinding_pk_btc),
+        );
+
+        // Add collateral change output
+        let collateral_change_value = collateral_value - details.collateral_amount;
+        if collateral_change_value > 0 {
+            ft.add_output(
+                PartialOutput::new(
+                    user_script.clone(),
+                    collateral_change_value,
+                    details.collateral_asset_id,
+                )
+                .with_blinding_key(user_blinding_pk_btc),
+            );
+        }
+
+        // Extract
+        let (mut pset, inp_txout_sec) = ft.extract_pst();
+
+        // Blind, add details
+        let mut rng = thread_rng();
+        pset.blind_last(&mut rng, &EC, &inp_txout_sec)
+            .map_err(|e| LendingError::Config(format!("blinding error: {e}")))?;
+
+        self.wollet
+            .add_details(&mut pset)
+            .map_err(LendingError::Wallet)?;
+
+        // Finalize Simplicity program inputs on the PSET
+        self.finalize_program_inputs(&ft, &mut pset)?;
+
+        Ok(CreateBorrowTransaction { pset })
     }
 
     pub fn fully_repay_loan(&self, _details: RepaymentDetails) -> Result<(), LendingError> {
@@ -328,6 +598,7 @@ pub struct OfferDetails {
     pub collateral_amount: u64,
     pub loan_expiration_time: u32,
     pub principal_interest_rate: u16,
+    pub protocol_fee_keeper_asset_id: AssetId,
 }
 
 pub struct RepaymentDetails {
@@ -343,11 +614,5 @@ pub struct BorrowerAccountCreationResult {
 }
 
 pub struct CreateBorrowTransaction {
-    inner: PartiallySignedTransaction,
-}
-
-impl CreateBorrowTransaction {
-    pub fn inner(&self) -> &PartiallySignedTransaction {
-        &self.inner
-    }
+    pub pset: PartiallySignedTransaction,
 }

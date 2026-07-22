86e721c87d499c29be5362d6841687badb6d6c87
topologoanatom <vvolovyk@blockstream.com>
2026-07-22 11:30:00 +0300

    wasm: add multi-issuance test

--- lwk_wasm/tests/node/tx_builder.js
+++ lwk_wasm/tests/node/tx_builder.js
@@ -81,9 +81,93 @@ function runInputOrderTest() {
     }
 }
 
+function runIssueAssetTest() {
+    try {
+        const descriptorString = fs
+            .readFileSync(`${__dirname}/../../test_data/update_with_mnemonic/descriptor2.txt`, 'utf8')
+            .trim();
+        const encryptedUpdate = fs
+            .readFileSync(
+                `${__dirname}/../../test_data/update_with_mnemonic/update_serialized_encrypted2.txt`,
+                'utf8'
+            )
+            .trim();
+
+        const network = lwk.Network.testnet();
+        const descriptor = new lwk.WolletDescriptor(descriptorString);
+        const update = lwk.Update.deserializeDecryptedBase64(encryptedUpdate, descriptor);
+
+        const wollet = new lwk.WolletBuilder(network, descriptor).build();
+        wollet.applyUpdate(update);
+
+        const utxos = wollet.utxos();
+        assert.strictEqual(utxos.length, 2);
+        const policyAsset = network.policyAsset();
+        const lbtcUtxo = utxos.find((u) => u.unblinded().asset().toString() === policyAsset.toString());
+        const assetUtxo = utxos.find((u) => u.unblinded().asset().toString() !== policyAsset.toString());
+
+        // Two issuances in the same transaction, sequentially assigned to the wallet's two UTXOs
+        const request1 = new lwk.IssuanceRequest(BigInt(1000), BigInt(1));
+        const request2 = new lwk.IssuanceRequest(BigInt(2000), BigInt(2));
+        let builder = new lwk.TxBuilder(network);
+        builder = builder.addIssuance(request1);
+        builder = builder.addIssuance(request2);
+        builder = builder.setWalletUtxos(utxos.map((u) => u.outpoint()));
+        const pset = builder.finish(wollet);
+
+        const inputs = pset.inputs();
+        assert.strictEqual(inputs.length, 2);
+
+        const issuanceInputs = inputs.filter((i) => i.issuance() !== undefined);
+        assert.strictEqual(issuanceInputs.length, 2);
+        for (const input of issuanceInputs) {
+            assert.strictEqual(input.issuance().isIssuance(), true);
+            assert.strictEqual(input.issuance().isReissuance(), false);
+            assert.notStrictEqual(input.issuanceAsset(), undefined);
+            assert.notStrictEqual(input.issuanceToken(), undefined);
+        }
+        assert.notStrictEqual(
+            issuanceInputs[0].issuanceAsset().toString(),
+            issuanceInputs[1].issuanceAsset().toString()
+        );
+
+        // Two issuances in the same transaction, each pinned to a different one of the wallet's two UTXOs
+        const request3 = new lwk.IssuanceRequest(BigInt(3000), BigInt(5));
+        const request4 = new lwk.IssuanceRequest(BigInt(4000), BigInt(6));
+        let pinnedBuilder = new lwk.TxBuilder(network);
+        pinnedBuilder = pinnedBuilder.setWalletUtxos([assetUtxo.outpoint(), lbtcUtxo.outpoint()]);
+        pinnedBuilder = pinnedBuilder.setInputsOrder([assetUtxo.outpoint(), lbtcUtxo.outpoint()]);
+        pinnedBuilder = pinnedBuilder.addIssuance(request3.pinInput(assetUtxo.outpoint()));
+        pinnedBuilder = pinnedBuilder.addIssuance(request4.pinInput(lbtcUtxo.outpoint()));
+        const pinnedPset = pinnedBuilder.finish(wollet);
+
+        const pinnedInputs = pinnedPset.inputs();
+        assert.strictEqual(pinnedInputs.length, 2);
+
+        assert.strictEqual(pinnedInputs[0].previousTxid().toString(), assetUtxo.outpoint().txid().toString());
+        assert.strictEqual(pinnedInputs[0].previousVout(), assetUtxo.outpoint().vout());
+        assert.notStrictEqual(pinnedInputs[0].issuance(), undefined);
+        assert.strictEqual(pinnedInputs[0].issuance().isIssuance(), true);
+
+        assert.strictEqual(pinnedInputs[1].previousTxid().toString(), lbtcUtxo.outpoint().txid().toString());
+        assert.strictEqual(pinnedInputs[1].previousVout(), lbtcUtxo.outpoint().vout());
+        assert.notStrictEqual(pinnedInputs[1].issuance(), undefined);
+        assert.strictEqual(pinnedInputs[1].issuance().isIssuance(), true);
+
+        assert.notStrictEqual(
+            pinnedInputs[0].issuanceAsset().toString(),
+            pinnedInputs[1].issuanceAsset().toString()
+        );
+    } catch (error) {
+        console.error("Issue asset test failed:", error);
+        throw error;
+    }
+}
+
 function runTxBuilderTest() {
     runManualCoinSelectionTest();
     runInputOrderTest();
+    runIssueAssetTest();
 }
 
 if (require.main === module) {

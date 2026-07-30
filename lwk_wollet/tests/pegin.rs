7022db500a2fe3eedde6709382630173070259ab
Riccardo Casatta <riccardo@casatta.it>
2026-07-30 14:45:10 +0200

    wollet: claim pegins in regtest
    Exercise the complete LWK pegin flow against Bitcoin and Elements nodes using a signature-protected federation policy. This replaces the node-wallet reference flow with LWK address derivation, PSET construction, signing, finalization, and broadcast.

--- lwk_wollet/tests/pegin.rs
+++ lwk_wollet/tests/pegin.rs
@@ -1,49 +1,78 @@
 use crate::test_wollet::*;
 use clients::blocking::BlockchainBackend;
-use elements::hex::ToHex;
+use elements::hex::{FromHex, ToHex};
 use lwk_common::electrum_ssl::{LIQUID_SOCKET, LIQUID_TESTNET_SOCKET};
+use lwk_common::Signer;
+use lwk_signer::SwSigner;
 use lwk_test_util::*;
 use lwk_wollet::pegin::fetch_last_full_header;
 use lwk_wollet::*;
 
 #[test]
 fn claim_pegin() {
-    // TODO this test makes a pegin using the node as a reference implementation to implement the pegin
-    // in the lwk wallet
-    let env = TestEnvBuilder::from_env().with_bitcoind().build();
+    let env = TestEnvBuilder::from_env()
+        .with_bitcoind()
+        .with_fedpeg_script(FED_PEG_SCRIPT)
+        .build();
+    let network = env.elementsd_network();
+    let signer = SwSigner::new(TEST_MNEMONIC, false).unwrap();
+    let descriptor: WolletDescriptor = format!(
+        "ct(slip77({TEST_MNEMONIC_SLIP77}),elwpkh({}/*))",
+        signer.xpub()
+    )
+    .parse()
+    .unwrap();
+    let wollet = WolletBuilder::new(network, descriptor).build().unwrap();
+
+    let tip = env.elementsd_height() as u32;
+    let epoch_length = network.dynamic_epoch_length();
+    let full_header_height = (tip / epoch_length) * epoch_length;
+    let block_hash = env.elementsd_call("getblockhash", &[full_header_height.into()]);
+    let header_hex = env.elementsd_call("getblockheader", &[block_hash, false.into()]);
+    let header_bytes = Vec::<u8>::from_hex(header_hex.as_str().unwrap()).unwrap();
+    let header: elements::BlockHeader = elements::encode::deserialize(&header_bytes).unwrap();
+    let fed_peg = FedPeg::from_block_header(network, &header).unwrap();
+    let pegin_address = wollet.pegin_address(Some(0), &fed_peg).unwrap();
 
     env.bitcoind_generate(101);
-    let (mainchain_address, claim_script) = env.elementsd_getpeginaddress();
-    let txid = env.bitcoind_sendtoaddress(&mainchain_address, 100_000_000);
-    let tx = env.bitcoind_getrawtransaction(txid);
-    let tx_bytes = bitcoin::consensus::serialize(&tx);
-
-    let pegin_vout = tx
-        .output
-        .iter()
-        .position(|o| o.script_pubkey == mainchain_address.script_pubkey())
-        .unwrap();
+    let txid = env.bitcoind_sendtoaddress(pegin_address.address(), 100_000_000);
+    let transaction = env.bitcoind_getrawtransaction(txid);
 
     env.bitcoind_generate(101);
-    let proof = env.bitcoind_gettxoutproof(txid);
-
+    let proof_hex = env.bitcoind_gettxoutproof(txid);
+    let proof = Vec::<u8>::from_hex(&proof_hex).unwrap();
     env.elementsd_generate(2);
 
-    let address_lbtc = env.elementsd_getnewaddress().to_string();
+    let funding = PeginFunding::from_raw(
+        pegin_address,
+        &bitcoin::consensus::serialize(&transaction),
+        &proof,
+    )
+    .unwrap();
+    let destination = env.elementsd_getnewaddress();
+    let mut pset = wollet
+        .tx_builder()
+        .add_pegin_input(PeginInput::from(funding))
+        .unwrap()
+        .drain_lbtc_to(destination)
+        .finish()
+        .unwrap();
 
-    let inputs = serde_json::json!([ {"txid":txid, "vout": pegin_vout,"pegin_bitcoin_tx": tx_bytes.to_hex(), "pegin_txout_proof": proof, "pegin_claim_script": claim_script } ]);
-    let outputs = serde_json::json!([
-        {address_lbtc: "0.9", "blinder_index": 0},
-        {"fee": "0.1" }
-    ]);
-
-    let psbt = env.elementsd_raw_createpsbt(inputs, outputs);
-
-    assert_eq!(env.elementsd_expected_next(&psbt), "updater");
-    let psbt = env.elementsd_walletprocesspsbt(&psbt);
-    assert_eq!(env.elementsd_expected_next(&psbt), "extractor");
-    let tx_hex = env.elementsd_finalizepsbt(&psbt);
-    let _txid = env.elementsd_sendrawtransaction(&tx_hex);
+    assert_eq!(signer.sign(&mut pset).unwrap(), 1);
+    let transaction = wollet.finalize(&mut pset).unwrap();
+    assert!(transaction.input[0].is_pegin());
+    assert_eq!(transaction.input[0].witness.pegin_witness.len(), 6);
+    let transaction_hex = elements::encode::serialize(&transaction).to_hex();
+    let mempool_result =
+        env.elementsd_call("testmempoolaccept", &[serde_json::json!([transaction_hex])]);
+    assert!(
+        mempool_result[0]["allowed"].as_bool().unwrap(),
+        "{mempool_result}"
+    );
+    assert_eq!(
+        env.elementsd_sendrawtransaction(&transaction_hex),
+        transaction.txid().to_string()
+    );
 }
 
 #[test]

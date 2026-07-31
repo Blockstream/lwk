268ec188c58659db991a25524049434d80e5fe6b
Leonardo Comandini <leo@leocomandini.org>
2026-07-31 15:54:26 +0200

    bindings: tests: amp2: elipamp2

--- lwk_bindings/tests/bindings/amp2.py
+++ lwk_bindings/tests/bindings/amp2.py
@@ -47,3 +47,40 @@ wollet.wait_for_tx(txid, client)
 # note: this is might be rejected by amp2
 custom_desc = "ct(1111111111111111111111111111111111111111111111111111111111111111,elwsh(and_v(v:pk(026a04ab98d9e4774ad806e302dddeb63bea16b5cb5f223ee77478e861bb583eb3),multi(2,[342c8926/87h/1h/0h]tpubDDWUA7YvBHxdurKUrYFkdjsB59koHqvGRJ3j9zDhwMycxXHXz1ujTfHMB66K4rEWDM8BoDKDdJx3rVGp2qUSPnXVpQXi8qtnXqa96nPnZAH/0/*,[af9e5bc2/87h/1h/0h]tpubDDRPayLs2vBkRkyQ9X2BEhojxCy9vvZpjhubEVosz5pi66LuuAuyZQiUtsPBN5wSfhWLoMYM3gqVqT3Po4GpcWGUfPh8514ZBB9hfWFNEUA/0/*,[57411aec/87h/1h/0h]tpubDDmweWcTcRb54kZqy3Gv5JF8SjAyuoK3uPYXp24uz6nfsKjJojxjdZAang5HXDmtS8tg5CJntUC4fzn4aY5Dsg6Aphvq42vK9edmgX83NFg/0/*))))";
 amp2_desc = Amp2Descriptor.new_with_custom_descriptor(WolletDescriptor(custom_desc))
+
+# e2e with elipamp2 desc
+account = 0
+desc = amp2.elipamp2_from_signer(signer, account)
+
+# getting the descriptor with signer managed externally leads to the same descriptor
+user_path = amp2.elipamp2_user_path(account)
+view_path = amp2.elipamp2_view_path(account)
+user_keyorigin_xpub = signer.keyorigin_xpub_from_path(user_path)
+view_keyorigin_xpub = signer.keyorigin_xpub_from_path(view_path)
+desc_from_str = amp2.elipamp2_from_str(user_keyorigin_xpub, view_keyorigin_xpub)
+assert str(desc.descriptor()) == str(desc_from_str.descriptor())
+
+dwid = amp2.register_wallet(desc)
+
+wollet = Wollet(network, desc.descriptor(), datadir=None)
+client = ElectrumClient.from_url(env.electrum_url())
+
+sats = 100000
+txid = env.send_to_address(wollet.address(0).address(), sats, asset=None)
+wollet.wait_for_tx(txid, client)
+
+sent_sats = 1000
+node_address = env.get_new_address()
+
+b = network.tx_builder()
+b.add_lbtc_recipient(node_address, sent_sats)
+pset = b.finish(wollet)
+
+pset = signer.sign(pset)
+pset = amp2.cosign(pset)
+
+pset = wollet.finalize(pset)
+
+tx = pset.extract_tx()
+txid = client.broadcast(tx)
+wollet.wait_for_tx(txid, client)

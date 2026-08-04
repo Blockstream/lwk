cd32ab7ebfc54ce3e8b7ca2e098cbf9ad8ebd734
Volodymyr Herashchenko <herashchenko_vs@proton.me>
2026-08-04 15:31:28 +0300

    simplicity: lending: move client to separate module

--- lwk_simplicity/src/lending/core.rs
+++ lwk_simplicity/src/lending/core.rs
@@ -30,47 +30,7 @@ use simplicityhl::WitnessValues;
 
 use crate::lending::error::LendingError;
 use crate::lending::network::to_simplicity_network;
-
-use super::indexer::response::FactoryDetailsResponse;
-
-enum AnyClient {
-    Electrum(Box<ElectrumClient>),
-    Esplora(EsploraClient),
-}
-
-impl AnyClient {
-    #[allow(dead_code)]
-    fn broadcast(&self, tx: &Transaction) -> Result<Txid, lwk_wollet::Error> {
-        match self {
-            AnyClient::Electrum(c) => c.broadcast(tx),
-            AnyClient::Esplora(c) => c.broadcast(tx),
-        }
-    }
-
-    fn full_scan(
-        &mut self,
-        wollet: &Wollet,
-    ) -> Result<Option<lwk_wollet::Update>, lwk_wollet::Error> {
-        match self {
-            AnyClient::Electrum(c) => c.full_scan(wollet),
-            AnyClient::Esplora(c) => c.full_scan(wollet),
-        }
-    }
-
-    fn get_transaction(&self, txid: Txid) -> Result<Transaction, lwk_wollet::Error> {
-        match self {
-            AnyClient::Electrum(c) => c.get_transaction(txid),
-            AnyClient::Esplora(c) => c.get_transaction(txid),
-        }
-    }
-
-    fn tip(&mut self) -> Result<lwk_wollet::elements::BlockHeader, lwk_wollet::Error> {
-        match self {
-            AnyClient::Electrum(c) => c.tip(),
-            AnyClient::Esplora(c) => c.tip(),
-        }
-    }
-}
+use crate::lending::{client::AnyClient, indexer::response::FactoryDetailsResponse};
 
 pub struct LendingSession {
     network: Network,

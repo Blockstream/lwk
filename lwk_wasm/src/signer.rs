9c0b3199f79287d779bba1d0bb9f43efd6b99e70
Leonardo Comandini <leo@leocomandini.org>
2026-08-04 09:53:41 +0200

    wasm: signer: keyOriginXpubFromPath

--- lwk_wasm/src/signer.rs
+++ lwk_wasm/src/signer.rs
@@ -77,6 +77,15 @@ impl Signer {
         Ok(self.inner.fingerprint().to_string())
     }
 
+    /// Derive an xpub at `path` and return it as a keyorigin xpub string
+    #[wasm_bindgen(js_name = keyoriginXpubFromPath)]
+    pub fn keyorigin_xpub_from_path(&self, path: &str) -> Result<String, Error> {
+        let path: bip32::DerivationPath = path.parse()?;
+        let fingerprint = self.inner.fingerprint();
+        let xpub = lwk_common::Signer::derive_xpub(&self.inner, &path)?;
+        Ok(format!("[{fingerprint}/{path}]{xpub}"))
+    }
+
     /// Return the mnemonic of the signer
     pub fn mnemonic(&self) -> Mnemonic {
         self.inner

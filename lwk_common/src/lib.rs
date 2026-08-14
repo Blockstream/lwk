faa637aee7e64b95be938f58a3fc3957679b1437
topologoanatom <vvolovyk@blockstream.com>
2026-08-14 14:30:24 +0300

    common: move input_script logic for further usage

--- lwk_common/src/lib.rs
+++ lwk_common/src/lib.rs
@@ -403,13 +403,8 @@ pub fn pset_signatures(pset: &PartiallySignedTransaction) -> Vec<PsetSignatures>
         .collect()
 }
 
-/// Return the sighash declared by an input, the default implied by the spent output script, or `None` if the spent output is unknown.
-pub fn input_sighash(input: &elements::pset::Input) -> Option<u32> {
-    if let Some(sighash) = input.sighash_type {
-        return Some(sighash.to_u32());
-    }
-
-    let script_pubkey = if let Some(prevout) = input.witness_utxo.as_ref() {
+fn input_spent_script_pubkey(input: &elements::pset::Input) -> Option<&Script> {
+    if let Some(prevout) = input.witness_utxo.as_ref() {
         Some(&prevout.script_pubkey)
     } else if let Some(tx) = input.non_witness_utxo.as_ref() {
         tx.output
@@ -417,9 +412,16 @@ pub fn input_sighash(input: &elements::pset::Input) -> Option<u32> {
             .map(|o| &o.script_pubkey)
     } else {
         None
-    };
+    }
+}
 
-    script_pubkey.map(|s| if s.is_v1_p2tr() { 0 } else { 1 })
+/// Return the sighash declared by an input, the default implied by the spent output script, or `None` if the spent output is unknown.
+pub fn input_sighash(input: &elements::pset::Input) -> Option<u32> {
+    if let Some(sighash) = input.sighash_type {
+        return Some(sighash.to_u32());
+    }
+
+    input_spent_script_pubkey(input).map(|s| if s.is_v1_p2tr() { 0 } else { 1 })
 }
 
 /// Return the issuances of a PSET, for each input return an Issuance but the struct must be checked with [`Issuance::is_issuance`] if it's a real issuance or reissuance.

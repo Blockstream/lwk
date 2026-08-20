c4f29c2ee97659136d762881771d6fa5bef9dc93
Leonardo Comandini <leo@leocomandini.org>
2026-08-20 08:17:15 +0200

    bindings: DescriptorPublicKey: getters

--- lwk_bindings/src/dpk.rs
+++ lwk_bindings/src/dpk.rs
@@ -1,6 +1,6 @@
 use std::{fmt, str::FromStr, sync::Arc};
 
-use crate::LwkError;
+use crate::{DerivationPath, LwkError};
 
 /// A descriptor public key
 #[derive(uniffi::Object)]
@@ -15,6 +15,12 @@ impl fmt::Display for DescriptorPublicKey {
     }
 }
 
+impl AsRef<lwk_common::DescriptorPublicKey> for DescriptorPublicKey {
+    fn as_ref(&self) -> &lwk_common::DescriptorPublicKey {
+        &self.inner
+    }
+}
+
 #[uniffi::export]
 impl DescriptorPublicKey {
     /// Construct a DescriptorPublicKey from its string representation
@@ -24,5 +30,22 @@ impl DescriptorPublicKey {
     pub fn new(s: &str) -> Result<Arc<Self>, LwkError> {
         let inner = lwk_common::DescriptorPublicKey::from_str(s)?;
         Ok(Arc::new(Self { inner }))
+    }
+
+    /// Return the fingerprint of the master key, if key origin information is available.
+    pub fn fingerprint(&self) -> Option<String> {
+        self.inner.fingerprint().map(|f| f.to_string())
+    }
+
+    /// Return the extended public key, without any key origin information.
+    pub fn xpub(&self) -> Option<String> {
+        self.inner.xpub().map(|f| f.to_string())
+    }
+
+    /// Return the derivation path from the master key, if key origin information is available.
+    pub fn derivation_path(&self) -> Option<Arc<DerivationPath>> {
+        self.inner
+            .derivation_path()
+            .map(|p| Arc::new(p.clone().into()))
     }
 }

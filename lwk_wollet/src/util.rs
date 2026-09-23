e25b1c1d21bb77bd11ff1ae2115dd1d0f2835714
Volodymyr Herashchenko <herashchenko_vs@proton.me>
2026-09-23 14:17:26 +0300

    wollet: remove once_cell dep

--- lwk_wollet/src/util.rs
+++ lwk_wollet/src/util.rs
@@ -2,8 +2,8 @@ use crate::secp256k1;
 use rand::thread_rng;
 
 /// The secp256k1 context, created once and reused across the crate since creating it is expensive.
-pub static EC: once_cell::sync::Lazy<secp256k1::Secp256k1<secp256k1::All>> =
-    once_cell::sync::Lazy::new(|| {
+pub static EC: std::sync::LazyLock<secp256k1::Secp256k1<secp256k1::All>> =
+    std::sync::LazyLock::new(|| {
         let mut ctx = secp256k1::Secp256k1::new();
         let mut rng = thread_rng();
         ctx.randomize(&mut rng);
